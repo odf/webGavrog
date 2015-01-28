d787c6516e30b530b82a0cdb7fa0b69a28e23703
odf <olaf.delgado@gmail.com>
2015-01-28 19:12:02 +1100

    implement more D-symbol properties

--- src/dsymbols/properties.js
+++ src/dsymbols/properties.js
@@ -140,6 +140,75 @@ var orbitReps = function orbitReps(ds, indices, seeds) {
 };
 
 
+var isConnected = function isConnected(ds) {
+  return seq.size(traversal(ds, ds.indices())) < 2;
+};
+
+
+var orbit = function orbit(ds, indices, seed) {
+  var seen = I.Set().asMutable();
+  var result = I.List().asMutable();
+
+  traversal(ds, indices, [seed]).forEach(function(e) {
+    var D = e[2];
+    if (D && !seen.contains(D)) {
+      seen.add(D);
+      result.push(D);
+    }
+  });
+
+  return result.asImmutable();
+};
+
+
+var partialOrientation = function partialOrientation(ds) {
+  var ori = I.Map().asMutable();
+
+  traversal(ds, ds.indices(), ds.elements()).forEach(function(e) {
+    var Di = e[0];
+    var i = e[1];
+    var D = e[2];
+
+    if (D && !ori.get(D))
+      ori.set(D, i == root ? 1 : -ori.get(Di));
+  });
+
+  return ori.asImmutable();
+};
+
+
+var isLoopless = function isOriented(ds) {
+  return ds.elements().every(function(D) {
+    return ds.indices().every(function(i) {
+      return D != ds.s(i, D);
+    });
+  });
+};
+
+
+var isOriented = function isOriented(ds) {
+  var ori = partialOrientation(ds);
+
+  return ds.elements().every(function(D) {
+    return ds.indices().every(function(i) {
+      return ori.get(D) != ori.get(ds.s(i, D));
+    });
+  });
+};
+
+
+var isWeaklyOriented = function isOriented(ds) {
+  var ori = partialOrientation(ds);
+
+  return ds.elements().every(function(D) {
+    return ds.indices().every(function(i) {
+      var Di = ds.s(i, D);
+      return D == Di || ori.get(D) != ori.get(Di);
+    });
+  });
+};
+
+
 module.exports = {
   isMinimal    : isMinimal,
   typePartition: typePartition,
@@ -148,22 +217,41 @@ module.exports = {
 
 
 if (require.main == module) {
-  var ds = DS.parse(
+  var test = function test(ds) {
+    console.log('ds = '+ds);
+    console.log();
+
+    console.log('    symbol is '+(isConnected(ds) ? '' : 'not ')+'connected.');
+    console.log('    symbol is '+(isMinimal(ds) ? '' : 'not ')+'minimal.');
+    console.log('    symbol is '+(isLoopless(ds) ? '' : 'not ')+'loopless.');
+    console.log('    symbol is '+(isOriented(ds) ? '' : 'not ')+'oriented.');
+    console.log('    symbol is '+(isWeaklyOriented(ds) ? '' : 'not ')
+                +'weakly oriented.');
+    console.log('    type partition: '+typePartition(ds));
+    console.log('    traversal: ' + traversal(ds, ds.indices(), ds.elements()));
+    console.log();
+
+    console.log('    0,1 orbit reps: '+orbitReps(ds, [0, 1]));
+    console.log('    1,2 orbit reps: '+orbitReps(ds, [1, 2]));
+    console.log('    0,2 orbit reps: '+orbitReps(ds, [0, 2]));
+    console.log();
+
+    console.log('    0,1 orbit of 1: '+orbit(ds, [0, 1], 1));
+    console.log('    1,2 orbit of 1: '+orbit(ds, [1, 2], 1));
+    console.log('    0,2 orbit of 1: '+orbit(ds, [0, 2], 1));
+    console.log();
+
+    console.log('    partial orientation: '+partialOrientation(ds));
+    console.log();
+    console.log();
+  }
+
+  test(DS.parse(
     '<1.1:24:' +
       '2 4 6 8 10 12 14 16 18 20 22 24,' +
       '16 3 5 7 9 11 13 15 24 19 21 23,' +
       '10 9 20 19 14 13 22 21 24 23 18 17:' +
-      '8 4,3 3 3 3>')
+      '8 4,3 3 3 3>'));
 
-  console.log('ds = '+ds);
-  console.log();
-
-  console.log('    symbol is '+(isMinimal(ds) ? '' : 'not ')+'minimal.');
-  console.log('    type partition: '+typePartition(ds));
-  console.log('    traversal: ' + traversal(ds, ds.indices(), ds.elements()));
-  console.log();
-
-  console.log('    0,1 orbit reps: '+orbitReps(ds, [0, 1]));
-  console.log('    1,2 orbit reps: '+orbitReps(ds, [1, 2]));
-  console.log('    0,2 orbit reps: '+orbitReps(ds, [0, 2]));
+  test(DS.parse('<1.1:3:1 2 3,1 3,2 3:4 8,3>'));
 }
