18b3bac92b8abe1c505b8a0eae38c907cfc7dc23
odf <olaf.delgado@gmail.com>
2015-03-01 21:01:14 +1100

    some more work on numeric framework

--- src/numbers/number.js
+++ src/numbers/number.js
@@ -3,8 +3,26 @@
 var I = require('immutable');
 
 
-var _allJoiningPathPairs = function _allJoiningPathPairs(upcasts) {
-  // TODO implement this
+var _apply = function _apply(x, f) {
+  return f(x);
+};
+
+
+var _pathPairs = function _pathPairs(s, t, eOut, eIn) {
+  return I.fromJS([[], []]);
+};
+
+
+var _coercionPathPairs = function _coercionPathPairs(upcasts) {
+  var _outEdges = upcasts.groupBy(function(e) { return e.get(0); });
+  var _inEdges = upcasts.groupBy(function(e) { return e.get(1); });
+  var _types = I.Set(_outEdges.keys());
+
+  return I.Map(_types.map(function(s) {
+    return [s, I.Map(_types.map(function(t) {
+      return [t, _pathPairs(s, t, _outEdges, _inEdges)];
+    }))];
+  }));
 };
 
 
@@ -12,12 +30,10 @@ var number = function number(promote, types, upcasts, downcasts) {
   var _methods = I.Map(I.fromJS(types).map(function(t) {
     return [t.get('type'), t];
   }));
-  var _upcastMatrix = _allJoiningPathPairs(I.fromJS(upcasts));
+  var _coercionMatrix = _coercionPathPairs(I.fromJS(upcasts));
   var _downcasts = I.Map(I.fromJS(downcasts));
 
-  var _apply = function _apply(x, f) {
-    return f(x);
-  };
+  console.log(_coercionMatrix);
 
   var _num = function _num(n) {
     if (!!n && n.type)
@@ -26,14 +42,14 @@ var number = function number(promote, types, upcasts, downcasts) {
       return promote(n);
   };
 
-  var _upcast = function _upcast(a, b) {
+  var _coerce = function _coerce(a, b) {
     a = _num(a);
     b = _num(b);
 
     if (a.type == b.type)
       return [a, b];
     else {
-      var paths = _upcastMatrix.getIn([a.type, b.type]);
+      var paths = _coercionMatrix.getIn([a.type, b.type]);
       return [paths[0].reduce(_apply, a), paths[1].reduce(_apply, b)];
     }
   };
@@ -46,28 +62,28 @@ var number = function number(promote, types, upcasts, downcasts) {
   var _property = function _property(name) {
     return function f(n) {
       n = _num(n);
-      return n.type[name](n);
+      return _methods.get(n.type)[name](n);
     };
   };
 
   var _unary = function _unary(name) {
+    var _f = _property(name);
     return function f(n) {
-      n = _num(n);
-      return _downcast(n.type[name](n));
+      return _downcast(_f(n));
     };
   };
 
   var _relation = function _unary(name) {
     return function f(a, b) {
-      var t = _upcast(a, b);
-      return t[0].type[name](t[0], t[1]);
+      var t = _coerce(a, b);
+      return _methods.get(t[0].type)[name](t[0], t[1]);
     };
   };
 
   var _binary = function _unary(name) {
+    var _f = _relation(name);
     return function f(a, b) {
-      var t = _upcast(a, b);
-      return _downcast(t[0].type[name](t[0], t[1]));
+      return _downcast(_f(a, b));
     };
   };
 
@@ -86,3 +102,13 @@ var number = function number(promote, types, upcasts, downcasts) {
   var idiv     = _binary('idiv');
   var mod      = _binary('mod');
 };
+
+
+if (require.main == module) {
+  number(
+    function() {}, // promote
+    [], // types
+    [[1,2,'a'],[1,3,'b'],[2,3,'c']], //upcasts
+    [] // downcasts
+  );
+}
