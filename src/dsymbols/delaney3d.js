d82dc51de13aa1877ff799f00ccc2ff28fe0e9f4
odf <olaf.delgado@gmail.com>
2015-04-05 17:28:32 +1000

    add two more test examples for pseudoToroidalCover()

--- src/dsymbols/delaney3d.js
+++ src/dsymbols/delaney3d.js
@@ -187,4 +187,6 @@ if (require.main == module) {
   }
 
   test(delaney.parse('<1.1:1 3:1,1,1,1:4,3,4>'));
+  test(delaney.parse('<1.1:2 3:2,1 2,1 2,2:6,3 2,6>'));
+  test(delaney.parse('<1.1:2 3:1 2,1 2,1 2,2:3 3,3 4,4>'));
 }
