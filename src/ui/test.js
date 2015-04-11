176391a5a597dd3af720c10a499227c350ece755
odf <olaf.delgado@gmail.com>
2015-04-11 22:23:37 +1000

    make the graphics test render a ball-and-stick model

--- src/ui/test.js
+++ src/ui/test.js
@@ -1,8 +1,12 @@
 'use strict';
 
+var I     = require('immutable');
 var THREE = require('three');
 var React = require('react');
 var $     = React.DOM;
+
+var R     = require('../arithmetic/float');
+var vec   = require('../arithmetic/vector')(R, 0);
 
 var Display3d = require('./Display3d');
 
@@ -28,34 +32,106 @@ var makeCamera = function(distance) {
 };
 
 
+var geometry = function geometry(vertices, faces) {
+  var geom = new THREE.Geometry();
+
+  vertices.forEach(function(v) {
+    geom.vertices.push(new THREE.Vector3(v[0], v[1], v[2]));
+  });
+
+  faces.forEach(function(f) {
+    f.forEach(function(v, i) {
+      if (i > 0 && i+1 < f.length)
+        geom.faces.push(new THREE.Face3(f[0], f[i], f[i+1]));
+    });
+  });
+
+  geom.computeFaceNormals();
+  return geom;
+};
+
+
+var stick = function stick(p, q, radius, segments) {
+  var n = segments;
+  var d = vec.normalized(vec.minus(q, p));
+  var ex = vec.make([1,0,0]);
+  var ey = vec.make([0,1,0]);
+  var t = vec.dotProduct(d, ex) > 0.9 ? ey : ex;
+  var u = vec.crossProduct(d, t);
+  var v = vec.crossProduct(d, u);
+  var a = Math.PI * 2 / n;
+
+  var section = I.Range(0, n).map(function(i) {
+    var x = a * i;
+    var c = Math.cos(x) * radius;
+    var s = Math.sin(x) * radius;
+    return vec.plus(vec.scaled(c, u), vec.scaled(s, v));
+  });
+
+  return geometry(
+    I.List().concat(section.map(function(c) { return vec.plus(c, p); }),
+                    section.map(function(c) { return vec.plus(c, q); }))
+      .map(function(v) { return v.data.toJS(); }),
+    I.Range(0, n).map(function(i) {
+      var j = (i + 1) % n;
+      return [i, j, j+n, i+n];
+    })
+  );
+}
+
+
+var ballAndStick = function ballAndStick(
+  name, positions, edges, ballMaterial, stickMaterial)
+{
+  var model = new THREE.Object3D();
+  var ball  = new THREE.SphereGeometry(2, 16, 8);
+
+  positions.forEach(function(p) {
+    var s = new THREE.Mesh(ball, ballMaterial);
+    s.position.x = p[0];
+    s.position.y = p[1];
+    s.position.z = p[2];
+    model.add(s);
+  });
+
+  edges.forEach(function(e) {
+    var u = e[0];
+    var v = e[1];
+    var s = stick(vec.make(positions[e[0]]), vec.make(positions[e[1]]), 1, 8);
+    s.computeVertexNormals();
+    model.add(new THREE.Mesh(s, stickMaterial));
+  });
+
+  return model;
+};
+
+
 var makeScene = function(model, camera) {
   var scene  = new THREE.Scene();
 
   if (!model) {
-    var sphere = new THREE.SphereGeometry(1, 12, 6);
-    var sphereMat = new THREE.MeshPhongMaterial({
+    var ballMaterial = new THREE.MeshPhongMaterial({
       color    : 0xff0000,
       shininess: 50
     });
-    var rod = new THREE.CylinderGeometry(0.5, 0.5, 4, 6);
-    var rodMat = new THREE.MeshPhongMaterial({
+    var stickMaterial = new THREE.MeshPhongMaterial({
       color    : 0x0000ff,
       shininess: 50
     });
-    model = new THREE.Object3D();
-    var s1 = new THREE.Mesh(sphere, sphereMat);
-    s1.position.x = -2;
-    model.add(s1);
-    var s2 = new THREE.Mesh(sphere, sphereMat);
-    s2.position.x = 2;
-    model.add(s2);
-    var t = new THREE.Mesh(rod, rodMat);
-    t.rotation.z = 1.5707;
-    model.add(t);
+    model = ballAndStick(
+      'cube',
+      [[-10,-10,-10], [-10,-10, 10], [-10, 10,-10], [-10, 10, 10],
+       [ 10,-10,-10], [ 10,-10, 10], [ 10, 10,-10], [ 10, 10, 10]],
+      [[0,1], [2,3], [4,5], [6,7],
+       [0,2], [1,3], [4,6], [5,7],
+       [0,4], [1,5], [2,6], [3,7]],
+      ballMaterial,
+      stickMaterial
+    );
   }
 
   if (!camera)
-    camera = makeCamera(3);
+    camera = makeCamera(18);
 
   scene.add(model);
   scene.add(camera);
