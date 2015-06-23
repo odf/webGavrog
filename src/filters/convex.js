ac11850c2e7f8fd6115acdb7dc2df11176a49975
odf <olaf.delgado@gmail.com>
2015-06-23 20:42:36 +1000

    fix the convexity filter

--- src/filters/convex.js
+++ src/filters/convex.js
@@ -2,22 +2,29 @@
 
 var fs = require('fs');
 
-var DS     = require('../dsymbols/delaney');
-var tiling = require('../dsymbols/tilings');
-var F      = require('../arithmetic/float');
-var M      = require('../arithmetic/matrix')(F, 0, 1);
+var DS       = require('../dsymbols/delaney');
+var tiling   = require('../dsymbols/tilings');
+var periodic = require('../pgraphs/periodic');
+var Q        = require('../arithmetic/number');
+var M        = require('../arithmetic/matrix')(Q, 0, 1);
+var V        = require('../arithmetic/vector')(Q, 0);
 
 var text = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
 
 DS.parseSymbols(text).forEach(function(ds) {
-  var d = DS.dim(ds);
-  var t = tiling(ds);
+  var dim = DS.dim(ds);
+  var G   = tiling(ds).graph;
+  var pos = periodic.barycentricPlacement(G).map(V.make);
 
-  if (t.cover.elements().every(function(D) {
-    var A = M.make(t.positions.get(D).valueSeq().map(function(x) {
-      return x.data;
-    }));
-    return M.rank(A) == d;
-  }))
+  var good = periodic.adjacencies(G).entrySeq().every(function(e) {
+    var p = pos.get(e[0]);
+    var neighbors = e[1].map(function(n) {
+      return V.minus(V.plus(pos.get(n.v), V.make(n.s)), p);
+    });
+    var A = M.make(neighbors.map(function(v) { return v.data; }));
+    return M.rank(A) == dim;
+  });
+
+  if (good)
     console.log(''+ds);
 });
