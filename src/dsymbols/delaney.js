221495732d7d5d30c2a1d3cd4dfc5aa30a066381
odf <olaf.delgado@gmail.com>
2015-08-29 20:58:02 +1000

    start converting to ES6

--- src/dsymbols/delaney.js
+++ src/dsymbols/delaney.js
@@ -1,48 +1,30 @@
-'use strict';
-
-var I = require('immutable');
+import * as I from 'immutable';
 
 
-var _assert = function(condition, message) {
+const _assert = function(condition, message) {
   if (!condition)
     throw new Error(message || 'assertion error');
 };
 
 
-var _isElement = function _isElement(dsImpl, D) {
-  return typeof D == 'number' && D >= 1 && D <= dsImpl.size;
-};
+const _isElement = (dsImpl, D) =>
+  typeof D == 'number' && D >= 1 && D <= dsImpl.size;
 
-var _elements = function _elements(dsImpl) {
-  return I.Range(1, dsImpl.size+1);
-};
+const _isIndex = (dsImpl, i) =>
+  typeof i == 'number' && i >= 0 && i <= dsImpl.dim;
 
-var _isIndex = function _isIndex(dsImpl, i) {
-  return typeof i == 'number' && i >= 0 && i <= dsImpl.dim;
-};
+const _elements = dsImpl => I.Range(1, dsImpl.size+1);
+const _indices  = dsImpl => I.Range(0, dsImpl.dim+1);
+const _index    = (dsImpl, i, D) => i * dsImpl.size + D - 1;
+const _get      = (dsImpl, list, i, D) => list.get(_index(dsImpl, i, D));
+const _set      = (dsImpl, list, i, D, x) => list.set(_index(dsImpl, i, D), x);
 
-var _indices = function _indices(dsImpl) {
-  return I.Range(0, dsImpl.dim+1);
-};
-
-var _index = function offset(dsImpl, i, D) {
-  return i * dsImpl.size + D - 1;
-};
-
-var _get = function offset(dsImpl, list, i, D) {
-  return list.get(_index(dsImpl, i, D));
-};
-
-var _set = function offset(dsImpl, list, i, D, x) {
-  return list.set(_index(dsImpl, i, D), x);
-};
-
-var _s = function _s(dsImpl, i, D) {
+const _s = function _s(dsImpl, i, D) {
   if (_isElement(dsImpl, D) && _isIndex(dsImpl, i))
     return _get(dsImpl, dsImpl.s, i, D);
 };
 
-var _v = function _v(dsImpl, i, j, D) {
+const _v = function _v(dsImpl, i, j, D) {
   if (_isElement(dsImpl, D) && _isIndex(dsImpl, i) && _isIndex(dsImpl, j)) {
     if (j == i+1)
       return _get(dsImpl, dsImpl.v, i, D);
@@ -56,23 +38,20 @@ var _v = function _v(dsImpl, i, j, D) {
 };
 
 
-var _merge = function _merge(a, b) {
-  return a.withMutations(function(list) {
-    b.forEach(function(x, i) {
-      if (x !== undefined)
-        list.set(i, x);
-    });
+const _merge = (a, b) => a.withMutations(list => {
+  b.forEach((x, i) => {
+    if (x !== undefined) list.set(i, x);
   });
-};
+});
 
 
-var _precheckPairings = function _checkPairings(specs, size) {
+const _precheckPairings = function _checkPairings(specs, size) {
   specs.forEach(function(p) {
     _assert(p.size == 1 || p.size == 2,
             'expected pair or singleton, got '+p);
 
-    var D = p.get(0);
-    var E = p.size > 1 ? p.get(1) : p.get(0);
+    const D = p.get(0);
+    const E = p.size > 1 ? p.get(1) : p.get(0);
 
     _assert(typeof D == 'number' && D % 1 == 0 && D > 0,
             'expected a positive integer, got '+D);
@@ -87,21 +66,21 @@ var _precheckPairings = function _checkPairings(specs, size) {
 };
 
 
-var _withPairings = function _withPairings(dsImpl, i, inputs) {
-  var specs = I.List(inputs).map(I.List);
+const _withPairings = function _withPairings(dsImpl, i, inputs) {
+  const specs = I.List(inputs).map(I.List);
   _precheckPairings(specs, dsImpl.size);
 
   _assert(typeof i == 'number' && i % 1 == 0 && i >= 0 && i <= dsImpl.dim,
           'expected an integer between 0 and '+dsImpl.dim+', got i');
 
-  var sNew = I.List().withMutations(function(list) {
-    var dangling = [];
+  const sNew = I.List().withMutations(function(list) {
+    const dangling = [];
 
     specs.forEach(function(p) {
-      var D = p.get(0);
-      var E = p.size > 1 ? p.get(1) : p.get(0);
-      var Di = _get(dsImpl, list, i, D);
-      var Ei = _get(dsImpl, list, i, E);
+      const D = p.get(0);
+      const E = p.size > 1 ? p.get(1) : p.get(0);
+      const Di = _get(dsImpl, list, i, D);
+      const Ei = _get(dsImpl, list, i, E);
 
       _assert(Di === undefined || Di == E,
               'conflicting partners '+Di+' and '+E+' for '+D);
@@ -125,12 +104,12 @@ var _withPairings = function _withPairings(dsImpl, i, inputs) {
 };
 
 
-var _precheckBranchings = function _checkBranchings(specs, size) {
+const _precheckBranchings = function _checkBranchings(specs, size) {
   specs.forEach(function(p) {
     _assert(p.size == 2, 'expected pair, got '+p);
 
-    var D = p.get(0);
-    var v = p.get(1);
+    const D = p.get(0);
+    const v = p.get(1);
 
     _assert(typeof D == 'number' && D % 1 == 0 && D > 0,
             'expected a positive integer, got '+D);
@@ -143,23 +122,23 @@ var _precheckBranchings = function _checkBranchings(specs, size) {
 };
 
 
-var _withBranchings = function _withBranchings(dsImpl, i, inputs) {
-  var specs = I.List(inputs).map(I.List);
+const _withBranchings = function _withBranchings(dsImpl, i, inputs) {
+  const specs = I.List(inputs).map(I.List);
   _precheckBranchings(specs, dsImpl.size);
 
   _assert(typeof i == 'number' && i % 1 == 0 && i >= 0 && i <= dsImpl.dim-1,
           'expected integer between 0 and '+dsImpl.dim-1+', got i');
 
-  var vNew = I.List().withMutations(function(list) {
+  const vNew = I.List().withMutations(function(list) {
     specs.forEach(function(p) {
-      var D = p.get(0);
-      var v = p.get(1);
-      var vD = _get(dsImpl, list, i, D);
+      const D = p.get(0);
+      const v = p.get(1);
+      const vD = _get(dsImpl, list, i, D);
 
       _assert(vD === undefined || vD == v,
               'conflicting values '+vD+' and '+v+' for '+D);
 
-      var E = D;
+      let E = D;
       do {
         E = _get(dsImpl, dsImpl.s, i, E) || E;
         _set(dsImpl, list, i, E, v);
@@ -174,48 +153,40 @@ var _withBranchings = function _withBranchings(dsImpl, i, inputs) {
 };
 
 
-var _fromData = function _fromData(dim, sData, vData) {
-  var s = I.List(sData);
-  var v = I.List(vData);
+const _fromData = function _fromData(dim, sData, vData) {
+  const s = I.List(sData);
+  const v = I.List(vData);
+  const size = v.size / dim;
 
-  var _ds = {
-    s   : s,
-    v   : v,
-    dim : dim,
-    size: v.size / dim
-  };
+  const _ds = { s, v, dim, size };
 
   return {
-    isElement: function(D)       { return _isElement(_ds, D); },
-    elements : function()        { return _elements(_ds); },
-    isIndex  : function(i)       { return _isIndex(_ds, i); },
-    indices  : function()        { return _indices(_ds); },
-    s        : function(i, D)    { return _s(_ds, i, D); },
-    v        : function(i, j, D) { return _v(_ds, i, j, D); },
-    toString : function()        { return stringify(this); },
+    isElement     : d         => _isElement(_ds, D),
+    elements      : ()        => _elements(_ds),
+    isIndex       : i         => _isIndex(_ds, i),
+    indices       : ()        => _indices(_ds),
+    s             : (i, D)    => _s(_ds, i, D),
+    v             : (i, j, D) => _v(_ds, i, j, D),
+    withPairings  : (i, data) => _withPairings(_ds, i, data),
+    withBranchings: (i, data) => _withBranchings(_ds, i, data),
 
-    withPairings  : function(i, inputs) {
-      return _withPairings(_ds, i, inputs);
-    },
-    withBranchings: function(i, inputs) {
-      return _withBranchings(_ds, i, inputs);
-    }
+    toString() { return stringify(this); }
   }
 };
 
 
-var build = function build(dim, size, pairingsFn, branchingsFn) {
-  var s = I.List().setSize((dim+1) * size);
-  var v = I.List().setSize(dim * size);
-  var ds0 = _fromData(dim, s, v);
+const build = function build(dim, size, pairingsFn, branchingsFn) {
+  const s = I.List().setSize((dim+1) * size);
+  const v = I.List().setSize(dim * size);
+  const ds0 = _fromData(dim, s, v);
 
-  var ds1 = I.Range(0, dim+1).reduce(
+  const ds1 = I.Range(0, dim+1).reduce(
     function(tmp, i) {
       return tmp.withPairings(i, pairingsFn(ds0, i));
     },
     ds0);
 
-  var ds2 = I.Range(0, dim).reduce(
+  const ds2 = I.Range(0, dim).reduce(
     function(tmp, i) {
       return tmp.withBranchings(i, branchingsFn(ds1, i));
     },
@@ -225,47 +196,47 @@ var build = function build(dim, size, pairingsFn, branchingsFn) {
 };
 
 
-var _parseInts = function _parseInts(str) {
+const _parseInts = function _parseInts(str) {
   return str.trim().split(/\s+/).map(function(s) { return parseInt(s); });
 };
 
 
-var parse = function parse(str) {
-  var parts = str.trim().replace(/^</, '').replace(/>$/, '').split(/:/);
+const parse = function parse(str) {
+  const parts = str.trim().replace(/^</, '').replace(/>$/, '').split(/:/);
   if (parts[0].match(/\d+\.\d+/))
     parts.shift();
 
-  var dims = _parseInts(parts[0]);
-  var size = dims[0];
-  var dim  = dims[1] || 2;
+  const dims = _parseInts(parts[0]);
+  const size = dims[0];
+  const dim  = dims[1] || 2;
 
-  var gluings = parts[1].split(/,/).map(_parseInts);
-  var degrees = parts[2].split(/,/).map(_parseInts);
+  const gluings = parts[1].split(/,/).map(_parseInts);
+  const degrees = parts[2].split(/,/).map(_parseInts);
 
-  var s = new Array((dim+1) * size);
-  var v = new Array(dim * size);
+  const s = new Array((dim+1) * size);
+  const v = new Array(dim * size);
 
-  var get = function get(a, i, D) { return a[i * size + D - 1]; };
-  var set = function set(a, i, D, x) { a[i * size + D - 1] = x; };
+  const get = function get(a, i, D) { return a[i * size + D - 1]; };
+  const set = function set(a, i, D, x) { a[i * size + D - 1] = x; };
 
-  for (var i = 0; i <= dim; ++i) {
-    var k = -1;
-    for (var D = 1; D <= size; ++D) {
+  for (let i = 0; i <= dim; ++i) {
+    let k = -1;
+    for (let D = 1; D <= size; ++D) {
       if (!get(s, i, D)) {
-        var E = gluings[i][++k];
+        const E = gluings[i][++k];
         set(s, i, D, E);
         set(s, i, E, D);
       }
     }
   }
 
-  for (var i = 0; i < dim; ++i) {
-    var k = -1;
-    for (var D = 1; D <= size; ++D) {
+  for (let i = 0; i < dim; ++i) {
+    let k = -1;
+    for (let D = 1; D <= size; ++D) {
       if (!get(v, i, D)) {
-        var m = degrees[i][++k];
-        var E = D;
-        var r = 0;
+        const m = degrees[i][++k];
+        let E = D;
+        let r = 0;
 
         do {
           E = get(s, i, E) || E;
@@ -274,7 +245,7 @@ var parse = function parse(str) {
         }
         while (E != D);
 
-        var b = m / r;
+        const b = m / r;
 
         do {
           E = get(s, i, E) || E;
@@ -291,16 +262,16 @@ var parse = function parse(str) {
 };
 
 
-var orbitReps1 = function orbitReps1(ds, i) {
+const orbitReps1 = function orbitReps1(ds, i) {
   return ds.elements().filter(function(D) {
     return (ds.s(i, D) || D) >= D;
   });
 };
 
 
-var orbit2 = function orbit2(ds, i, j, D) {
+const orbit2 = function orbit2(ds, i, j, D) {
   return I.Set().withMutations(function(set) {
-    var E = D;
+    let E = D;
     do {
       E = ds.s(i, E) || E;
       set.add(E);
@@ -312,13 +283,13 @@ var orbit2 = function orbit2(ds, i, j, D) {
 };
 
 
-var orbitReps2 = function orbitReps2(ds, i, j) {
-  var seen = new Array(ds.elements().size + 1);
-  var result = [];
+const orbitReps2 = function orbitReps2(ds, i, j) {
+  const seen = new Array(ds.elements().size + 1);
+  const result = [];
 
   ds.elements().forEach(function(D) {
     if (!seen[D]) {
-      var E = D;
+      let E = D;
 
       do {
         E = ds.s(i, E) || E;
@@ -336,8 +307,8 @@ var orbitReps2 = function orbitReps2(ds, i, j) {
 };
 
 
-var stringify = function stringify(ds) {
-  var sDefs = ds.indices()
+const stringify = function stringify(ds) {
+  const sDefs = ds.indices()
     .map(function(i) {
       return orbitReps1(ds, i)
         .map(function(D) { return ds.s(i, D) || 0; })
@@ -345,7 +316,7 @@ var stringify = function stringify(ds) {
     })
     .join(',');
 
-  var mDefs = ds.indices()
+  const mDefs = ds.indices()
     .filter(function(i) { return ds.isIndex(i+1); })
     .map(function(i) {
       return orbitReps2(ds, i, i+1)
@@ -354,16 +325,16 @@ var stringify = function stringify(ds) {
     })
     .join(',');
 
-  var n = ds.elements().size;
-  var d = ds.indices().size - 1;
+  const n = ds.elements().size;
+  const d = ds.indices().size - 1;
 
   return '<1.1:'+n+(d == 2 ? '' : ' '+d)+':'+sDefs+':'+mDefs+'>';
 };
 
 
-var r = function r(ds, i, j, D) {
-  var k = 0;
-  var E = D;
+const r = function r(ds, i, j, D) {
+  let k = 0;
+  let E = D;
 
   do {
     E = ds.s(i, E) || E;
@@ -376,7 +347,7 @@ var r = function r(ds, i, j, D) {
 };
 
 
-var m = function m(ds, i, j, D) {
+const m = function m(ds, i, j, D) {
   return ds.v(i, j, D) * r(ds, i, j, D);
 };
 
@@ -414,7 +385,7 @@ module.exports = {
     return text
       .split('\n')
       .filter(function(line) {
-        var t = line.trim();
+        const t = line.trim();
         return t.length > 0 && t[0] != '#';
       })
       .map(parse);
@@ -423,7 +394,7 @@ module.exports = {
 
 
 if (require.main == module) {
-  var ds = parse('<1.1:3:1 2 3,1 3,2 3:4 8,3>');
+  const ds = parse('<1.1:3:1 2 3,1 3,2 3:4 8,3>');
 
   console.log(stringify(ds));
   console.log('' + ds);
