ca8b0ad2081183c333ba7d458f8f69abdfb1966c
odf <olaf.delgado@gmail.com>
2015-08-29 17:12:39 +1000

    convert matrix code to ES6

--- src/arithmetic/matrix.js
+++ src/arithmetic/matrix.js
@@ -1,25 +1,21 @@
-'use strict';
-
-var I = require('immutable');
+import * as I from 'immutable';
 
 
-var matrix = function matrix(scalar, zero, one) {
+export default function matrix(scalar, zero, one) {
 
-  var Matrix = I.Record({
+  const Matrix = I.Record({
     nrows: undefined,
     ncols: undefined,
     data : undefined
   });
 
   Matrix.prototype.toString = function() {
-    return '<'+this.nrows+'x'+this.ncols+' matrix: '+this.data+'>'
+    return '<'+this.nrows+'x'+this.ncols+' matrix: '+this.data+'>';
   };
 
-  var get = function get(A, i, j) {
-    return A.data.getIn([i, j]);
-  };
+  const get = (A, i, j) => A.data.getIn([i, j]);
 
-  var _make = function _make(data) {
+  const _make = function _make(data) {
     if (data.size == 0 || data.first().size == 0)
       throw new Error('both dimensions must be positive');
 
@@ -30,90 +26,84 @@ var matrix = function matrix(scalar, zero, one) {
     });
   };
 
-  var make = function make(data) {
-    var tmp = I.List(data.map(I.List));
-    var m = tmp.map(function(row) { return row.size; }).max();
-    return _make(tmp.map(function(row) {
-      return row.concat(I.Repeat(zero, m - row.size));
-    }));
+  const make = function make(data) {
+    const tmp = I.List(data.map(I.List));
+    const m = tmp.map(row => row.size).max();
+    return _make(tmp.map(row => row.concat(I.Repeat(zero, m - row.size))));
   };
 
-  var constant = function constant(nrows, ncols, value) {
-    var x = value === undefined ? zero : value;
+  const constant = function constant(nrows, ncols, value) {
+    const x = value === undefined ? zero : value;
     return _make(I.List(I.Repeat(I.List(I.Repeat(x, ncols)), nrows)));
   };
 
-  var identity = function identity(n) {
-    var zrow = I.List(I.Repeat(zero, n));
-    return _make(I.Range(0, n).map(function(i) { return zrow.set(i, one); }));
+  const identity = function identity(n) {
+    const zrow = I.List(I.Repeat(zero, n));
+    return _make(I.Range(0, n).map(i => zrow.set(i, one)));
   };
 
-  var transposed = function transposed(A) {
-    return _make(I.Range(0, A.ncols).map(function(j) {
-      return I.Range(0, A.nrows).map(function(i) {
-        return get(A, i, j);
-      });
-    }));
+  const transposed = function transposed(A) {
+    return _make(
+      I.Range(0, A.ncols).map(j => (
+        I.Range(0, A.nrows).map(i => (
+          get(A, i, j)))))
+    );
   };
 
-  var set = function set(A, i, j, x) {
+  const set = function set(A, i, j, x) {
     return _make(A.data.setIn([i, j], x));
   };
 
-  var update = function update(A, i, j, fn) {
+  const update = function update(A, i, j, fn) {
     return _make(A.data.updateIn([i, j], fn));
   };
 
-  var plus = function plus(A, B) {
+  const plus = function plus(A, B) {
     if (A.nrows != B.nrows || A.ncols != B.ncols)
       throw new Error('shapes do not match');
 
-    return _make(I.Range(0, A.nrows).map(function(i) {
-      return I.Range(0, A.ncols).map(function(j) {
-        return scalar.plus(get(A, i, j), get(B, i, j));
-      });
-    }));
+    return _make(
+      I.Range(0, A.nrows).map(i => (
+        I.Range(0, A.ncols).map(j => (
+          scalar.plus(get(A, i, j), get(B, i, j))))))
+    );
   };
 
-  var minus = function plus(A, B) {
+  const minus = function minus(A, B) {
     if (A.nrows != B.nrows || A.ncols != B.ncols)
       throw new Error('shapes do not match');
 
-    return _make(I.Range(0, A.nrows).map(function(i) {
-      return I.Range(0, A.ncols).map(function(j) {
-        return scalar.minus(get(A, i, j), get(B, i, j));
-      });
-    }));
+    return _make(
+      I.Range(0, A.nrows).map(i => (
+        I.Range(0, A.ncols).map(j => (
+          scalar.minus(get(A, i, j), get(B, i, j))))))
+    );
   };
 
-  var scaled = function scaled(f, A) {
-    return _make(A.data.map(function(row) {
-      return row.map(function(x) {
-        return scalar.times(f, x);
-      });
-    }));
+  const scaled = function scaled(f, A) {
+    return _make(A.data.map(row => row.map(x => scalar.times(f, x))));
   };
 
-  var times = function times(A, B) {
+  const times = function times(A, B) {
     if (A.ncols != B.nrows)
       throw new Error('shapes do not match');
 
-    return _make(I.Range(0, A.nrows).map(function(i) {
-      return I.Range(0, B.ncols).map(function(j) {
-        return I.Range(0, A.ncols)
-          .map(function(k) { return scalar.times(get(A, i, k), get(B, k, j)); })
-          .reduce(scalar.plus, zero);
-      });
-    }));
+    return _make(
+      I.Range(0, A.nrows).map(i => (
+        I.Range(0, B.ncols).map(j => (
+          I.Range(0, A.ncols)
+            .map(k => scalar.times(get(A, i, k), get(B, k, j)))
+            .reduce(scalar.plus, zero)))))
+    );
   };
 
 
-  var _findPivot = function _findPivot(A, row, col, overField) {
-    var best = row;
-    for (var i = row; i < A.nrows; ++i) {
-      var x = scalar.abs(get(A, i, col));
+  const _findPivot = function _findPivot(A, row, col, overField) {
+    let best = row;
+    for (let i = row; i < A.nrows; ++i) {
+      const x = scalar.abs(get(A, i, col));
       if (scalar.sgn(x) != 0) {
-        var d = scalar.cmp(x, scalar.abs(get(A, best, col)));
+        const d = scalar.cmp(x, scalar.abs(get(A, best, col)));
         if (overField ? d > 0 : d < 0)
           best = i;
       }
@@ -121,56 +111,49 @@ var matrix = function matrix(scalar, zero, one) {
     return best;
   };
 
-  var _swapRows = function _swapRows(A, i, j) {
+  const _swapRows = function _swapRows(A, i, j) {
     return _make(A.data.set(i, A.data.get(j)).set(j, A.data.get(i)));
   };
 
-  var _negateRow = function _negateRow(A, i) {
+  const _negateRow = function _negateRow(A, i) {
     return _make(A.data.set(i, A.data.get(i).map(scalar.negative)));
   };
 
-  var _truncate = function _truncate(x, a) {
-    var d = scalar.times(a, scalar.epsilon);
+  const _truncate = function _truncate(x, a) {
+    const d = scalar.times(a, scalar.epsilon);
     if (scalar.cmp(scalar.abs(x), scalar.abs(d)) < 0)
       return zero;
     else
       return x;
   };
 
-  var _adjustRow = function _adjustRow(A, i, j, f) {
-    var _val = function(k) {
-      return scalar.plus(get(A, i, k), scalar.times(get(A, j, k), f));
-    };
-    var val;
+  const _adjustRow = function _adjustRow(A, i, j, f) {
+    const fn0 = k => scalar.plus(get(A, i, k), scalar.times(get(A, j, k), f));
+    const fn  = scalar.epsilon ? k => _truncate(fn0(k), get(A, i, k)) : fn0;
 
-    if (scalar.epsilon)
-      val = function(k) { return _truncate(_val(k), get(A, i, k)); };
-    else
-      val = _val;
-
-    return _make(A.data.set(i, I.Range(0, A.ncols).map(val)));
+    return _make(A.data.set(i, I.Range(0, A.ncols).map(fn)));
   };
 
-  var _Triangulation = I.Record({
+  const _Triangulation = I.Record({
     R: undefined,
     U: undefined,
     sign: undefined
   });
 
-  var triangulation = function triangulation(A, overField) {
-    var R = A;
-    var U = identity(R.nrows);
-    var col = 0;
-    var sign = 1;
-    var overField = !!overField;
-    var divide = overField ? scalar.div : scalar.idiv;
+  const triangulation = function triangulation(A, overField) {
+    const divide = overField ? scalar.div : scalar.idiv;
 
-    for (var row = 0; row < R.nrows; ++row) {
-      var cleared = false;
+    let R = A;
+    let U = identity(R.nrows);
+    let col = 0;
+    let sign = 1;
+
+    for (let row = 0; row < R.nrows; ++row) {
+      let cleared = false;
 
       while (!cleared && col < R.ncols) {
-        var pivotRow = _findPivot(R, row, col, overField);
-        var pivot = get(R, pivotRow, col);
+        const pivotRow = _findPivot(R, row, col, overField);
+        const pivot = get(R, pivotRow, col);
 
         if (scalar.sgn(pivot) == 0) {
           ++col;
@@ -191,9 +174,9 @@ var matrix = function matrix(scalar, zero, one) {
 
         cleared = true;
 
-        for (var k = row + 1; k < R.nrows; ++k) {
+        for (let k = row + 1; k < R.nrows; ++k) {
           if (scalar.sgn(get(R, k, col)) != 0) {
-            var f = scalar.negative(divide(get(R, k, col), get(R, row, col)));
+            const f = scalar.negative(divide(get(R, k, col), get(R, row, col)));
 
             R = _adjustRow(R, k, row, f);
             U = _adjustRow(U, k, row, f);
@@ -214,28 +197,28 @@ var matrix = function matrix(scalar, zero, one) {
   };
 
 
-  var _rank = function _rank(R) {
-    var row = 0;
-    for (var col = 0; col < R.ncols; ++col)
+  const _rank = function _rank(R) {
+    let row = 0;
+    for (let col = 0; col < R.ncols; ++col)
       if (row < R.nrows && scalar.sgn(get(R, row, col)) != 0)
         ++row;
     return row;
   };
 
 
-  var rank = function rank(A) {
+  const rank = function rank(A) {
     return _rank(triangulation(A, true).R);
   };
 
 
-  var _determinant = function _determinant(t) {
+  const _determinant = function _determinant(t) {
     return I.Range(0, t.R.nrows)
-      .map(function(i) { return get(t.R, i, i); })
+      .map(i => get(t.R, i, i))
       .reduce(scalar.times, t.sign);
   };
 
 
-  var determinant = function determinant(A) {
+  const determinant = function determinant(A) {
     if (A.nrows != A.ncols)
       throw new Error('must be a square matrix');
 
@@ -243,20 +226,20 @@ var matrix = function matrix(scalar, zero, one) {
   };
 
 
-  var _solve = function _solve(R, v) {
-    var n = R.nrows;
-    var m = R.ncols;
-    var k = v.ncols;
+  const _solve = function _solve(R, v) {
+    const n = R.nrows;
+    const m = R.ncols;
+    const k = v.ncols;
+    const top = Math.min(n, m);
 
-    var X = constant(m, k);
-    var top = Math.min(n, m);
+    let X = constant(m, k);
 
-    for (var j = 0; j < k; ++j) {
-      for (var i = top-1; i >= 0; --i) {
-        var x = I.Range(i+1, top).map(function(nu) {
-          return scalar.times(get(R, i, nu), get(X, nu, j));
-        }).reduce(scalar.plus, zero);
-        var right = scalar.minus(get(v, i, j), x);
+    for (let j = 0; j < k; ++j) {
+      for (let i = top-1; i >= 0; --i) {
+        const x = I.Range(i+1, top)
+          .map(nu => scalar.times(get(R, i, nu), get(X, nu, j)))
+          .reduce(scalar.plus, zero);
+        const right = scalar.minus(get(v, i, j), x);
 
         if (scalar.sgn(right) == 0)
           X = set(X, i, j, right);
@@ -271,17 +254,17 @@ var matrix = function matrix(scalar, zero, one) {
   };
 
 
-  var solve = function solve(A, b) {
+  const solve = function solve(A, b) {
     if (A.nrows != b.nrows)
       throw new Error('matrix shapes must match');
 
-    var t = triangulation(A, true);
+    const t = triangulation(A, true);
 
     return _solve(t.R, times(t.U, b));
   };
 
 
-  var inverse = function inverse(A) {
+  const inverse = function inverse(A) {
     if (A.nrows != A.ncols)
       throw new Error('must be a square matrix');
 
@@ -289,50 +272,48 @@ var matrix = function matrix(scalar, zero, one) {
   };
 
 
-  var _nullSpace = function _nullSpace(R) {
-    var n = R.nrows;
-    var m = R.ncols;
-    var r = _rank(R);
-    var d = m - r;
+  const _nullSpace = function _nullSpace(R) {
+    const n = R.nrows;
+    const m = R.ncols;
+    const r = _rank(R);
+    const d = m - r;
 
     if (d == 0)
       return null;
     else if (r == 0)
       return identity(m);
 
-    var B = make(I.Range(0, r).map(function(i) {
-      return I.Range(0, d).map(function(j) {
-        return (j + r >= n) ? 0 : scalar.negative(M.get(R, i, j + r));
-      });
-    }));
+    const B = make(
+      I.Range(0, r).map(i => (
+        I.Range(0, d).map(j => (
+          (j + r >= n) ? 0 : scalar.negative(get(R, i, j + r))))))
+    );
 
-    var S = _solve(make(R.data.slice(0,r)), B);
-    return make(S.data.slice(0, r).concat(M.identity(d).data));
+    const S = _solve(make(R.data.slice(0,r)), B);
+    return make(S.data.slice(0, r).concat(identity(d).data));
   };
 
 
-  var nullSpace = function nullSpace(A) {
+  const nullSpace = function nullSpace(A) {
     return _nullSpace(triangulation(A, true).R);
   };
 
 
-  var _rowProduct = function _rowProduct(A, i, j) {
+  const _rowProduct = function _rowProduct(A, i, j) {
     return I.Range(0, A.ncols)
-      .map(function(k) { return scalar.times(get(A, i, k), get(A, j, k)); })
+      .map(k => scalar.times(get(A, i, k), get(A, j, k)))
       .reduce(scalar.plus, zero);
   };
 
-  var _normalizeRow = function _normalizeRow(A, i) {
-    var norm = Math.sqrt(scalar.toJS(_rowProduct(A, i, i)));
-    return _make(A.data.set(i, A.data.get(i).map(function(x) {
-      return scalar.div(x, norm);
-    })));
+  const _normalizeRow = function _normalizeRow(A, i) {
+    const norm = Math.sqrt(scalar.toJS(_rowProduct(A, i, i)));
+    return _make(A.data.set(i, A.data.get(i).map(x => scalar.div(x, norm))));
   };
 
-  var orthonormalized = function orthonormalized(A) {
-    I.Range(0, A.nrows).forEach(function(i) {
-      I.Range(0, i).forEach(function(j) {
-        A = _adjustRow(A, i, j, scalar.negative(_rowProduct(A, i, j)));
+  const orthonormalized = function orthonormalized(A) {
+    I.Range(0, A.nrows).forEach(i => {
+      I.Range(0, i).forEach(j => {
+        A = _adjustRow(A, i, j, scalar.negative(_rowProduct(A, i, j)))
       });
       A = _normalizeRow(A, i);
     });
@@ -363,11 +344,8 @@ var matrix = function matrix(scalar, zero, one) {
 };
 
 
-module.exports = matrix;
-
-
 if (require.main == module) {
-  var M = matrix(require('./number'), 0, 1);
+  let M = matrix(require('./number'), 0, 1);
 
   console.log(M.constant(3, 4));
   console.log(M.constant(3, 4, 5));
@@ -377,8 +355,8 @@ if (require.main == module) {
   console.log(M.transposed(M.set(M.identity(3), 0, 1, 4)));
   console.log();
 
-  var testTriangulation = function testTriangulation(A) {
-    var t = M.triangulation(A);
+  const testTriangulation = function testTriangulation(A) {
+    const t = M.triangulation(A);
     console.log('A = '+A);
     console.log('t.U = '+t.U);
     console.log('t.R = '+t.R);
@@ -392,8 +370,8 @@ if (require.main == module) {
   testTriangulation(M.make([[1,2,3],[6,5,4],[7,8,9]]));
   testTriangulation(M.make([[1],[2,3],[4,5,6]]));
 
-  var testSolve = function testSolve(A, b) {
-    var x = M.solve(A, b);
+  const testSolve = function testSolve(A, b) {
+    const x = M.solve(A, b);
     console.log('A = '+A);
     console.log('b = '+b);
     console.log('x = '+x);
@@ -406,8 +384,8 @@ if (require.main == module) {
   testSolve(M.make([[1,2,3],[0,4,5],[0,0,6]]),
             M.make([[1],[2,3],[4,5,6]]));
 
-  var testInverse = function testInverse(A) {
-    var B = M.inverse(A);
+  const testInverse = function testInverse(A) {
+    const B = M.inverse(A);
     console.log('A = '+A);
     console.log('R = '+M.triangulation(A, true).R);
     if (B) {
@@ -431,9 +409,9 @@ if (require.main == module) {
   testInverse(M.make([[1,2,3],[4,5,6],[7,8,9]]));
   testInverse(M.make([[1,2,3],[2,4,6],[3,6,9]]));
 
-  var testOrthonormalize = function testOrthonormalize(A) {
+  const testOrthonormalize = function testOrthonormalize(A) {
     console.log('A = '+A);
-    var O = M.orthonormalized(A);
+    const O = M.orthonormalized(A);
     console.log('O = '+O);
     console.log('O * O^t = '+M.times(O, M.transposed(O)));
     console.log();
