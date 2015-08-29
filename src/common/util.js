d3966b61bc1b333e050f13a610dbb1a4415eda17
odf <olaf.delgado@gmail.com>
2015-08-29 20:18:04 +1000

    convert to ES6

--- src/common/util.js
+++ src/common/util.js
@@ -1,24 +1,14 @@
-'use strict';
-
-var I = require('immutable');
+import * as I from 'immutable';
 
 
-var cmpLex = function cmpLex(cmp) {
-  if (typeof cmp != 'function')
-    cmp = function(x, y) { return x < y ? -1 : x > y ? 1 : 0; };
-
+export function cmpLex(cmp = (x, y) => x < y ? -1 : x > y ? 1 : 0) {
   return function(a, b) {
-    var n = Math.min(a.size, b.size);
-    for (var i = 0; i < n; ++i) {
-      var d = cmp(a.get(i), b.get(i));
+    const n = Math.min(a.size, b.size);
+    for (let i = 0; i < n; ++i) {
+      const d = cmp(a.get(i), b.get(i));
       if (d)
         return d;
     }
     return a.size - b.size;
   };
-};
-
-
-module.exports = {
-  cmpLex: cmpLex
 };
