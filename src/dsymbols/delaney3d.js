09023aca9e0f5e55929ddc5ad2df5bb986c3a914
odf <olaf.delgado@gmail.com>
2015-09-12 19:23:41 +1000

    convert delaney3d to ES6

--- src/dsymbols/delaney3d.js
+++ src/dsymbols/delaney3d.js
@@ -1,18 +1,20 @@
-'use strict';
+import * as I from 'immutable';
 
-var I = require('immutable');
-var Q = require('../arithmetic/number');
-var M = require('../arithmetic/matrix')(Q, 0, 1);
+import * as Q from '../arithmetic/number';
+import     _M from '../arithmetic/matrix';
 
-var generators  = require('../common/generators');
-var cosets      = require('../fpgroups/cosets');
-var stabilizer  = require('../fpgroups/stabilizer');
-var fundamental = require('./fundamental');
-var derived     = require('./derived');
-var covers      = require('./covers');
+import stabilizer from '../fpgroups/stabilizer';
+
+import * as generators  from '../common/generators';
+import * as cosets      from '../fpgroups/cosets';
+import * as fundamental from './fundamental';
+import * as derived     from './derived';
+import * as covers      from './covers';
 
 
-var _coreType = {
+const M = _M(Q, 0, 1);
+
+const _coreType = {
    1: 'z1',
    2: 'z2',
    3: 'z3',
@@ -22,61 +24,52 @@ var _coreType = {
   24: 's4'
 };
 
-var _fullyInvolutive = function _fullyInvolutive(ct) {
-  return ct.every(function(row) {
-    return row.keySeq().every(function(g) {
-      return row.get(g) == row.get(-g);
-    });
-  });
-};
 
-var _traceWord = function _traceWord(ct, k, word) {
-  return word.reduce(function(k, g) { return ct.getIn([k, g]); }, k);
-};
+const _fullyInvolutive = ct =>
+  ct.every(row => row.keySeq().every(g => row.get(g) == row.get(-g)));
 
-var _degree = function _degree(ct, word) {
-  var k = 0;
-  for (var i = 1; ; ++i) {
+
+const _traceWord = (ct, k, word) => word.reduce((k, g) => ct.getIn([k, g]), k);
+
+
+const _degree = function _degree(ct, word) {
+  let k = 0;
+  for (let i = 1; ; ++i) {
     k = _traceWord(ct, k, word);
     if (k == 0)
       return i;
   }
 };
 
-var _flattensAll = function _flattensAll(ct, cones) {
-  return cones.every(function(cone) {
-    return _degree(ct, cone[0]) == cone[1];
-  });
-};
 
-var _isDiagonal = function _isDiagonal(mat) {
-  return I.Range(0, mat.nrows).every(function(i) {
-    return I.Range(0, mat.ncols).every(function(j) {
-      return i == j || Q.sgn(M.get(mat, i, j)) == 0;
-    });
-  });
-};
+const _flattensAll = (ct, cones) =>
+  cones.every(([wd, d]) => _degree(ct, wd) == d);
 
-var _gcd = function _gcd(a, b) {
+
+const _isDiagonal = mat => (
+  I.Range(0, mat.nrows).every(i => (
+    I.Range(0, mat.ncols).every(j => (
+      i == j || 0 == Q.sgn(M.get(mat, i, j)))))));
+
+
+const _gcd = function _gcd(a, b) {
   a = Q.abs(a);
   b = Q.abs(b);
 
-  while (Q.sgn(b) > 0) {
-    var t = b;
-    b = Q.mod(a, b);
-    a = t;
-  }
+  while (Q.sgn(b) > 0)
+    [a, b] = [b, Q.mod(a, b)];
 
   return a;
 };
 
-var _factors = function _factors(xs) {
+
+const _factors = function _factors(xs) {
   return I.List(xs).withMutations(function(xs) {
     I.Range(0, xs.size).forEach(function(i) {
-      var a = xs.get(i);
+      let a = xs.get(i);
       I.Range(i+1, xs.size).forEach(function(j) {
-        var b = xs.get(j);
-        var g = _gcd(a, b);
+        const b = xs.get(j);
+        const g = _gcd(a, b);
         xs.set(j, Q.sgn(g) == 0 ? 0 : Q.times(Q.idiv(a, g), b));
         a = g;
       });
@@ -85,105 +78,80 @@ var _factors = function _factors(xs) {
   });
 };
 
-var _invariants = function _invariants(nrGens, rels) {
-  var mat = M.make(cosets.relatorMatrix(nrGens, rels));
+
+const _invariants = function _invariants(nrGens, rels) {
+  let mat = M.make(cosets.relatorMatrix(nrGens, rels));
 
   while (!_isDiagonal(mat)) {
     mat = M.transposed(M.triangulation(mat).R);
     mat = M.transposed(M.triangulation(mat).R);
   }
 
-  var d = Math.min(mat.nrows, mat.ncols);
-  var diag = I.Range(0, d).map(function(i) { return M.get(mat, i, i); });
-  var factors = _factors(diag)
-    .filter(function(x) { return Q.cmp(x, 1) != 0; })
-    .sort(Q.cmp);
+  const d       = Math.min(mat.nrows, mat.ncols);
+  const diag    = I.Range(0, d).map(i => M.get(mat, i, i));
+  const factors = _factors(diag).filter(x => Q.cmp(x, 1)).sort(Q.cmp);
 
   return I.Repeat(0, nrGens - d).concat(factors);
 };
 
-var pseudoToroidalCover = function pseudoToroidalCover(ds) {
+
+export function pseudoToroidalCover(ds) {
   ds = derived.orientedCover(ds);
 
-  var fg = fundamental.fundamentalGroup(ds);
-  var cones = fg.cones;
+  const fg = fundamental.fundamentalGroup(ds);
+  const cones = fg.cones;
 
-  if (cones.some(function(c) { return c[1] == 5 || c[1] > 6; }))
+  if (cones.some(c => c[1] == 5 || c[1] > 6))
     throw new Error('violates the crystallographic restriction');
 
-  var cones2 = cones.filter(function(c) { return c[1] == 2; });
-  var cones3 = cones.filter(function(c) { return c[1] == 3; });
+  const cones2 = cones.filter(c => c[1] == 2);
+  const cones3 = cones.filter(c => c[1] == 3);
 
-  var sub4 = generators.results(cosets.tables(fg.nrGenerators, fg.relators, 4));
-  var base = sub4.map(cosets.coreTable);
+  const tableGen = cosets.tables(fg.nrGenerators, fg.relators, 4);
+  const base     = generators.results(tableGen).map(cosets.coreTable);
 
-  var cores = base.filter(function(ct) { return _flattensAll(ct, cones); })
-    .map(function(ct) {
-      if (ct.size == 4)
-        return _fullyInvolutive(ct) ? ['v4', ct] : ['z4', ct];
-      else
-        return [_coreType[ct.size], ct];
-    });
+  const cType = ct =>
+    ct.size == 4 ? (_fullyInvolutive(ct) ? 'v4' : 'z4') : _coreType[ct.size];
 
-  var z2a = base.filter(function(ct) {
-    return ct.size == 2 && _flattensAll(ct, cones2);
-  });
+  const cores = base
+    .filter(ct => _flattensAll(ct, cones))
+    .map(ct => [cType(ct), ct]);
 
-  var z2b = base.filter(function(ct) {
-    return ct.size == 2 && !_flattensAll(ct, cones2);
-  });
+  const z2a = base.filter(ct => ct.size == 2 &&  _flattensAll(ct, cones2));
+  const z2b = base.filter(ct => ct.size == 2 && !_flattensAll(ct, cones2));
+  const z3a = base.filter(ct => ct.size == 3 &&  _flattensAll(ct, cones3));
+  const s3a = base.filter(ct => ct.size == 6 &&  _flattensAll(ct, cones3));
 
-  var z3a = base.filter(function(ct) {
-    return ct.size == 3 && _flattensAll(ct, cones3);
-  });
+  const z6 = z3a.flatMap(a => (
+    z2a
+      .map(b => cosets.intersectionTable(a, b))
+      .filter(ct => ct.size == 6 && _flattensAll(ct, cones))
+      .map(ct => ['z6', ct])));
 
-  var s3a = base.filter(function(ct) {
-    return ct.size == 6 && _flattensAll(ct, cones3);
-  });
+  const d6 = s3a.flatMap(a => (
+    z2b
+      .map(b => cosets.intersectionTable(a, b))
+      .filter(ct => ct.size == 12 && _flattensAll(ct, cones))
+      .map(ct => ['d6', ct])));
 
-  var z6 = z3a.flatMap(function(a) {
-    return z2a
-      .map(function(b) { return cosets.intersectionTable(a, b); })
-      .filter(function(ct) {
-        return ct.size == 6 && _flattensAll(ct, cones);
-      })
-      .map(function(ct) { return ['z6', ct]; });
-  });
+  const categorized = I.List().concat(cores, z6, d6).groupBy(a => a[0]);
 
-  var d6 = s3a.flatMap(function(a) {
-    return z2b
-      .map(function(b) { return cosets.intersectionTable(a, b); })
-      .filter(function(ct) {
-        return ct.size == 12 && _flattensAll(ct, cones);
-      })
-      .map(function(ct) { return ['d6', ct]; });
-  });
+  const candidates = I.List('z1 z2 z3 z4 v4 s3 z6 d4 d6 a4 s4'.split(' '))
+    .flatMap(type => categorized.get(type) || []);
 
-  var categorized = I.List().concat(cores, z6, d6).groupBy(function(a) {
-    return a[0];
-  });
-  var candidates = I.List('z1 z2 z3 z4 v4 s3 z6 d4 d6 a4 s4'.split(' '))
-    .flatMap(function(type) {
-      return categorized.get(type) || [];
-    });
+  const mapFn = map => (...args) => map.getIn(args);
 
-  var mapFn = function(map) {
-    return function() {
-      return map.getIn([].slice.apply(arguments));
-    };
-  };
-
-  var good = candidates.find(function(entry) {
-    var table = entry[1];
-    var domain = table.keySeq();
-    var stab = stabilizer(
+  const good = candidates.find(function(entry) {
+    const table = entry[1];
+    const domain = table.keySeq();
+    const stab = stabilizer(
       domain.first(),
       fg.nrGenerators,
       fg.relators,
       domain,
       mapFn(table)
     );
-    var inv = _invariants(stab.generators.size, stab.relators);
+    const inv = _invariants(stab.generators.size, stab.relators);
     return inv.map(Q.sgn).equals(I.List([0,0,0]));
   });
 
@@ -191,17 +159,12 @@ var pseudoToroidalCover = function pseudoToroidalCover(ds) {
 };
 
 
-module.exports = {
-  pseudoToroidalCover: pseudoToroidalCover
-};
-
-
 if (require.main == module) {
-  var delaney = require('./delaney');
+  const delaney = require('./delaney');
 
-  var test = function test(ds) {
+  const test = function test(ds) {
     console.log('ds = '+ds);
-    var cov = pseudoToroidalCover(ds);
+    const cov = pseudoToroidalCover(ds);
     console.log('cov = '+cov);
     console.log();
   }
