17787a9f4d28aae578d9b8b007a5193b768bbe02
odf <olaf.delgado@gmail.com>
2015-09-14 19:59:50 +1000

    convert stabilizer to ES6

--- src/fpgroups/stabilizer.js
+++ src/fpgroups/stabilizer.js
@@ -1,28 +1,26 @@
-'use strict';
-
-var I = require('immutable');
-var fw = require('./freeWords');
+import * as I  from 'immutable';
+import * as fw from './freeWords';
 
 
-var _relatorPermutations = function _relatorPermutations(w) {
+const _relatorPermutations = function _relatorPermutations(w) {
   return I.Range(0, w.size).flatMap(function(i) {
-    var wx = fw.product([w.slice(i), w.slice(0, i)]);
+    const wx = fw.product([w.slice(i), w.slice(0, i)]);
     return [wx, fw.inverse(wx)];
   });
 };
 
 
-var _relatorsByStartGen = function _relatorsByStartGen(relators) {
+const _relatorsByStartGen = function _relatorsByStartGen(relators) {
   return I.List(relators).map(I.List)
     .flatMap(_relatorPermutations)
-    .groupBy(function(rel) { return rel.get(0); })
+    .groupBy(rel => rel.get(0))
     .map(I.Set);
 };
 
 
-var _sgn = function _sgn(x) { return (x > 0) - (x < 0); };
+const _sgn = x => (x > 0) - (x < 0);
 
-var _cmpGens = function _cmpGens(a, b) {
+const _cmpGens = function(a, b) {
   if (_sgn(a) != _sgn(b))
     return _sgn(b) - _sgn(a);
   else
@@ -30,10 +28,10 @@ var _cmpGens = function _cmpGens(a, b) {
 };
 
 
-var _cmpWords = function _cmpWords(a, b) {
-  var n = Math.min(a.size, b.size);
-  for (var i = 0; i < n; ++i) {
-    var d = _cmpGens(a.get(i), b.get(i));
+const _cmpWords = function _cmpWords(a, b) {
+  const n = Math.min(a.size, b.size);
+  for (let i = 0; i < n; ++i) {
+    const d = _cmpGens(a.get(i), b.get(i));
     if (d)
       return d;
   }
@@ -46,30 +44,26 @@ var _cmpWords = function _cmpWords(a, b) {
 };
 
 
-var Edge = I.Record({
+const Edge = I.Record({
   point: undefined,
   gen  : undefined
 });
 
 
-var _inverseGen = function _inverseGen(g) {
-  return fw.inverse([g]).first();
-};
+const _inverseGen = g => fw.inverse([g]).first();
 
 
-var _reverseEdge = function _reverseEdge(edge, action) {
-  return new Edge({
-    point: action(edge.point, edge.gen),
-    gen  : _inverseGen(edge.gen)
-  });
-};
+const _reverseEdge = (edge, action) => new Edge({
+  point: action(edge.point, edge.gen),
+  gen  : _inverseGen(edge.gen)
+});
 
 
-var _traceWord = function _traceWord(point, w, edge2word, action) {
-  var p = point;
-  var trace = fw.empty;
+const _traceWord = function _traceWord(point, w, edge2word, action) {
+  let p = point;
+  let trace = fw.empty;
   w.forEach(function(g) {
-    var t = edge2word.get(new Edge({ point: p, gen: g }));
+    const t = edge2word.get(new Edge({ point: p, gen: g }));
     trace = fw.product([trace, t]);
     p = action(p, g);
   });
@@ -78,36 +72,36 @@ var _traceWord = function _traceWord(point, w, edge2word, action) {
 };
 
 
-var _closeRelations = function _closeRelations(
+const _closeRelations = function _closeRelations(
   startEdge,
   edge2word,
   relsByGen,
   action
 ) {
-  var queue = I.List([startEdge]);
+  let queue = I.List([startEdge]);
 
   while (!queue.isEmpty()) {
-    var next = queue.first();
+    const next = queue.first();
     queue = queue.rest();
 
-    var p = next.point;
-    var g = next.gen;
+    const p = next.point;
+    const g = next.gen;
 
     relsByGen.get(g).forEach(function(r) {
-      var x = p;
-      var cuts = I.List();
+      let x = p;
+      let cuts = I.List();
       r.forEach(function(h, i) {
-        var next = new Edge({ point: x, gen: h });
+        const next = new Edge({ point: x, gen: h });
         if (edge2word.get(next) == null)
           cuts = cuts.push([i, next]);
         x = action(x, h);
       });
 
       if (cuts.size == 1) {
-        var i = cuts.first()[0];
-        var cut = cuts.first()[1];
-        var w = fw.inverse(fw.product([r.slice(i+1), r.slice(0, i)]));
-        var trace = _traceWord(cut.point, w, edge2word, action);
+        const i = cuts.first()[0];
+        const cut = cuts.first()[1];
+        const w = fw.inverse(fw.product([r.slice(i+1), r.slice(0, i)]));
+        const trace = _traceWord(cut.point, w, edge2word, action);
 
         edge2word = edge2word
           .set(cut, trace)
@@ -122,18 +116,19 @@ var _closeRelations = function _closeRelations(
 };
 
 
-var _spanningTree = function _spanningTree(basePoint, nrGens, action) {
-  var queue = I.List([basePoint]);
-  var seen = I.Set([basePoint]);
-  var gens = I.Range(1, nrGens+1).flatMap(function(i) { return [i, -i]; });
-  var edges = I.List();
+const _spanningTree = function _spanningTree(basePoint, nrGens, action) {
+  const gens = I.Range(1, nrGens+1).flatMap(i => [i, -i]);
+
+  let queue = I.List([basePoint]);
+  let seen = I.Set([basePoint]);
+  let edges = I.List();
 
   while (!queue.isEmpty()) {
-    var point = queue.first();
+    const point = queue.first();
     queue = queue.rest();
 
     gens.forEach(function(g) {
-      var p = action(point, g);
+      const p = action(point, g);
       if (!seen.contains(p)) {
         queue = queue.push(p);
         seen = seen.add(p);
@@ -146,16 +141,16 @@ var _spanningTree = function _spanningTree(basePoint, nrGens, action) {
 };
 
 
-var stabilizer = function stabilizer(
+const stabilizer = function stabilizer(
   basePoint, nrGens, relators, domain, action
 ) {
-  var relsByGen = _relatorsByStartGen(relators);
-  var tree = _spanningTree(basePoint, nrGens, action);
-  var gens = I.Range(1, nrGens+1).flatMap(function(i) { return [i, -i]; });
-  var id = fw.empty;
+  const relsByGen = _relatorsByStartGen(relators);
+  const tree = _spanningTree(basePoint, nrGens, action);
+  const gens = I.Range(1, nrGens+1).flatMap(i => [i, -i]);
+  const id = fw.empty;
 
-  var point2word = I.Map([[basePoint, id]]);
-  var edge2word = I.Map();
+  let point2word = I.Map([[basePoint, id]]);
+  let edge2word = I.Map();
 
   tree.forEach(function(edge) {
     edge2word = edge2word.set(edge, id).set(_reverseEdge(edge, action), id);
@@ -165,18 +160,18 @@ var stabilizer = function stabilizer(
       fw.product([point2word.get(edge.point), [edge.gen]]));
   });
 
-  var lastGen = 0;
-  var generators = I.List();
+  let lastGen = 0;
+  let generators = I.List();
 
   domain.forEach(function(px) {
-    var wx = point2word.get(px);
+    const wx = point2word.get(px);
     gens.forEach(function(g) {
-      var edge = new Edge({ point: px, gen: g });
+      const edge = new Edge({ point: px, gen: g });
       if (edge2word.get(edge) == null) {
-        var py = action(px, g);
-        var wy = point2word.get(py);
-        var h = ++lastGen;
-        var redge = _reverseEdge(edge, action);
+        const py = action(px, g);
+        const wy = point2word.get(py);
+        const h = ++lastGen;
+        const redge = _reverseEdge(edge, action);
         edge2word = edge2word
           .set(edge, fw.word([h]))
           .set(redge, fw.inverse([h]));
@@ -187,14 +182,12 @@ var stabilizer = function stabilizer(
     });
   });
 
-  var subrels = I.Set(domain)
-    .flatMap(function(p) {
-      return relators.map(function(w) {
-        var trace = _traceWord(p, w, edge2word, action);
-        return _relatorPermutations(trace).min(_cmpWords);
-      });
-    })
-    .filter(function(w) { return w && w.size > 0; })
+  const subrels = I.Set(domain)
+    .flatMap(p => relators.map(function(w) {
+      const trace = _traceWord(p, w, edge2word, action);
+      return _relatorPermutations(trace).min(_cmpWords);
+    }))
+    .filter(w => w && w.size > 0)
     .sort(_cmpWords);
 
   return { generators: generators, relators: subrels };
@@ -205,11 +198,7 @@ module.exports = stabilizer;
 
 
 if (require.main == module) {
-  var mapFn = function(map) {
-    return function() {
-      return map.getIn([].slice.apply(arguments));
-    };
-  };
+  const mapFn = map => (...args) => map.getIn(args);
 
   console.log(stabilizer(
     'a',
