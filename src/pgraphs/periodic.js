bc0e955c658c8cb3709f50ef9ba13906c64a8a72
odf <olaf.delgado@gmail.com>
2015-09-19 18:29:17 +1000

    convert periodic.js to ES6

--- src/pgraphs/periodic.js
+++ src/pgraphs/periodic.js
@@ -1,11 +1,11 @@
-'use strict';
+import * as I from 'immutable';
+import * as Q from '../arithmetic/number';
+import _M from '../arithmetic/matrix';
 
-var I = require('immutable');
-var Q = require('../arithmetic/number');
-var M = require('../arithmetic/matrix')(Q, 0, 1);
+const M = _M(Q, 0, 1);
 
 
-var Edge = I.Record({
+const Edge = I.Record({
   head : undefined,
   tail : undefined,
   shift: undefined
@@ -19,17 +19,14 @@ Edge.prototype.reverse = function reverse() {
   return new Edge({
     head : this.tail,
     tail : this.head,
-    shift: this.shift.map(function(x) { return -x; })
+    shift: this.shift.map(x => -x)
   });
 };
 
-var _isNegative = function _isNegative(vec) {
-  var d = vec
-    .map(function(x) { return (x > 0) - (x < 0); })
-    .filter(function(x) { return x != 0; })
-    .first();
-  return d < 0;
-};
+
+const _sgn = x => (x > 0) - (x < 0);
+const _isNegative = vec => vec.map(_sgn).find(x => x != 0) < 0;
+
 
 Edge.prototype.canonical = function canonical() {
   if (this.tail < this.head || (this.tail == this.head
@@ -39,12 +36,12 @@ Edge.prototype.canonical = function canonical() {
     return this;
 };
 
-var _makeEdge = function _makeEdge(e) {
+const _makeEdge = function _makeEdge(e) {
   return new Edge({ head: e[0], tail: e[1], shift: I.List(e[2]) }).canonical();
 };
 
 
-var Graph = I.Record({
+const Graph = I.Record({
   dim  : undefined,
   edges: undefined
 });
@@ -54,65 +51,55 @@ Graph.prototype.toString = function toString() {
 };
 
 
-var make = function make(data) {
-  var edges = I.Set(data).map(_makeEdge);
+const make = function make(data) {
+  const edges = I.Set(data).map(_makeEdge);
   if (edges.size == 0)
     throw new Error('cannot be empty');
 
-  var dim = edges.first().shift.size;
-  if (edges.some(function(e) { return e.shift.size != dim; }))
+  const dim = edges.first().shift.size;
+  if (edges.some(e => e.shift.size != dim))
     throw new Error('must have consistent shift dimensions');
 
   return new Graph({ dim: dim, edges: edges });
 };
 
 
-var CoverVertex = I.Record({
+const CoverVertex = I.Record({
   v: undefined,
   s: undefined
 });
 
 
-var _target = function _target(e) {
-  return CoverVertex({ v: e.tail, s: e.shift });
-};
+const _target = e => CoverVertex({ v: e.tail, s: e.shift });
 
 
-var adjacencies = function adjacencies(graph) {
-  var res = I.Map();
+const adjacencies = function adjacencies(graph) {
+  let res = I.Map();
 
   graph.edges.forEach(function(e) {
     res = res
-      .update(e.head, function(a) {
-        return (a || I.List()).push(_target(e));
-      })
-      .update(e.tail, function(a) {
-        return (a || I.List()).push(_target(e.reverse()));
-      });
+      .update(e.head, a => (a || I.List()).push(_target(e)))
+      .update(e.tail, a => (a || I.List()).push(_target(e.reverse())));
   });
 
   return res;
 };
 
 
-var coordinationSeq = function coordinationSeq(graph, start, dist) {
-  var adj  = adjacencies(graph);
-  var zero = I.List(I.Repeat(0, graph.dim));
-  var plus = function(s, t) {
-    return I.Range(0, graph.dim).map(function(i) {
-      return s.get(i) + t.get(i);
-    });
-  };
+const coordinationSeq = function coordinationSeq(graph, start, dist) {
+  const adj  = adjacencies(graph);
+  const zero = I.List(I.Repeat(0, graph.dim));
+  const plus = (s, t) => I.Range(0, graph.dim).map(i => s.get(i) + t.get(i));
 
-  var oldShell = I.Set();
-  var thisShell = I.Set([CoverVertex({ v: start, s: zero })]);
-  var res = I.List([1]);
+  let oldShell = I.Set();
+  let thisShell = I.Set([CoverVertex({ v: start, s: zero })]);
+  let res = I.List([1]);
 
   I.Range(1, dist+1).forEach(function(i) {
-    var nextShell = I.Set();
+    let nextShell = I.Set();
     thisShell.forEach(function(v) {
       adj.get(v.v).forEach(function(t) {
-        var w = CoverVertex({ v: t.v, s: plus(v.s, t.s) });
+        const w = CoverVertex({ v: t.v, s: plus(v.s, t.s) });
         if (!oldShell.contains(w) && !thisShell.contains(w))
           nextShell = nextShell.add(w);
       });
@@ -127,18 +114,18 @@ var coordinationSeq = function coordinationSeq(graph, start, dist) {
 };
 
 
-var _isConnectedOrbitGraph = function _isConnectedOrbitGraph(graph) {
-  var adj   = adjacencies(graph);
-  var verts = I.List(adj.keySeq());
-  var start = verts.first();
-  var seen  = I.Set([start]);
-  var queue = I.List([start]);
+const _isConnectedOrbitGraph = function _isConnectedOrbitGraph(graph) {
+  const adj   = adjacencies(graph);
+  const verts = I.List(adj.keySeq());
+  const start = verts.first();
+  let seen  = I.Set([start]);
+  let queue = I.List([start]);
 
   while (!queue.isEmpty()) {
-    var v = queue.first();
+    const v = queue.first();
     queue = queue.shift();
     adj.get(v).forEach(function(t) {
-      var w = t.v;
+      const w = t.v;
       if (!seen.contains(w)) {
         seen = seen.add(w);
         queue = queue.push(w);
@@ -146,44 +133,39 @@ var _isConnectedOrbitGraph = function _isConnectedOrbitGraph(graph) {
     });
   }
 
-  return verts.every(function(v) { return seen.contains(v); });
+  return verts.every(v => seen.contains(v));
 };
 
 
-var _inc = function _inc(x) { return x + 1; };
-var _dec = function _dec(x) { return x - 1; };
+const _inc = x => x+1;
+const _dec = x => x-1;
 
-var _addToRow = function _addToRow(A, i, vec) {
-  vec.forEach(function(x, j) {
-    A = M.update(A, i, j, function(y) { return Q.plus(x, y); });
-  });
+
+const _addToRow = function _addToRow(A, i, vec) {
+  vec.forEach((x, j) => { A = M.update(A, i, j, y => Q.plus(x, y)); });
   return A;
 };
 
-var _getRow = function _getRow(A, i) {
-  return I.List(I.Range(0, A.ncols).map(function(j) { return M.get(A, i, j); }));
-};
+const _getRow = (A, i) => I.List(I.Range(0, A.ncols).map(j => M.get(A, i, j)));
 
 
-var barycentricPlacement = function barycentricPlacement(graph) {
+const barycentricPlacement = function barycentricPlacement(graph) {
   if (!_isConnectedOrbitGraph(graph))
     throw new Error('must have a connected orbit graph');
 
-  var adj   = adjacencies(graph);
-  var verts = I.List(adj.keySeq());
-  var vIdcs = I.Map(I.Range(0, verts.size).map(function(i) {
-    return [verts.get(i), i];
-  }));
+  const adj   = adjacencies(graph);
+  const verts = I.List(adj.keySeq());
+  const vIdcs = I.Map(I.Range(0, verts.size).map(i => [verts.get(i), i]));
 
-  var n = verts.size;
-  var d = graph.dim;
-  var A = M.constant(n+1, n);
-  var t = M.constant(n+1, d);
+  const n = verts.size;
+  const d = graph.dim;
+  let A = M.constant(n+1, n);
+  let t = M.constant(n+1, d);
 
   verts.forEach(function(v, i) {
     adj.get(v).forEach(function(c) {
       if (c.v != v) {
-        var j = vIdcs.get(c.v);
+        const j = vIdcs.get(c.v);
         A = M.update(A, i, j, _dec);
         A = M.update(A, i, i, _inc);
         t = _addToRow(t, i, c.s);
@@ -192,19 +174,14 @@ var barycentricPlacement = function barycentricPlacement(graph) {
   });
   A = M.set(A, n, 0, 1);
 
-  var p = M.solve(A, t);
+  const p = M.solve(A, t);
 
-  return I.Map(I.Range(0, n).map(function(i) {
-    return [verts.get(i), _getRow(p, i)];
-  }));
+  return I.Map(I.Range(0, n).map(i => [verts.get(i), _getRow(p, i)]));
 };
 
 
-var barycentricPlacementAsFloat = function barycentricPlacementAsFloat(graph) {
-  return barycentricPlacement(graph).map(function(p) {
-    return p.map(Q.toJS);
-  });
-};
+const barycentricPlacementAsFloat = graph =>
+  barycentricPlacement(graph).map(p => p.map(Q.toJS));
 
 
 module.exports = {
@@ -217,7 +194,7 @@ module.exports = {
 
 
 if (require.main == module) {
-  var test = function test(g) {
+  const test = function test(g) {
     console.log('g = '+g);
     console.log('  cs  = '+coordinationSeq(g, 1, 10));
     console.log('  pos = '+barycentricPlacement(g));
