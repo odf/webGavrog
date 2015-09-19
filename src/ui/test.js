2eb1b0c450f9c7c9486c26ac497a34e2e45b00f1
odf <olaf.delgado@gmail.com>
2015-09-19 19:17:52 +1000

    convert test.js to ES6

--- src/ui/test.js
+++ src/ui/test.js
@@ -1,47 +1,48 @@
-'use strict';
+import * as I     from 'immutable';
+import * as THREE from 'three';
+import * as React from 'react';
 
-var I     = require('immutable');
-var THREE = require('three');
-var React = require('react');
-var $     = React.DOM;
+import * as R from '../arithmetic/float';
+import _M     from '../arithmetic/matrix';
+import _V     from '../arithmetic/vector';
 
-var R         = require('../arithmetic/float');
-var M         = require('../arithmetic/matrix')(R, 0, 1);
-var V         = require('../arithmetic/vector')(R, 0);
-var delaney   = require('../dsymbols/delaney');
-var tiling    = require('../dsymbols/tilings');
-var periodic  = require('../pgraphs/periodic');
+const M = _M(R, 0, 1);
+const V = _V(R, 0);
 
-var Display3d = require('./Display3d');
+import * as delaney  from '../dsymbols/delaney';
+import * as periodic from '../pgraphs/periodic';
+import tiling        from '../dsymbols/tilings';
+
+import Display3d from './Display3d';
 
 
-var CoverVertex = I.Record({
+const CoverVertex = I.Record({
   v: undefined,
   s: undefined
 });
 
-var graphPortion = function graphPortion(graph, start, dist) {
-  var adj  = periodic.adjacencies(graph);
+const graphPortion = function graphPortion(graph, start, dist) {
+  const adj  = periodic.adjacencies(graph);
 
-  var v0 = new CoverVertex({ v: start, s: V.constant(graph.dim) });
-  var vertices = I.Map([[v0, 0]]);
-  var edges = I.Set();
-  var thisShell = I.List([v0]);
+  const v0 = new CoverVertex({ v: start, s: V.constant(graph.dim) });
+  let vertices = I.Map([[v0, 0]]);
+  let edges = I.Set();
+  let thisShell = I.List([v0]);
 
   I.Range(1, dist+1).forEach(function(i) {
-    var nextShell = I.Set();
+    let nextShell = I.Set();
     thisShell.forEach(function(v) {
-      var i = vertices.get(v);
+      const i = vertices.get(v);
 
       adj.get(v.v).forEach(function(t) {
-        var w = new CoverVertex({ v: t.v, s: V.plus(v.s, V.make(t.s)) });
+        const w = new CoverVertex({ v: t.v, s: V.plus(v.s, V.make(t.s)) });
 
         if (vertices.get(w) == null) {
           vertices = vertices.set(w, vertices.size);
           nextShell = nextShell.add(w);
         }
 
-        var j = vertices.get(w);
+        const j = vertices.get(w);
 
         if (!edges.contains(I.List([i, j])) && !edges.contains(I.List([j, i])))
           edges = edges.add(I.List([i, j]));
@@ -51,22 +52,20 @@ var graphPortion = function graphPortion(graph, start, dist) {
     thisShell = nextShell;
   });
 
-  var verts = I.List();
-  vertices.keySeq().forEach(function(v) {
-    verts = verts.set(vertices.get(v), v);
-  });
+  let verts = I.List();
+  vertices.keySeq().forEach(v => { verts = verts.set(vertices.get(v), v); });
 
   return {
     vertices: verts,
-    edges   : edges.map(function(e) { return e.toArray(); })
+    edges   : edges.map(e => e.toArray())
   };
 };
 
 
-var geometry = function geometry(vertices, faces) {
-  var geom = new THREE.Geometry();
+const geometry = function geometry(vertices, faces) {
+  const geom = new THREE.Geometry();
 
-  vertices.forEach(function(v) {
+  vertices.forEach(v => {
     geom.vertices.push(new THREE.Vector3(v[0], v[1], v[2]));
   });
 
@@ -82,50 +81,50 @@ var geometry = function geometry(vertices, faces) {
 };
 
 
-var stick = function stick(p, q, radius, segments) {
-  var n = segments;
-  var d = V.normalized(V.minus(q, p));
-  var ex = V.make([1,0,0]);
-  var ey = V.make([0,1,0]);
-  var t = V.dotProduct(d, ex) > 0.9 ? ey : ex;
-  var u = V.normalized(V.crossProduct(d, t));
-  var v = V.normalized(V.crossProduct(d, u));
-  var a = Math.PI * 2 / n;
+const stick = function stick(p, q, radius, segments) {
+  const n = segments;
+  const d = V.normalized(V.minus(q, p));
+  const ex = V.make([1,0,0]);
+  const ey = V.make([0,1,0]);
+  const t = V.dotProduct(d, ex) > 0.9 ? ey : ex;
+  const u = V.normalized(V.crossProduct(d, t));
+  const v = V.normalized(V.crossProduct(d, u));
+  const a = Math.PI * 2 / n;
 
-  var section = I.Range(0, n).map(function(i) {
-    var x = a * i;
-    var c = Math.cos(x) * radius;
-    var s = Math.sin(x) * radius;
+  const section = I.Range(0, n).map(function(i) {
+    const x = a * i;
+    const c = Math.cos(x) * radius;
+    const s = Math.sin(x) * radius;
     return V.plus(V.scaled(c, u), V.scaled(s, v));
   });
 
   return geometry(
-    I.List().concat(section.map(function(c) { return V.plus(c, p); }),
-                    section.map(function(c) { return V.plus(c, q); }))
-      .map(function(v) { return v.data.toJS(); }),
+    I.List().concat(section.map(c => V.plus(c, p)),
+                    section.map(c => V.plus(c, q)))
+      .map(v => v.data.toJS()),
     I.Range(0, n).map(function(i) {
-      var j = (i + 1) % n;
+      const j = (i + 1) % n;
       return [i, j, j+n, i+n];
     })
   );
 };
 
 
-var shrunk = function shrunk(f, vertices) {
-  var n = vertices.size;
-  var last = vertices.get(n-1);
-  return I.List(vertices.take(n-1).map(function(v) {
-    return V.plus(V.scaled(f, v), V.scaled(1-f, last));
-  })).push(last);
+const shrunk = function shrunk(f, vertices) {
+  const n = vertices.size;
+  const last = vertices.get(n-1);
+  return I.List(
+    vertices.take(n-1).map(v => V.plus(V.scaled(f, v), V.scaled(1-f, last)))
+  ).push(last);
 };
 
 
-var tetrahedra = function tetrahedron(vertexLists, material) {
-  var extract = function(v) { return v.data.toJS(); };
-  var model = new THREE.Object3D();
+const tetrahedra = function tetrahedron(vertexLists, material) {
+  const extract = v => v.data.toJS();
+  const model = new THREE.Object3D();
 
   vertexLists.forEach(function(vs) {
-    var geom = geometry(vs.map(extract),
+    const geom = geometry(vs.map(extract),
                         [[0,1,2],[1,0,3],[2,1,3],[0,2,3],
                          [0,2,1],[1,3,0],[2,3,1],[0,3,2]]);
     model.add(new THREE.Mesh(geom, material));
@@ -135,14 +134,14 @@ var tetrahedra = function tetrahedron(vertexLists, material) {
 };
 
 
-var ballAndStick = function ballAndStick(
+const ballAndStick = function ballAndStick(
   name, positions, edges, ballRadius, stickRadius, ballMaterial, stickMaterial)
 {
-  var model = new THREE.Object3D();
-  var ball  = new THREE.SphereGeometry(ballRadius, 16, 8);
+  const model = new THREE.Object3D();
+  const ball  = new THREE.SphereGeometry(ballRadius, 16, 8);
 
   positions.forEach(function(p) {
-    var s = new THREE.Mesh(ball, ballMaterial);
+    const s = new THREE.Mesh(ball, ballMaterial);
     s.position.x = p[0];
     s.position.y = p[1];
     s.position.z = p[2];
@@ -150,9 +149,9 @@ var ballAndStick = function ballAndStick(
   });
 
   edges.forEach(function(e) {
-    var u = V.make(positions[e[0]]);
-    var v = V.make(positions[e[1]]);
-    var s = stick(u, v, stickRadius, 8);
+    const u = V.make(positions[e[0]]);
+    const v = V.make(positions[e[1]]);
+    const s = stick(u, v, stickRadius, 8);
     s.computeVertexNormals();
     model.add(new THREE.Mesh(s, stickMaterial));
   });
@@ -161,8 +160,8 @@ var ballAndStick = function ballAndStick(
 };
 
 
-var light = function(color, x, y, z) {
-  var light = new THREE.PointLight(color);
+const light = function(color, x, y, z) {
+  const light = new THREE.PointLight(color);
 
   light.position.set(x, y, z);
 
@@ -170,39 +169,37 @@ var light = function(color, x, y, z) {
 };
 
 
-var apply = function(v, A) {
+const apply = function(v, A) {
   return V.make(M.times(M.make([v.data]), A).data.first());
 };
 
 
-var makeScene = function(model, camera) {
-  var scene  = new THREE.Scene();
+const makeScene = function() {
+  const scene  = new THREE.Scene();
 
-  var ballMaterial = new THREE.MeshPhongMaterial({
+  const ballMaterial = new THREE.MeshPhongMaterial({
     color    : 0xff0000,
     shininess: 50
   });
 
-  var stickMaterial = new THREE.MeshPhongMaterial({
+  const stickMaterial = new THREE.MeshPhongMaterial({
     color    : 0x0000ff,
     shininess: 50
   });
 
-  var ds  = delaney.parse('<1.1:2 3:2,1 2,1 2,2:6,3 2,6>');
-  var t   = tiling(ds);
-  var net = t.graph;
-  var g   = graphPortion(net, 0, 3);
-  var pos = t.positions;
-  var verts = g.vertices.map(function(v) {
-    var p = V.plus(pos.getIn([t.node2chamber.get(v.v), 0]), v.s);
+  const ds  = delaney.parse('<1.1:2 3:2,1 2,1 2,2:6,3 2,6>');
+  const t   = tiling(ds);
+  const net = t.graph;
+  const g   = graphPortion(net, 0, 3);
+  const pos = t.positions;
+  let verts = g.vertices.map(function(v) {
+    const p = V.plus(pos.getIn([t.node2chamber.get(v.v), 0]), v.s);
     return apply(p, t.basis).data.toJS();
   }).toArray();
   if (delaney.dim(ds) == 2)
-    verts = verts.map(function(p) {
-      return [p[0], p[1], 0];
-    });
+    verts = verts.map(p => [p[0], p[1], 0]);
 
-  var model = ballAndStick(
+  const model = ballAndStick(
     'cube',
     verts,
     g.edges,
@@ -212,16 +209,14 @@ var makeScene = function(model, camera) {
     stickMaterial
   );
 
-  var chambers = tetrahedra(
-    t.cover.elements().map(function(D) {
-      return shrunk(0.8, pos.get(D).valueSeq().map(function(p) {
-        return apply(p, t.basis);
-      }));
-    }),
-    ballMaterial);
+  const chambers = tetrahedra(
+    t.cover.elements()
+      .map(D => shrunk(0.8, pos.get(D).valueSeq().map(p => apply(p, t.basis)))),
+    ballMaterial
+  );
 
-  var distance = 18;
-  var camera = new THREE.PerspectiveCamera(25, 1, 0.1, 10000);
+  const distance = 18;
+  const camera = new THREE.PerspectiveCamera(25, 1, 0.1, 10000);
   camera.name = 'camera';
   camera.position.z = 5*distance;
 
@@ -236,12 +231,12 @@ var makeScene = function(model, camera) {
 };
 
 
-var App = React.createClass({
+const App = React.createClass({
   displayName: 'App',
 
-  getInitialState: function() {
-    var scene = makeScene();
-    var camera = scene.getObjectByName('camera');
+  getInitialState() {
+    const scene = makeScene();
+    const camera = scene.getObjectByName('camera');
     return {
       scene: scene,
       camera: camera,
@@ -249,24 +244,24 @@ var App = React.createClass({
     };
   },
 
-  handleResize: function(data) {
+  handleResize(data) {
     this.setState({
       width: window.innerWidth,
       height: window.innerHeight
     });
   },
 
-  componentDidMount: function() {
+  componentDidMount() {
     this.handleResize();
     window.addEventListener('resize', this.handleResize);
   },
 
-  componentWillUnmount: function() {
+  componentWillUnmount() {
     window.removeEventListener('resize', this.handleResize);
   },
 
-  render: function() {
-    return $.div(
+  render() {
+    return React.DOM.div(
       null,
       React.createElement(Display3d, {
         scene           : this.state.scene,
