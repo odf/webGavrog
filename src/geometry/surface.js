8c42b4758fab01619e141bdf5cdf90c69957946a
odf <olaf.delgado@gmail.com>
2015-09-27 18:28:16 +1000

    start implementation of subdivision surface

--- src/geometry/surface.js
+++ src/geometry/surface.js
@@ -7,20 +7,20 @@ const M = _M(F, 0, 1);
 const V = _V(F, 0);
 
 
-const faceNormal = function faceNormal(vs) {
-  vs = I.List(vs);
+const pairs = as => as.pop().zip(as.shift()).push([as.last(), as.first()]);
 
-  const es = vs.pop().zip(vs.shift()).push([vs.last(), vs.first()]);
-  const normal = es
+const corners = pos => idcs => idcs.map(i => pos.get(i));
+
+const centroid = pos => V.scaled(1/pos.size, pos.reduce(V.plus));
+
+
+const faceNormal = vs => V.normalized(
+  pairs(I.List(vs))
     .map(e => V.crossProduct(e[0], e[1]))
-    .reduce(V.plus);
+    .reduce(V.plus)
+);
 
-  return V.normalized(normal);
-};
-
-
-const faceNormals = (pos, faces) =>
-  faces.map(idcs => faceNormal(idcs.map(i => pos.get(i))));
+const faceNormals = (pos, faces) => faces.map(corners(pos)).map(faceNormal);
 
 
 const vertexNormals = function vertexNormals(pos, faces, faceNormals) {
@@ -32,6 +32,51 @@ const vertexNormals = function vertexNormals(pos, faces, faceNormals) {
   });
 
   return normals.map(V.normalized);
+};
+
+
+const subD = function subD({ pos, faces, isFixed }) {
+  const n        = pos.size;
+  const m        = faces.size;
+  const rPos     = pos.asMutable();
+  const rIsFixed = isFixed.asMutable();
+
+  faces.map(corners(pos)).map(centroid).forEach(v => rPos.push(v));
+  I.Repeat(false, faces.size).forEach(x => rIsFixed.push(x));
+
+  const rFaces    = I.List().asMutable();
+  const edgeIndex = I.Map().asMutable();
+
+  faces.forEach((vs, f) => {
+    const k = vs.size;
+    const edgePointIndices = [];
+
+    pairs(vs).forEach(([v, w]) => {
+      let e = edgeIndex.get(I.List([w, v]));
+      if (e == null) {
+        e = rPos.size;
+        rPos.push(V.scaled(0.5, V.plus(pos.get(v), pos.get(w))));
+        rIsFixed.push(isFixed.get(v) && isFixed.get(w));
+      }
+      edgeIndex.set(I.List([v, w]), e);
+      edgePointIndices.push(e);
+    });
+
+    I.Range(0, k)
+      .map(j => I.List([
+        n + f,
+        edgePointIndices[(j + k - 1) % k],
+        vs.get(j),
+        edgePointIndices[j]
+      ]))
+      .forEach(f => rFaces.push(f));
+  });
+
+  return {
+    pos    : rPos.asImmutable(),
+    faces  : rFaces.asImmutable(),
+    isFixed: rIsFixed.asImmutable()
+  };
 };
 
 
@@ -47,3 +92,9 @@ const normals = faceNormals(pos, faces);
 console.log(normals);
 
 console.log(vertexNormals(pos, faces, normals));
+
+console.log(subD({
+  pos,
+  faces,
+  isFixed: I.List(I.Repeat(true, 8))
+}));
