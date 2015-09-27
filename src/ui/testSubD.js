b76508bbe966ed1c68e46994c50eeffb75a32972
odf <olaf.delgado@gmail.com>
2015-09-27 19:38:36 +1000

    add a separate test for surface subdivision

---
+++ src/ui/testSubD.js
@@ -0,0 +1,121 @@
+import * as I     from 'immutable';
+import * as THREE from 'three';
+import * as React from 'react';
+
+import * as surface from '../geometry/surface';
+
+import Display3d from './Display3d';
+
+
+const geometry = function geometry(vertices, faces) {
+  const geom = new THREE.Geometry();
+
+  vertices.forEach(v => {
+    geom.vertices.push(new THREE.Vector3(v[0], v[1], v[2]));
+  });
+
+  faces.forEach(function(f) {
+    f.forEach(function(v, i) {
+      if (i > 0 && i+1 < f.length)
+        geom.faces.push(new THREE.Face3(f[0], f[i], f[i+1]));
+    });
+  });
+
+  geom.computeFaceNormals();
+  return geom;
+};
+
+
+const model = material => {
+  const pos = [[0,0,0], [0,0,1], [0,1,0], [0,1,1],
+               [1,0,0], [1,0,1], [1,1,0], [1,1,1]];
+
+  const faces = [[0,1,3,2],[5,4,6,7],
+                 [1,0,4,5],[2,3,7,6],
+                 [0,2,6,4],[3,1,5,7]];
+
+  return new THREE.Mesh(geometry(pos, faces), material);
+};
+
+
+const light = function(color, x, y, z) {
+  const light = new THREE.PointLight(color);
+
+  light.position.set(x, y, z);
+
+  return light;
+};
+
+
+const apply = function(v, A) {
+  return V.make(M.times(M.make([v.data]), A).data.first());
+};
+
+
+const makeScene = function() {
+  const scene  = new THREE.Scene();
+
+  const material = new THREE.MeshPhongMaterial({
+    color    : 0x0000ff,
+    shininess: 50
+  });
+
+  const distance = 18;
+  const camera = new THREE.PerspectiveCamera(25, 1, 0.1, 10000);
+  camera.name = 'camera';
+  camera.position.z = 5*distance;
+
+  camera.add(light(0xffffff,  3*distance,  5*distance, distance));
+  camera.add(light(0x666666, -5*distance, -5*distance, distance));
+
+  scene.add(model(material));
+  scene.add(camera);
+
+  return scene;
+};
+
+
+const App = React.createClass({
+  displayName: 'App',
+
+  getInitialState() {
+    const scene = makeScene();
+    const camera = scene.getObjectByName('camera');
+    return {
+      scene: scene,
+      camera: camera,
+      cameraParameters: { distance: camera.position.z }
+    };
+  },
+
+  handleResize(data) {
+    this.setState({
+      width: window.innerWidth,
+      height: window.innerHeight
+    });
+  },
+
+  componentDidMount() {
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+  },
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  },
+
+  render() {
+    return React.DOM.div(
+      null,
+      React.createElement(Display3d, {
+        scene           : this.state.scene,
+        camera          : this.state.camera,
+        cameraParameters: this.state.cameraParameters,
+        width           : this.state.width - 20,
+        height          : this.state.height - 20
+      }));
+  }
+});
+
+
+React.render(React.createElement(App), document.getElementById('react-main'));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path    = require('path');
+var basedir = __dirname;
+
+
+module.exports = [ "test", "testSubD" ].map(function(name) {
+  return {
+    context: path.join(basedir, "src"),
+    entry: [ "babel-core/polyfill", "./ui/"+name+".js" ],
+    output: {
+      path: path.join(basedir, "public", "js"),
+      filename: name+".js"
+    },
+    module: {
+      loaders: [
+        { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+        { test: /\.json$/, loader: "json" }
+      ]
+    },
+    resolve: {
+      extensions: [ "", ".js", ".jsx", ".json" ]
+    }
+  };
+});
