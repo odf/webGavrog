4e2c38dc087206c0d3677177891b938d79fdf2d2
odf <olaf.delgado@gmail.com>
2015-10-16 19:26:07 +1100

    add a timer function

--- src/common/util.js
+++ src/common/util.js
@@ -12,3 +12,14 @@ export function cmpLex(cmp = (x, y) => x < y ? -1 : x > y ? 1 : 0) {
     return a.size - b.size;
   };
 };
+
+
+export function timer() {
+  let _last = performance.now();
+
+  return () => {
+    const previous = _last;
+    _last = performance.now();
+    return Math.round(_last - previous);
+  };
+};
