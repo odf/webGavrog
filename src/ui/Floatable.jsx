002a55c245e2485123925ca4952f3cbd1b4da185
odf <olaf.delgado@gmail.com>
2015-10-22 18:26:45 +1100

    make Floatable components draggable

--- src/ui/Floatable.jsx
+++ src/ui/Floatable.jsx
@@ -1,9 +1,53 @@
 import * as React from 'react';
 
 export default React.createClass({
+  getInitialState() {
+    return {
+      posX: 10,
+      posY: 10
+    };
+  },
+
+  handleMouseDown(event) {
+    event.preventDefault();
+
+    document.addEventListener('mousemove', this.handleMouseMove, false);
+    document.addEventListener('mouseup'  , this.handleMouseUp, false);
+
+    this.setState({
+      mouseDown: true,
+      offsetX: this.state.posX - event.clientX,
+      offsetY: this.state.posY - event.clientY
+    });
+  },
+
+  handleMouseMove(event) {
+    event.preventDefault();
+
+    this.setState({
+      posX: event.clientX + this.state.offsetX,
+      posY: event.clientY + this.state.offsetY
+    });
+  },
+
+  handleMouseUp(event) {
+    event.preventDefault();
+
+    document.removeEventListener('mousemove', this.handleMouseMove);
+    document.removeEventListener('mouseup'  , this.handleMouseUp);
+
+    this.setState({
+      mouseDown: false
+    });
+  },
+
   render() {
     return (
-      <div className="floatable">
+      <div className="floatable"
+           style={{ left  : `${this.state.posX}px`,
+                    top   : `${this.state.posY}px`,
+                    cursor: this.state.mouseDown ? 'grabbing' : 'grab' }}
+           onMouseDown={this.handleMouseDown}>
         {this.props.children}
       </div>
     );
