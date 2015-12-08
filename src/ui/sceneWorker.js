f325697ad82c60c0480517dc72e325246dee14c6
odf <olaf.delgado@gmail.com>
2015-12-08 20:39:26 +1100

    add some timings to the surface processing code

--- src/ui/sceneWorker.js
+++ src/ui/sceneWorker.js
@@ -11,14 +11,15 @@ import * as delaney3d from '../dsymbols/delaney3d';
 import * as util      from '../common/util';
 
 
-const processedSolid = t0 => {
+const processedSolid = (t0, timers) => {
+  timers && timers.switchTo('adding flattened center faces');
   const t1 = surface.withFlattenedCenterFaces(t0);
+  timers && timers.switchTo('subdividing the faces');
   const t2 = I.Range(0, 2).reduce(s => surface.subD(s), t1);
+  timers && timers.switchTo('insetting');
   const t3 = surface.insetAt(t2, 0.03, t2.isFixed);
+  timers && timers.switchTo('beveling');
   const t4 = surface.beveledAt(t3, 0.015, t2.isFixed);
-
-  const isCorner = I.Range(0, t4.pos.size).map(i => i >= t3.pos.size);
-  const t5 = surface.beveledAt(t4, 0.005, isCorner);
 
   return t4;
 };
@@ -32,9 +33,14 @@ const handlers = {
       isFixed: I.fromJS(isFixed)
     };
 
-    const t = util.timer();
-    const surfOut = processedSolid(surfIn);
-    console.log(`${Math.round(t())} msec in total to process the surfaces`);
+    const timer = util.timer();
+    const timers = util.timers();
+
+    const surfOut = processedSolid(surfIn, timers);
+
+    console.log(`${Math.round(timer())} msec in total to process the surfaces`);
+    console.log(`  surface processing details:`);
+    console.log(`${JSON.stringify(timers.current(), null, 2)}`);
 
     return {
       pos    : surfOut.pos.map(v => v.data).toJS(),
