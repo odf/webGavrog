bb825d3e25d8157df864d17f8bc90ebe191c579a
odf <olaf.delgado@gmail.com>
2016-02-06 20:47:37 +1100

    add methods for floating point and mixed fp and rational operands

---
+++ src/arithmeticNew/reals.js
@@ -0,0 +1,52 @@
+const a = require('./base').default()
+
+
+const { methods: intMethods } = require('./integers').default();
+a.register(intMethods);
+
+
+const { methods: ratMethods } = require('./fractions').default(
+  a.ops(), ['Integer', 'LongInt'], 'Fraction'
+);
+a.register(ratMethods);
+
+
+const ratOps = a.ops();
+
+const realMethods = {
+  toJS    : [ { argtypes: ['Float'], method: x => x  } ],
+  negative: [ { argtypes: ['Float'], method: x => -x } ],
+  abs     : [ { argtypes: ['Float'], method: x => Math.abs(x) } ],
+  sgn     : [ { argtypes: ['Float'], method: x => (x > 0) - (x < 0) } ]
+};
+
+for (const [op, name] of [
+  [(x, y) => (x > y) - (x < y), 'cmp'  ],
+  [(x, y) => x + y            , 'plus' ],
+  [(x, y) => x - y            , 'minus'],
+  [(x, y) => x * y            , 'times'],
+  [(x, y) => x / y            , 'div'  ]
+]) {
+  realMethods[name] = [ { argtypes: ['Float', 'Float'], method: op } ];
+
+  for (const ratType of ['Integer', 'LongInt', 'Fraction']) {
+    realMethods[name].push({
+      argtypes: ['Float', ratType], method: (x, y) => op(x, ratOps.toJS(y))
+    });
+    realMethods[name].push({
+      argtypes: [ratType, 'Float'], method: (x, y) => op(ratOps.toJS(x), y)
+    });
+  }
+}
+
+a.register(realMethods);
+
+const ops = a.ops();
+
+export default ops;
+
+
+if (require.main == module) {
+  console.log(`${ops.div(2,3)}`);
+  console.log(`${ops.plus(ops.div(2,3), 0.1)}`);
+}
