40df7ee7aa82cd3da38dcf1f6e952defc1dd9a65
odf <olaf.delgado@gmail.com>
2016-02-07 18:10:13 +1100

    add default operator implementations

--- src/arithmeticNew/base.js
+++ src/arithmeticNew/base.js
@@ -13,17 +13,28 @@ export const typeOf = x => {
 };
 
 
+const defaults = {
+  isZero       : [{ method: (x, ops) => ops.sgn(x) == 0 }],
+  isPositive   : [{ method: (x, ops) => ops.sgn(x) >  0 }],
+  isNonNegative: [{ method: (x, ops) => ops.sgn(x) >= 0 }],
+  isNegative   : [{ method: (x, ops) => ops.sgn(x) <  0 }],
+  isNonPositive: [{ method: (x, ops) => ops.sgn(x) <= 0 }],
+
+  mod: [{ method: (x, y, ops) => ops.minus(x, ops.times(ops.idiv(x, y), y)) }]
+};
+
+
 export default function arithmetic() {
   const _registry = I.Map().asMutable();
 
-  const _call = op => {
+  const _call = (op, ops) => {
     const dispatch = _registry.get(op);
 
     return (...args) => {
-      const method = dispatch.getIn(args.map(typeOf));
+      const method = dispatch.getIn(args.map(typeOf)) || dispatch.get()
 
       if (method)
-        return method(...args);
+        return method(...args, ops);
       else {
         const msg = `Operator '${op}' not defined on [${args.map(typeOf)}]`;
         throw new Error(msg);
@@ -31,11 +42,11 @@ export default function arithmetic() {
     };
   };
 
-  return {
+  const result = {
     register(specs) {
       for (const op in specs) {
         for (const {argtypes, method} of specs[op]) {
-          _registry.setIn([op].concat(argtypes), method);
+          _registry.setIn([op].concat(argtypes || [undefined]), method);
         }
       }
       return this;
@@ -43,10 +54,12 @@ export default function arithmetic() {
 
     ops() {
       const result = {};
-      _registry.keySeq().forEach(op => { result[op] = _call(op); });
+      _registry.keySeq().forEach(op => { result[op] = _call(op, result); });
       return result;
     }
   };
+
+  return result.register(defaults);
 };
 
 
@@ -55,11 +68,13 @@ if (require.main == module) {
     .register({
       add: [
         { argtypes: ['Integer', 'Integer'], method: (a, b) => a + b },
-        { argtypes: ['Integer', 'String' ], method: (n, s) => `${n}+"${s}"` }
+        { argtypes: ['Integer', 'String' ], method: (n, s) => `${n}+"${s}"` },
+        { method: (x, y) => `${x} plus ${y}` }
       ]
     })
     .ops();
 
   console.log(`add(3, 4) = ${ops.add(3, 4)}`);
   console.log(`add(5, "Olaf") = ${ops.add(5, "Olaf")}`);
+  console.log(`add("Olaf", "Delgado") = ${ops.add("Olaf", "Delgado")}`);
 }
