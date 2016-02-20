634b6062b5eac69866523afa1375df5121532d25
odf <olaf.delgado@gmail.com>
2016-02-20 13:17:47 +1100

    convert tilings.js to new arithmetic

--- src/dsymbols/tilings.js
+++ src/dsymbols/tilings.js
@@ -1,11 +1,4 @@
 import * as I from 'immutable';
-import * as F from '../arithmetic/float';
-
-import _M from '../arithmetic/matrix';
-import _V from '../arithmetic/vector';
-
-const M = _M(F, 0, 1);
-const V = _V(F, 0);
 
 import * as cosets      from '../fpgroups/cosets';
 import * as delaney     from './delaney';
@@ -16,6 +9,13 @@ import * as fundamental from './fundamental';
 import * as covers      from './covers';
 import * as periodic    from '../pgraphs/periodic';
 
+import { floatMatrices } from '../arithmeticNew/types';
+const ops = floatMatrices;
+
+
+const _array = (len, val = 0) => Array(len).fill(val);
+const _identity = n => _array(n).map((_, i) => _array(n).fill(1, i, i+1));
+
 
 const _remainingIndices = (ds, i) => ds.indices().filter(j => j != i);
 
@@ -23,12 +23,12 @@ const _remainingIndices = (ds, i) => ds.indices().filter(j => j != i);
 const _edgeTranslations = function _edgeTranslations(cov) {
   const fg  = fundamental.fundamentalGroup(cov);
   const n   = fg.nrGenerators;
-  const nul = M.nullSpace(M.make(cosets.relatorMatrix(n, fg.relators)));
+  const nul = ops.nullSpace(cosets.relatorMatrix(n, fg.relators).toJS());
 
   return fg.edge2word.map(function(a) {
     return a.map(function(b) {
-      const v = M.make([cosets.relatorAsVector(b, n)]);
-      return V.make(M.times(v, nul).data[0]);
+      const v = cosets.relatorAsVector(b, n).toJS();
+      return ops.times(v, nul)[0];
     });
   });
 };
@@ -36,7 +36,7 @@ const _edgeTranslations = function _edgeTranslations(cov) {
 
 const _cornerShifts = function _cornerShifts(cov, e2t) {
   const dim = delaney.dim(cov);
-  const zero = V.constant(dim);
+  const zero = _array(dim);
 
   return I.Map().withMutations(function(m) {
     cov.indices().forEach(function(i) {
@@ -50,7 +50,8 @@ const _cornerShifts = function _cornerShifts(cov, e2t) {
         if (k == properties.traversal.root)
           m.setIn([D, i], zero);
         else
-          m.setIn([D, i], V.minus(m.getIn([Dk, i]), e2t.getIn([Dk, k]) || zero));
+          m.setIn([D, i],
+                  ops.minus(m.getIn([Dk, i]), e2t.getIn([Dk, k]) || zero));
       });
     });
   });
@@ -59,7 +60,7 @@ const _cornerShifts = function _cornerShifts(cov, e2t) {
 
 const _skeleton = function _skeleton(cov, e2t, c2s) {
   const dim = delaney.dim(cov);
-  const zero = V.constant(dim);
+  const zero = _array(dim);
   const chambers = cov.elements();
   const idcs0 = _remainingIndices(cov, 0);
   const nodeReps = properties.orbitReps(cov, idcs0, chambers);
@@ -77,9 +78,9 @@ const _skeleton = function _skeleton(cov, e2t, c2s) {
       const t = e2t.getIn([D, 0]) || zero;
       const sD = c2s.getIn([D, 0]);
       const sE = c2s.getIn([E, 0]);
-      const s = V.minus(V.plus(t, sE), sD);
+      const s = ops.minus(ops.plus(t, sE), sD);
 
-      return [v, w, s.data];
+      return [v, w, s];
     });
 
   return {
@@ -95,25 +96,25 @@ const _chamberPositions = function _chamberPositions(cov, e2t, c2s, skel, pos) {
   let result = I.Map();
 
   cov.elements().forEach(function(D) {
-    const p = pos.get(skel.chamber2node.get(D));
+    const p = pos.get(skel.chamber2node.get(D)).toJS();
     const t = c2s.getIn([D, 0]);
-    result = result.setIn([D, 0], V.plus(V.make(p), t));
+    result = result.setIn([D, 0], ops.plus(p, t));
   });
 
   I.Range(1, dim+1).forEach(function(i) {
     const idcs = I.Range(0, i);
     properties.orbitReps(cov, idcs, cov.elements()).forEach(function(D) {
       const orb = properties.orbit(cov, idcs, D);
-      let s = V.constant(dim);
+      let s = _array(dim);
       orb.forEach(function(E) {
         const p = result.getIn([E, 0]);
         const t = c2s.getIn([E, i]);
-        s = V.plus(s, V.minus(p, t));
+        s = ops.plus(s, ops.minus(p, t));
       });
-      s = V.scaled(F.div(1, orb.size), s);
+      s = ops.times(ops.div(1, orb.size), s);
       orb.forEach(function(E) {
         const t = c2s.getIn([E, i]);
-        result = result.setIn([E, i], V.plus(s, t));
+        result = result.setIn([E, i], ops.plus(s, t));
       });
    });
   });
@@ -123,8 +124,8 @@ const _chamberPositions = function _chamberPositions(cov, e2t, c2s, skel, pos) {
 
 
 const _chamberBasis = function _chamberBasis(pos, D) {
-  const t = pos.get(D).valueSeq();
-  return M.make(I.Range(1, t.size).map(i => V.minus(t.get(i), t.get(0)).data));
+  const t = pos.get(D).valueSeq().toArray();
+  return t.slice(1).map(v => ops.minus(v, t[0]));
 };
 
 
@@ -133,50 +134,46 @@ const _symmetries = function _symmetries(ds, cov, pos) {
   const m = delaney.size(cov) / n;
 
   const D = ds.elements()
-    .find(D => F.sgn(M.determinant(_chamberBasis(pos, D))) != 0);
-  const A = M.inverse(_chamberBasis(pos, D));
+    .find(D => ops.sgn(ops.determinant(_chamberBasis(pos, D))) != 0);
+  const A = ops.inverse(_chamberBasis(pos, D));
 
-  return I.Range(0, m).map(i => M.times(A, _chamberBasis(pos, D + i*n)));
+  return I.Range(0, m).map(i => ops.times(A, _chamberBasis(pos, D + i*n)));
 };
 
 
 const _resymmetrizedGramMatrix = function _resymmetrizedGramMatrix(G, syms) {
-  let A = M.scaled(0, G);
+  let A = ops.times(0, G);
 
   syms.forEach(S => {
-    A = M.plus(A, M.times(S, M.times(G, M.transposed(S))));
+    A = ops.plus(A, ops.times(S, ops.times(G, ops.transposed(S))));
   });
 
-  A = M.scaled(F.div(1, syms.size), A);
+  A = ops.times(ops.div(1, syms.size), A);
 
   return A;
 };
 
 
-const _scalarProduct = function _scalarProduct(v, w, G) {
-  const A = M.times(M.make([v.data]),
-                    M.times(G, M.transposed(M.make([w.data]))));
-  return M.get(A, 0, 0);
-};
+const _scalarProduct = (v, w, G) => ops.times(ops.times(v, G)[0], w)[0][0];
 
 
 const _orthonormalBasis = function _orthonormalBasis(G) {
-  const n = G.nrows;
-  let e = I.List(M.identity(n).data.map(V.make));
+  const [n, m] = ops.shape(G);
+  let e = _identity(n);
 
   I.Range(0, n).forEach(function(i) {
-    let v = e.get(i);
+    let v = e[i];
     I.Range(0, i).forEach(function(j) {
-      const w = e.get(j);
+      const w = e[j];
       const f = _scalarProduct(v, w, G);
-      v = V.minus(v, V.scaled(f, w));
+      v = ops.minus(v, ops.times(f, w));
     });
     const d = _scalarProduct(v, v, G);
-    v = V.scaled(1/Math.sqrt(d), v);
-    e = e.set(i, v);
+    v = ops.times(1/Math.sqrt(d), v);
+    e[i] = v;
   });
 
-  return M.make(e.map(v => v.data));
+  return e;
 };
 
 
@@ -195,8 +192,8 @@ export default function tiling(ds, cover) {
   const pos  = _chamberPositions(cov, e2t, c2s, skel, vpos);
   const syms = _symmetries(ds, cov, pos);
 
-  const G = _resymmetrizedGramMatrix(M.identity(delaney.dim(ds)), syms);
-  const basis = M.inverse(_orthonormalBasis(G));
+  const G = _resymmetrizedGramMatrix(_identity(delaney.dim(ds)), syms);
+  const basis = ops.inverse(_orthonormalBasis(G));
 
   return {
     cover       : cov,
@@ -212,6 +209,10 @@ export default function tiling(ds, cover) {
 
 
 if (require.main == module) {
+  Array.prototype.toString = function() {
+    return '[ ' + this.map(x => x.toString()).join(', ') + ' ]';
+  };
+
   const test = function test(ds) {
     console.log('ds = '+ds);
     console.log(tiling(ds));
