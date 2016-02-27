3404902f605e317de9ef1a2e36d5ceda33ae69f6
odf <olaf.delgado@gmail.com>
2016-02-27 21:10:29 +1100

    switch to new CGD parser

--- src/io/cgd.js
+++ src/io/cgd.js
@@ -1,3 +1,5 @@
+const parser = require('./cgdParser');
+
 import * as pg from '../pgraphs/periodic';
 
 
@@ -34,113 +36,6 @@ const translation = {
 };
 
 
-const splitLine = (s, lineNr) => {
-  const fields = [];
-  let inString = false;
-  let fieldStart = null;
-
-  for (let i = 0; i < s.length; ++i) {
-    const c = s[i];
-
-    if (c == '#')
-      break;
-    else if (c.match(/\s/)) {
-      if (fieldStart != null && !inString) {
-        fields.push(s.slice(fieldStart, i));
-        fieldStart = null;
-      }
-    }
-    else if (fieldStart == null)
-      fieldStart = i;
-    else if (!inString && s[fieldStart] == '"')
-      throw new Error(`whitespace missing after string at line ${lineNr}`);
-
-    if (c == '"')
-      inString = !inString;
-  }
-
-  if (inString)
-    throw new Error(`closing quotes missing at line ${lineNr}`);
-  else if (fieldStart != null)
-    fields.push(s.slice(fieldStart));
-
-  return fields;
-};
-
-
-const rawBlocks = function*(lines) {
-  let i = 0;
-  let current = null;
-
-  for (const line of lines) {
-    const lineNr = ++i;
-    const fields = splitLine(line, lineNr);
-    if (fields.length == 0)
-      continue;
-
-    if (fields[0].toLowerCase() == 'end') {
-      if (current == null)
-        throw new Error(`no active block to be closed at line ${lineNr}`);
-      yield current;
-      current = null;
-    }
-    else {
-      if (current == null)
-        current = [];
-
-      current.push({ lineNr, fields });
-    }
-  }
-
-  if (current != null)
-    throw new Error('file ends within a block');
-};
-
-
-const parseField = s => {
-  if (s[0] == '"')
-    return s.slice(1, -1);
-  else if (s.match(/^[\d+-.]/)) {
-    const parts = s.split('/');
-    if (parts.length > 2)
-      return s;
-    else if (parts.length == 2)
-      return { n: parseFloat(parts[0]), d: parseFloat(parts[1]) };
-    else
-      return parseFloat(s);
-  }
-  else
-    return s;
-};
-
-
-const preprocessBlock = block => {
-  const content = [];
-  let key = null;
-
-  for (let i = 1; i < block.length; ++i) {
-    const { lineNr, fields } = block[i];
-    const args = fields.map(parseField);
-
-    if (fields[0].match(/^[a-z]/i)) {
-      key = fields[0].toLowerCase();
-      if (translation[key] != null)
-        key = translation[key];
-      args.shift();
-    }
-
-    if (args.length)
-      content.push({ lineNr, key, args });
-  }
-
-  return {
-    lineNr: block[0].lineNr,
-    type  : block[0].fields[0].toLowerCase(),
-    content
-  };
-};
-
-
 const unknown = data => {
   return {
     content: data,
@@ -156,12 +51,12 @@ const processPeriodicGraphData = data => {
   let dim = null;
   let name = null;
 
-  for (const { lineNr, key, args } of data.content) {
+  for (const { key, args } of data.content) {
     if (key == 'name') {
       if (name != null)
-        warnings.push("Multiple names at line #{lineNr}");
+        warnings.push("Multiple names");
       else if (args.length == 0)
-        warnings.push("Empty name at line #{lineNr}");
+        warnings.push("Empty name");
       else
         name = args.join(' ');
     }
@@ -169,23 +64,23 @@ const processPeriodicGraphData = data => {
       let [v, w, ...shift] = args;
 
       if (w == null)
-        errors.push("Incomplete edge specification at line #{lineNr}");
+        errors.push("Incomplete edge specification");
       else {
         if (shift.length == 0 && dim != null) {
-          warnings.push("Missing shift vector at line #{lineNr}");
+          warnings.push("Missing shift vector");
           shift = new Array(dim).fill(0);
         }
 
         if (dim == null)
           dim = shift.length
         else if (shift.length != dim)
-          errors.push("Inconsistent shift dimensions at line #{lineNr}");
+          errors.push("Inconsistent shift dimensions");
 
         edges.push([v, w, shift]);
       }
     }
     else
-      warnings.push("Unknown keyword '#{key}' at line #{lineNr}");
+      warnings.push("Unknown keyword '#{key}'");
   }
 
   return { name, warnings, errors, graph: pg.make(edges) };
@@ -197,8 +92,41 @@ const makeStructure = {
 };
 
 
-export function* structures(lines) {
-  for (const b of rawBlocks(lines)) {
+const preprocessBlock = ({ type, content }) => ({
+  type,
+  content: content.map(({ key, args }) => ({
+    key: translation[key] || key,
+    args
+  }))
+});
+
+
+export function* structures(text) {
+  let blocks;
+
+  try {
+    blocks = parser.parse(text);
+  } catch(ex) {
+    if (ex.location) {
+      var n = ex.location.start.line - 1;
+      var m = ex.location.start.column || 0;
+      var lines = txt.split('\n');
+      var pre  = lines.slice(Math.max(n-5, 0), n);
+      var line = lines[n];
+      var post = lines.slice(n+1, n+6);
+      console.error(ex.message);
+      console.error('(line '+(n+1)+', column '+m+')\n');
+      if (pre.length > 0)
+        console.error('  ' + pre.join('\n  '));
+      console.error('* ' + line);
+      console.error('  ' + Array(m).join(' ') + '^');
+      console.error('  ' + post.join('\n  '));
+    }
+    else
+      console.error(ex);
+  }
+
+  for (const b of blocks) {
     const data = preprocessBlock(b);
     yield (makeStructure[data.type] || unknown)(data);
   }
@@ -207,28 +135,11 @@ export function* structures(lines) {
 
 if (require.main == module) {
   const fs = require('fs');
-  let lineNr = 0;
-
-  const test = s => {
-    try {
-      console.log(splitLine(s, ++lineNr).map(parseField));
-    } catch(ex) {
-      console.log(ex);
-    }
-  }
-
-  test('mutus nomen dedit cocis');
-  test('mutus nomen "dedit" cocis  ');
-  test('   mutus nomen "ded it" cocis 123 # done! ');
-  test('  # comment!');
-  test('mutus nomen "ded"it cocis');
-  test('mutus nomen "dedit cocis');
-  test('+1/3 -1.23e3 +15');
 
   process.argv.slice(2).forEach(file => {
-    const txt = fs.readFileSync(file, { encoding: 'utf8' });
-    const lines = txt.split(/\r?\n/);
-    for (const b of structures(lines))
+    const text = fs.readFileSync(file, { encoding: 'utf8' });
+
+    for (const b of structures(text))
       console.log(JSON.stringify(b, null, 2));
   });
 }
