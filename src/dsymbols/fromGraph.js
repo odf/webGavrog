c3fac022ea053b46eb72809e5b4a0137161648a7
odf <olaf.delgado@gmail.com>
2016-04-16 15:53:30 +1000

    read the graph data from a file

--- src/dsymbols/fromGraph.js
+++ src/dsymbols/fromGraph.js
@@ -1,7 +1,6 @@
 import * as I  from 'immutable';
 
-import { build }   from './delaney';
-import { minimal } from './derived';
+import { build } from './delaney';
 
 
 export function fromCyclicAdjencencies(adjs) {
@@ -53,48 +52,34 @@ export function fromCyclicAdjencencies(adjs) {
 
 
 if (require.main == module) {
-  const test = adjs => {
-    const ds = fromCyclicAdjencencies(adjs);
-    console.log(`${ds}`);
-    console.log(`${minimal(ds)}`);
+  const fs = require('fs');
+
+  const { minimal } = require('./derived');
+
+  Array.prototype.toString = function() {
+    return '[ ' + this.map(x => x.toString()).join(', ') + ' ]';
   };
 
+  process.argv.slice(2).forEach(file => {
+    console.log();
+    console.log();
+    console.log(file);
 
-  // Regular tetrahedron:
+    const text = fs.readFileSync(file, { encoding: 'utf8' });
+    const adjs = text
+      .split(/\n+/)
+      .filter(s => s.trim().length > 0)
+      .map(s => s.trim().split(/\s+/).map(f => parseInt(f)))
+      .map(a => [a[0], a.slice(1)]);
 
-  test([
-    [1, [2, 3, 4]],
-    [2, [1, 4, 3]],
-    [3, [1, 2, 4]],
-    [4, [1, 3, 2]]
-  ]);
+    console.log();
+    console.log(adjs);
 
-  // Klein graph (according to S. Hyde):
+    const ds = fromCyclicAdjencencies(adjs);
 
-  test([
-    [ 0, [ 1,  2,  3,  4,  5,  6,  7]],
-    [ 1, [ 0,  7, 21, 18, 15,  8,  2]],
-    [ 2, [ 0,  1,  8,  9, 10, 11,  3]],
-    [ 3, [ 0,  2, 11, 12, 13, 14,  4]],
-    [ 4, [ 0,  3, 14, 15, 16, 17,  5]],
-    [ 5, [ 0,  4, 17, 10, 18, 19,  6]],
-    [ 6, [ 0,  5, 19, 13,  9, 20,  7]],
-    [ 7, [ 0,  6, 20, 16, 12, 21,  1]],
-    [ 8, [ 1, 15, 14, 23, 20,  9,  2]],
-    [ 9, [ 2,  8, 20,  6, 13, 22, 10]],
-    [10, [ 2,  9, 22, 18,  5, 17, 11]],
-    [11, [ 2, 10, 17, 23, 21, 12,  3]],
-    [12, [ 3, 11, 21,  7, 16, 22, 13]],
-    [13, [ 3, 12, 22,  9,  6, 19, 14]],
-    [14, [ 3, 13, 19, 23,  8, 15,  4]],
-    [15, [ 4, 14,  8,  1, 18, 22, 16]],
-    [16, [ 4, 15, 22, 12,  7, 20, 17]],
-    [17, [ 4, 16, 20, 23, 11, 10,  5]],
-    [18, [ 5, 10, 22, 15,  1, 21, 19]],
-    [19, [ 5, 18, 21, 23, 14, 13,  6]],
-    [20, [ 6,  9,  8, 23, 17, 16,  7]],
-    [21, [ 1,  7, 12, 11, 23, 19, 18]],
-    [22, [ 9, 13, 12, 16, 15, 18, 10]],
-    [23, [ 8, 14, 19, 21, 11, 17, 20]]
-  ]);
+    console.log();
+    console.log(`${ds}`);
+    console.log();
+    console.log(`${minimal(ds)}`);
+  });
 }
