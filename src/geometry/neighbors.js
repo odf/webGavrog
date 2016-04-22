a34e376cd13efa0f4f3dbc6a8bfb7a84fbb54774
odf <olaf.delgado@gmail.com>
2016-04-22 20:40:45 +1000

    complete edge induction code

--- src/geometry/neighbors.js
+++ src/geometry/neighbors.js
@@ -13,24 +13,46 @@ const withClosestDistances = (pos, points, dot, limit) =>
 
 const withRelevantDistances = (p, points, dot) =>
   withClosestDistances(p.pos, points, dot, p.degree+1)
-  .filter(q => q.point.index != p.index)
+  .filter(q => q.point.id != p.id)
   .slice(0, p.degree)
   .map(q => ({ base: p, neighbor: q.point, dist: q.dist }));
 
-const allDistances = (points, dot = ops.times) => {
+const allDistances = (points, dot) => {
   const tmp = points.map(p => withRelevantDistances(p, points, dot));
   return sortBy([].concat(...tmp), 'dist');
 };
 
 
+export function inducedEdges(points, dot = ops.times) {
+  const edges = [];
+  const neighbors = {};
+  points.forEach(p => neighbors[p.id] = []);
+
+  const unsaturated = p => neighbors[p.id].length < p.degree;
+
+  allDistances(points, dot).forEach(({ base: p, neighbor: q, dist: d }) => {
+    if (unsaturated(p) && unsaturated(q) && neighbors[q.id].indexOf(p.id) < 0) {
+      edges.push([p.id, q.id]);
+      neighbors[p.id].push(q.id);
+      neighbors[q.id].push(p.id);
+    }
+  });
+
+  return edges;
+};
+
+
 if (require.main == module) {
   const points = [
-    { index: 1, pos: [ 0, 0, 0], degree: 4 },
-    { index: 2, pos: [ 1, 1, 1], degree: 3 },
-    { index: 3, pos: [ 1,-1,-1], degree: 3 },
-    { index: 4, pos: [-1, 1,-1], degree: 3 },
-    { index: 5, pos: [-1,-1, 1], degree: 3 }
+    { id: 1, pos: [ 1, 1, 1], degree: 4 },
+    { id: 2, pos: [ 1,-1,-1], degree: 4 },
+    { id: 3, pos: [-1, 1,-1], degree: 4 },
+    { id: 4, pos: [-1,-1, 1], degree: 4 },
+    { id: 5, pos: [ 2, 2, 2], degree: 4 },
+    { id: 6, pos: [ 2,-2,-2], degree: 4 },
+    { id: 7, pos: [-2, 2,-2], degree: 4 },
+    { id: 8, pos: [-2,-2, 2], degree: 4 }
   ];
 
-  allDistances(points).forEach(d => console.log(d));
+  console.log(inducedEdges(points).map(e => e.sort()).sort());
 }
