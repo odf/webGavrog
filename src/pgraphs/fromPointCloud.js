434cb3156a75f1fe2469c14e9abf6321e2bd4637
odf <olaf.delgado@gmail.com>
2016-04-30 16:49:45 +1000

    some more work on edge inducing code

--- src/pgraphs/fromPointCloud.js
+++ src/pgraphs/fromPointCloud.js
@@ -3,11 +3,29 @@ import { points } from '../geometry/types';
 const ops = points;
 
 
-export default function fromPointCloud({ rawPoints, edges, basis, gram }) {
-  const dot = (v, w) => ops.times(v, ops.times(gram, w));
-  const dvs = ops.times(2, lattices.dirichletVectors(basis, dot));
+const flatMap   = (fn, xs) => xs.reduce((t, x) => t.concat(fn(x)), []);
+const cartesian = (xs, ys) => flatMap(x => ys.map(y => [x, y]), xs);
 
-  const pts = rawPoints.map(p => {
-    const s = lattices.shiftIntoDirichletDomain(p, dvs, dot);
-  });
+
+export default function fromPointCloud(rawPoints, edges, gram) {
+  const dot    = (v, w) => ops.times(v, ops.times(gram, w));
+  const basis  = ops.identityMatrix(gram.length);
+  const dvs    = lattices.dirichletVectors(basis, dot);
+  const dvs2   = ops.times(2, dvs);
+  const origin = ops.times(0, dvs[0]);
+
+  const points = cartesian(rawPoints, [origin].concat(dvs))
+    .map(([pos, shift]) => {
+      const p = ops.plus(pos, shift);
+      const s = lattices.shiftIntoDirichletDomain(p, dvs2, dot);
+      return [pos, ops.plus(shift, s)];
+    });
+  console.log(points);
 };
+
+
+if (require.main == module) {
+  fromPointCloud(
+    [[0.4,-0.1]], [], [[1,0], [0,1]]
+  );
+}
