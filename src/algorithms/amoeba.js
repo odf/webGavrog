6a64549117352ac935dfce3e38df5f7062589cb0
odf <olaf.delgado@gmail.com>
2016-06-28 21:15:01 +1000

    further work on downhill simplex

--- src/algorithms/amoeba.js
+++ src/algorithms/amoeba.js
@@ -5,19 +5,47 @@ const ops = matrices;
 const plus = (v, w) => ops.plus(v, w);
 
 
-const amotry = (simplex, factor, fn) => {
+const newPoint = (simplex, factor, fn) => {
   const dim = simplex.length - 1;
-  const c = ops.div(simplex.slice(0, dim).map(p => p.pos).reduce(plus), dim);
-  const p = ops.plus(c, ops.times(factor, ops.minus(simplex[dim].pos, c)));
-  const newPoint = { pos: p, val: fn(p) };
+  const position = simplex.map(vertex => vertex.pos);
+  const c = ops.div(positions.slice(0, dim).reduce(plus), dim);
+  const p = ops.plus(c, ops.times(factor, ops.minus(positions[dim], c)));
 
-  if (newPoint.val < simplex[dim].val) {
-    for (let k = dim; k > 0 && newPoint.val < simplex[k - 1].val; --k)
-      ;
-    return [k, [].concat(simplex.slice(0, k),
-                         [newPoint],
-                         simplex.slice(k, dim))];
+  return { pos: p, val: fn(p) }
+};
+
+
+const insert = (simplex, point) => {
+  for (let k = dim; k > 0 && point.val < simplex[k - 1].val; --k)
+    ;
+  return [k, [].concat(simplex.slice(0, k), [point], simplex.slice(k, dim))];
+};
+
+
+const shrink = (simplex, fn) => {
+  return simplex
+    .map(vertex => ops.div(ops.plus(simplex[0].pos, vertex.pos), 2.0))
+    .map(p => ({ pos: p, val: fn(p) }))
+    .sort((a, b) => ops.cmp(a.val, b.val))
+};
+
+
+const step = (simplex, fn) => {
+  const dim = simplex.length - 1;
+
+  const pr = newPoint(simplex, -1.0, fn);
+
+  if (pr.val < simplex[0].val) {
+    const pe = newPoint(simplex, -2.0, fn);
+    return insert(simplex, pe.val < pr.val ? pe : pr);
   }
-  else
-    return [dim + 1, simplex];
+  else if (pr.val < simplex[dim - 1].val)
+    return insert(simplex, pr);
+  else {
+    const pc = newPoint(simplex, 0.5, fn);
+    if (pc.val >= simplex[dim].val)
+      return shrink(simplex, fn);
+    else
+      return insert(simplex, pc);
+  }
 };
