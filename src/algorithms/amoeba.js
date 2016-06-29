43750bed77cc5dfe84506fab7f77e1a258fb42d2
odf <olaf.delgado@gmail.com>
2016-06-29 20:33:08 +1000

    finished amoeba implementation

--- src/algorithms/amoeba.js
+++ src/algorithms/amoeba.js
@@ -7,7 +7,7 @@ const plus = (v, w) => ops.plus(v, w);
 
 const newPoint = (simplex, factor, fn) => {
   const dim = simplex.length - 1;
-  const position = simplex.map(vertex => vertex.pos);
+  const positions = simplex.map(vertex => vertex.pos);
   const c = ops.div(positions.slice(0, dim).reduce(plus), dim);
   const p = ops.plus(c, ops.times(factor, ops.minus(positions[dim], c)));
 
@@ -16,20 +16,22 @@ const newPoint = (simplex, factor, fn) => {
 
 
 const insert = (simplex, point) => {
-  for (let k = dim; k > 0 && point.val < simplex[k - 1].val; --k)
-    ;
-  return [k, [].concat(simplex.slice(0, k), [point], simplex.slice(k, dim))];
+  const dim = simplex.length - 1;
+  let k = dim;
+  while (k > 0 && point.val < simplex[k - 1].val)
+    --k;
+  return [].concat(simplex.slice(0, k), [point], simplex.slice(k, dim));
 };
 
 
-const sorted = simplex =>
-  simplex.clone().sort((a, b) => ops.cmp(a.val, b.val));
+const makeSimplex = (positions, fn) => positions
+  .map(p => ({ pos: p, val: fn(p) }))
+  .sort((a, b) => ops.cmp(a.val, b.val));
 
 
-const shrink = (simplex, fn) => sorted(
-  simplex
-    .map(vertex => ops.div(ops.plus(simplex[0].pos, vertex.pos), 2.0))
-    .map(p => ({ pos: p, val: fn(p) })));
+const shrink = (simplex, fn) => makeSimplex(
+  simplex.map(vertex => ops.div(ops.plus(simplex[0].pos, vertex.pos), 2.0)), fn
+);
 
 
 const step = (simplex, fn) => {
@@ -53,6 +55,31 @@ const step = (simplex, fn) => {
 };
 
 
-const optimize = (fn, start, maxSteps, tolerance) => {
-  
+const optimize = (fn, dim, start, maxSteps, tolerance) => {
+  let s = makeSimplex(
+    [start].concat(ops.identityMatrix(dim).map(e => ops.plus(e, start))), fn
+  );
+  let i = 0;
+
+  while (i < maxSteps) {
+    const vlo = s[0].val;
+    const vhi = s[dim].val;
+    if (2 * Math.abs(vhi - vlo) <= tolerance * (Math.abs(vlo) + Math.abs(vhi)))
+      break;
+
+    s = step(s, fn);
+    ++i;
+  }
+
+  return { position: s[0].pos, value: s[0].val, steps: i };
 };
+
+
+if (require.main == module) {
+  console.log(optimize(
+    ([x, y]) => 1 + Math.pow(x - 0.4, 2) * Math.pow(y - 0.4, 2),
+    2,
+    [0, 0],
+    1000,
+    1e-12));
+}
