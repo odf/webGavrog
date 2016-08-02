a31d2453b9eb395fbdbd920f9938a820ea71d167
odf <olaf.delgado@gmail.com>
2016-08-02 20:03:58 +1000

    make permutations() a generator

--- src/common/combinatorics.js
+++ src/common/combinatorics.js
@@ -1,17 +1,15 @@
 const _swap = (a, i, j) => [a[i], a[j]] = [a[j], a[i]];
 
 
-export function permutations(n) {
+export function* permutations(n) {
   const p = [];
   for (let i = 1; i <= n; ++i)
     p.push(i);
 
-  const result = [];
-
   while (true) {
     let i, j;
 
-    result.push(p.slice());
+    yield p.slice();
 
     for (i = n-2; i >= 0 && p[i] > p[i+1]; --i)
       ;
@@ -26,6 +24,4 @@ export function permutations(n) {
     for (++i, j = n-1; i < j; ++i, --j)
       _swap(p, i, j);
   }
-
-  return result;
 };
