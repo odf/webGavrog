ce4201fc28a438023f81ece3d49f48035d98a4ab
odf <olaf.delgado@gmail.com>
2016-08-02 18:43:59 +1000

    remove an obsolete function

--- src/common/util.js
+++ src/common/util.js
@@ -14,19 +14,6 @@ const now = (() => {
 })();
 
 
-export function cmpLex(cmp = (x, y) => x < y ? -1 : x > y ? 1 : 0) {
-  return function(a, b) {
-    const n = Math.min(a.size, b.size);
-    for (let i = 0; i < n; ++i) {
-      const d = cmp(a.get(i), b.get(i));
-      if (d)
-        return d;
-    }
-    return a.size - b.size;
-  };
-};
-
-
 export function timer() {
   let _last = now();
 
