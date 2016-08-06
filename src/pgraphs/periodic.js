a5dbac712363c3d55f196862fffe26e35d6cc30b
odf <olaf.delgado@gmail.com>
2016-08-06 20:19:55 +1000

    make component multiplicity positive

--- src/pgraphs/periodic.js
+++ src/pgraphs/periodic.js
@@ -202,7 +202,7 @@ const _componentInCoverGraph = (graph, start) => {
   return {
     basis,
     nodes: nodes.toArray(),
-    multiplicity: ops.determinant(basis),
+    multiplicity: ops.abs(ops.determinant(basis)),
     graph: make(newEdges)
   };
 };
@@ -374,4 +374,17 @@ if (require.main == module) {
               [ 2, 4, [ 2, 0, 0 ] ],
               [ 2, 4, [ 0, 2, 0 ] ],
               [ 2, 4, [ 0, 0, 2 ] ] ]));
+
+  test(make([ [ 1, 2, [ 0, 0, 0 ] ],
+              [ 2, 3, [ 0, 0, 0 ] ],
+              [ 3, 4, [ 0, 0, 0 ] ],
+              [ 4, 5, [ 0, 0, 0 ] ],
+              [ 5, 6, [ 0, 0, 0 ] ],
+              [ 6, 1, [ 0, 0, 0 ] ],
+              [ 1, 2, [ 1, 0, 0 ] ],
+              [ 2, 3, [ 0, 1, 0 ] ],
+              [ 3, 4, [ 0, 0, 1 ] ],
+              [ 4, 5, [ -1, 0, 0 ] ],
+              [ 5, 6, [ 0, -1, 0 ] ],
+              [ 6, 1, [ 0, 0, -1 ] ] ]));
 }
