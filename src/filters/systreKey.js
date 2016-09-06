b8a670bff3d4eff82396baf5b349a85cbd96c1e3
odf <olaf.delgado@gmail.com>
2016-09-06 20:18:54 +1000

    add error handling

--- src/filters/systreKey.js
+++ src/filters/systreKey.js
@@ -13,10 +13,15 @@ process.argv.slice(2).forEach(file => {
   const text = fs.readFileSync(file, { encoding: 'utf8' });
 
   for (const b of io.structures(text)) {
-    const key = inv.invariant(b.graph);
     console.log(b.name);
-    for (const [head, tail, shift] of key) {
-      console.log(`  ${head} ${tail} ${shift}`);
+
+    try {
+      const key = inv.invariant(b.graph);
+      for (const [head, tail, shift] of key) {
+        console.log(`  ${head} ${tail} ${shift}`);
+      }
+    } catch(ex) {
+      console.log(ex);
     }
     console.log();
   }
