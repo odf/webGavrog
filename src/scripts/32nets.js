430af9f8faeb18a63a0235c5544cf27758bab900
odf <olaf.delgado@gmail.com>
2016-10-02 18:54:47 +1100

    add a script

---
+++ src/scripts/32nets.js
@@ -0,0 +1,20 @@
+import * as generators from '../common/generators';
+
+import * as branch from '../dsymbols/branchings2d';
+import * as covers from '../dsymbols/covers';
+import * as DS from '../dsymbols/delaney';
+import * as DS2D from '../dsymbols/delaney2d';
+import * as props from '../dsymbols/properties';
+
+
+if (require.main == module) {
+  const branchings = ds => branch.branchings(ds, 3, 3, 0, [1, 2, 3, 4, 5, 6]);
+
+  covers.covers(DS.parse('<1.1:1:1,1,1:0,0>'), 8)
+    .filter(ds => DS.orbitReps2(ds, 1, 2).size == 3
+            && DS.orbitReps2(ds, 0, 2).size == 2)
+    .filter(DS2D.isProtoEuclidean)
+    .flatMap(ds => generators.results(branchings(ds)))
+    .filter(props.isMinimal)
+    .forEach(ds => console.log(`${ds}`));
+}
