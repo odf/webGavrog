c112c3989e9113c2a9f7183a54948a12828a08bb
odf <olaf.delgado@gmail.com>
2016-12-04 20:08:25 +1100

    add more modular integer ops

--- src/arithmetic/residues.js
+++ src/arithmetic/residues.js
@@ -13,6 +13,59 @@ const _inverse = (a, m) => {
 };
 
 
+const _canon    = (a, m) => a < 0 ? (a % m) + m : a % m;
+const _negative = (a, m) => m - a;
+const _cmp      = (a, b) => (a > b) - (a < b);
+const _plus     = (a, b, m) => (a + b) % m;
+const _minus    = (a, b, m) => (m - b + a) % m;
+const _times    = (a, b, m) => (a * b) % m;
+const _div      = (a, b, m) => (_inverse(b) * a) % m;
+
+
+export function extend(baseOps, m) {
+  return {
+    toJS:     { Integer: a => _canon(a, m) },
+    negative: { Integer: a => _negative(_canon(a, m)) },
+    sgn:      { Integer: a => 1 },
+    abs:      { Integer: a => _canon(a, m) },
+    floor:    { Integer: a => _canon(a, m) },
+    ceil:     { Integer: a => _canon(a, m) },
+    round:    { Integer: a => _canon(a, m) },
+
+    cmp: {
+      Integer: {
+        Integer: (a, b) => _cmp(_canon(a, m), _canon(b, m))
+      }
+    },
+    plus: {
+      Integer: {
+        Integer: (a, b) => _plus(_canon(a, m), _canon(b, m), m)
+      }
+    },
+    minus: {
+      Integer: {
+        Integer: (a, b) => _minus(_canon(a, m), _canon(b, m), m)
+      }
+    },
+    times: {
+      Integer: {
+        Integer: (a, b) => _times(_canon(a, m), _canon(b, m), m)
+      }
+    },
+    div: {
+      Integer: {
+        Integer: (a, b) => _div(_canon(a, m), _canon(b, m), m)
+      }
+    },
+    mod: {
+      Integer: {
+        Integer: (a, b) => _canon(a, m) % _canon(b, m)
+      }
+    }
+  };
+};
+
+
 if (require.main == module) {
   const test = (a, m) => {
     const ainv = _inverse(a, m);
