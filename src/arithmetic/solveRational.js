0903ef98e697b50280be40e3483bcad60bbfe48c
odf <olaf.delgado@gmail.com>
2016-12-11 20:58:18 +1100

    more work on rational linear equation system solver

--- src/arithmetic/solveRational.js
+++ src/arithmetic/solveRational.js
@@ -1,21 +1,35 @@
 import * as mats from './matrices';
-import { intMatrices, residueClassRing } from './types';
+import { intMatrices, matrices, residueClassRing } from './types';
 
 const iops = intMatrices;
+const fops = matrices;
 
 const p = 9999991;
 const pops = mats.extend(residueClassRing(p), ['Integer'], true);
 
 
 const invModP = M => {
-  console.log(`invModP(${M})`);
+  const ops = pops;
   const n = M.length;
-  const A = M.map((row, i) => row.concat(pops.unitVector(n, i)));
-  console.log(`  A = ${A}`);
-  const E = pops.rowEchelonForm(A);
-  console.log(`  E = ${E}`);
+  const A = M.map((row, i) => row.concat(ops.unitVector(n, i)));
+  const E = ops.rowEchelonForm(A);
 
   return E.map(row => row.slice(n));
+};
+
+
+const numberOfPAdicStepsNeeded = (A, b) => {
+  const ops = fops;
+
+  const lengths = M => ops.transposed(M).map(r => ops.norm(r));
+  const max = v => v.reduce((x, y) => x > y ? x : y);
+  const product = v => v.reduce((x, y) => x * y);
+
+  const ls = lengths(A).concat(max(lengths(b)));
+  const delta = product(ls.sort().slice(A[0].length));
+  const golden = (1 + Math.sqrt(5)) / 2;
+
+  return ops.ceil(2 * Math.log(delta * golden) / Math.log(p));
 };
 
 
@@ -23,16 +37,23 @@ export default function solve(A, b) {
   console.log(`solve(${A}, ${b})`);
   const C = invModP(A);
   console.log(`  C = ${C}`);
+  console.log(`  A * C = ${pops.times(A, C)}`);
 
-  const nrSteps = 10; // TODO compute a proper step number
+  const nrSteps = numberOfPAdicStepsNeeded(A, b);
+  console.log(`  nrSteps = ${nrSteps}`);
 
   let bi = b;
-  let xi;
+  let pi = 1;
+  let si = 0;
 
   for (let i = 0; i < nrSteps; ++i) {
-    xi = pops.times(C, bi);
+    const xi = pops.times(C, bi);
     bi = iops.idiv(iops.minus(bi, iops.times(A, xi)), p);
+    si = iops.plus(si, iops.times(pi, xi));
+    pi = iops.times(pi, p);
   }
+  const s = si;
+  console.log(`  s <- ${s}`);
 };
 
 
