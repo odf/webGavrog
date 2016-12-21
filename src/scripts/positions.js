08bc39bbdf1c1370ef8dd6159c39c699fb351719
odf <olaf.delgado-friedrichs@anu.edu.au>
2016-12-21 13:06:26 +1100

    add script for printing out barycentric positions

---
+++ src/scripts/positions.js
@@ -0,0 +1,23 @@
+import * as io from '../io/cgd';
+import * as pgr from '../pgraphs/periodic';
+
+const fs = require('fs');
+
+Array.prototype.toString = function() {
+  return `${this.map(x => x.toString()).join(' ')}`;
+};
+
+pgr.useModularSolver(true);
+
+
+process.argv.slice(2).forEach(file => {
+  const text = fs.readFileSync(file, { encoding: 'utf8' });
+
+  for (const { graph, name } of io.structures(text)) {
+    console.log(name);
+    const pos = pgr.barycentricPlacement(graph);
+    for (const v of pgr.vertices(graph))
+      console.log(`  ${v} ${pos.get(v)}`);
+    console.log();
+  }
+});
