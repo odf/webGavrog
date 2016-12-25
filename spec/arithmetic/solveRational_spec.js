0c5e40248d4d160b853f14cf9975f8fbf3d0c166
odf <olaf.delgado@gmail.com>
2016-12-25 15:59:47 +1100

    extract a spec helpers module

--- spec/arithmetic/solveRational_spec.js
+++ spec/arithmetic/solveRational_spec.js
@@ -1,56 +1,13 @@
 import * as JS from 'jstest';
 import * as jsc from 'jsverify';
+import * as spec from '../helpers';
 
-import * as seq from '../../src/common/lazyseq';
 import solve from '../../src/arithmetic/solveRational';
 import { matrices } from '../../src/arithmetic/types';
 
 
-const skip = (v, i) => v.slice(0, i).concat(v.slice(i + 1));
-const skip2 = (A, i, j) => skip(A, i).map(row => skip(row, j));
-const set = (v, i, x) => v.slice(0, i).concat([x], v.slice(i + 1));
-const set2 = (A, i, j, x) => set(A, i, set(A[i], j, x));
-
-
-const linearEquations = arb => {
-  const generator = jsc.generator.bless(size => {
-    const n = jsc.random(1, Math.max(1, Math.floor(Math.sqrt(size))));
-
-    const A = new Array(n + 1);
-    for (let i = 0; i < n + 1; ++i) {
-      const row = A[i] = new Array(n);
-      for (let j = 0; j < n; ++j) {
-        row[j] = arb.generator(size);
-      }
-    }
-    return [A.slice(0, n), A[n].map(x => [x])];
-  });
-
-  const shrink = jsc.shrink.bless(([A, b]) => {
-    const n = A.length;
-
-    if (n <= 1)
-      return seq.nil;
-    else {
-      const idcs = seq.range(0, n);
-      const idxPairs = idcs.flatMap(i => idcs.map(j => [i, j]));
-      const skips = idxPairs.map(([i, j]) => [skip2(A, i, j), skip(b, i)]);
-      return skips;
-    }
-  });
-
-  const show = ([A, b]) => `${JSON.stringify(A)} * x = ${JSON.stringify(b)}`;
-
-  return {
-    generator: jsc.utils.curried2(generator, arguments),
-    shrink: jsc.utils.curried2(shrink, arguments),
-    show: jsc.utils.curried2(show, arguments)
-  };
-};
-
-
-const solveReturnsASolutionOrNull = jsc.forall(
-  linearEquations(jsc.nat),
+const solveReturnsASolution = jsc.forall(
+  spec.generators.linearEquations(jsc.nat),
   ([A, b]) => {
     const x = solve(A, b);
     return x == null || matrices.eq(matrices.times(A, x), b);
@@ -58,19 +15,9 @@ const solveReturnsASolutionOrNull = jsc.forall(
 );
 
 
-const verify = (property, options) => function() {
-  const result = jsc.check(property, Object.assign({ quiet: true }, options));
-
-  if (result === true)
-    this.assert(true);
-  else
-    this.flunk(`counterexample: ${result.counterexamplestr}`);
-};
-
-
 JS.Test.describe('solveRational', function() {
   const options = { tests: 1000, size: 100 };
-  this.it('returnsASolution', verify(solveReturnsASolutionOrNull, options));
+  this.it('returnsASolution', spec.verify(solveReturnsASolution, options));
 });
 
 
---
+++ spec/helpers.js
@@ -0,0 +1,56 @@
+import * as jsc from 'jsverify';
+
+import * as seq from '../src/common/lazyseq';
+
+
+const skip = (v, i) => v.slice(0, i).concat(v.slice(i + 1));
+const skip2 = (A, i, j) => skip(A, i).map(row => skip(row, j));
+
+
+export const generators = {
+  linearEquations(arb) {
+    const generator = jsc.generator.bless(size => {
+      const n = jsc.random(1, Math.max(1, Math.floor(Math.sqrt(size))));
+
+      const A = new Array(n + 1);
+      for (let i = 0; i < n + 1; ++i) {
+        const row = A[i] = new Array(n);
+        for (let j = 0; j < n; ++j) {
+          row[j] = arb.generator(size);
+        }
+      }
+      return [A.slice(0, n), A[n].map(x => [x])];
+    });
+
+    const shrink = jsc.shrink.bless(([A, b]) => {
+      const n = A.length;
+
+      if (n <= 1)
+        return seq.nil;
+      else {
+        const idcs = seq.range(0, n);
+        const idxPairs = idcs.flatMap(i => idcs.map(j => [i, j]));
+        const skips = idxPairs.map(([i, j]) => [skip2(A, i, j), skip(b, i)]);
+        return skips;
+      }
+    });
+
+    const show = ([A, b]) => `${JSON.stringify(A)} * x = ${JSON.stringify(b)}`;
+
+    return {
+      generator: jsc.utils.curried2(generator, arguments),
+      shrink: jsc.utils.curried2(shrink, arguments),
+      show: jsc.utils.curried2(show, arguments)
+    };
+  }
+};
+
+
+export const verify = (property, options) => function() {
+  const result = jsc.check(property, Object.assign({ quiet: true }, options));
+
+  if (result === true)
+    this.assert(true);
+  else
+    this.flunk(`counterexample: ${result.counterexamplestr}`);
+};
