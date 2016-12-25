ba64ea2caefe8e6082f39e5605af261027b13da6
odf <olaf.delgado@gmail.com>
2016-12-25 19:26:22 +1100

    add another integer test

--- spec/arithmetic/integer_spec.js
+++ spec/arithmetic/integer_spec.js
@@ -31,13 +31,23 @@ JS.Test.describe('a pair a,b of integers', function() {
     }));
 
   this.describe('', spec.property(
-    'satisfies (a + b)(a - b) = a^2 - b^2',
+    'satisfies (a + b) * (a - b) = a^2 - b^2',
     [spec.generators.digitStrings(), spec.generators.digitStrings()],
     (sa, sb) => {
       const a = ops.integer(sa);
       const b = ops.integer(sb);
       return ops.eq(ops.times(ops.plus(a, b), ops.minus(a, b)),
                     ops.minus(ops.times(a, a), ops.times(b, b)));
+    }));
+
+  this.describe('', spec.property(
+    'satisfies [a / b] * b = a - a % b unless b = 0',
+    [spec.generators.digitStrings(), spec.generators.digitStrings()],
+    (sa, sb) => {
+      const a = ops.integer(sa);
+      const b = ops.integer(sb);
+      return ops.eq(0, b) || ops.eq(ops.times(ops.idiv(a, b), b),
+                                    ops.minus(a, ops.mod(a, b)));
     }));
 });
 
