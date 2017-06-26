2f96127932cee04433e104cd8e29f094a63ffec7
odf <olaf.delgado@gmail.com>
2017-06-26 18:28:31 +1000

    tidy up amoeba code

--- src/algorithms/amoeba.js
+++ src/algorithms/amoeba.js
@@ -4,38 +4,19 @@ const ops = matrices;
 
 const newPoint = (simplex, factor, fn) => {
   const dim = simplex.length - 1;
-  const vs = simplex.map(vertex => vertex.pos);
-  const p = new Array(dim);
+  const p = new Array(dim + 1).fill(0);
 
-  for (let i = 0; i < dim; ++i) {
+  for (let i = 1; i < dim + 1; ++i) {
     let c = 0;
     for (let j = 0; j < dim; ++j)
-      c += vs[j][i];
-    c /= dim;
-    p[i] = factor * vs[dim][i] + (1 - factor) * c;
+      c += simplex[j][i];
+    p[i] = factor * simplex[dim][i] + (1 - factor) * c / dim;
   }
 
-  return { pos: p, val: fn(p) }
+  p[0] = fn(p.slice(1));
+
+  return p;
 };
-
-
-const insert = (simplex, point) => {
-  const dim = simplex.length - 1;
-  let k = dim;
-  while (k > 0 && point.val < simplex[k - 1].val)
-    --k;
-  return [].concat(simplex.slice(0, k), [point], simplex.slice(k, dim));
-};
-
-
-const makeSimplex = (positions, fn) => positions
-  .map(p => ({ pos: p, val: fn(p) }))
-  .sort((a, b) => ops.cmp(a.val, b.val));
-
-
-const shrink = (simplex, fn) => makeSimplex(
-  simplex.map(vertex => ops.div(ops.plus(simplex[0].pos, vertex.pos), 2.0)), fn
-);
 
 
 const step = (simplex, fn) => {
@@ -43,40 +24,52 @@ const step = (simplex, fn) => {
 
   const pr = newPoint(simplex, -1.0, fn);
 
-  if (pr.val < simplex[0].val) {
+  if (pr[0] < simplex[0][0]) {
     const pe = newPoint(simplex, -2.0, fn);
-    return insert(simplex, pe.val < pr.val ? pe : pr);
+    simplex[dim] = pe[0] < pr[0] ? pe : pr;
   }
-  else if (pr.val < simplex[dim - 1].val)
-    return insert(simplex, pr);
+  else if (pr[0] < simplex[dim - 1][0])
+    simplex[dim] = pr;
   else {
     const pc = newPoint(simplex, 0.5, fn);
-    if (pc.val >= simplex[dim].val)
-      return shrink(simplex, fn);
-    else
-      return insert(simplex, pc);
+    if (pc[0] < simplex[dim][0])
+      simplex[dim] = pc;
+    else {
+      for (let i = 0; i <= dim; ++i) {
+        simplex[i] = ops.div(ops.plus(simplex[i], simplex[0]), 2);
+        simplex[i][0] = fn(simplex[i].slice(1));
+      }
+    }
   }
+
+  simplex.sort((a, b) => a[0] - b[0]);
 };
 
 
 const optimize = (fn, dim, start, maxSteps, tolerance, initialScale=1.0) => {
   const m = ops.times(initialScale, ops.identityMatrix(dim));
 
-  let s = makeSimplex([start].concat(m.map(e => ops.plus(e, start))), fn);
-  let i = 0;
+  const simplex = [start].concat(m.map(e => ops.plus(e, start)))
+    .map(p => [fn(p)].concat(p))
+    .sort((a, b) => a[0] - b[0]);
 
+  let i = 0;
   while (i < maxSteps) {
-    const vlo = s[0].val;
-    const vhi = s[dim].val;
+    const vlo = simplex[0][0];
+    const vhi = simplex[dim][0];
     if (2 * Math.abs(vhi - vlo) <=
         tolerance * Math.max(tolerance, Math.abs(vlo) + Math.abs(vhi)))
       break;
 
-    s = step(s, fn);
+    step(simplex, fn);
     ++i;
   }
 
-  return { position: s[0].pos, value: s[0].val, steps: i };
+  return {
+    position: simplex[0].slice(1),
+    value: simplex[0][0],
+    steps: i
+  };
 };
 
 
