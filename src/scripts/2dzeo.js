fd0a5a543a5227f89dc255ebde1f10b0a47594c4
odf <olaf.delgado@gmail.com>
2017-08-13 11:59:47 +1000

    build vertex trivalence into initial enumeration

--- src/scripts/2dzeo.js
+++ src/scripts/2dzeo.js
@@ -7,25 +7,15 @@ import * as DS2D from '../dsymbols/delaney2d';
 import * as props from '../dsymbols/properties';
 
 
-const allRs = (ds, i, j) =>
-  DS.orbitReps2(ds, i, j).map(D => DS.r(ds, i, j, D));
-
 const allMs = (ds, i, j) =>
   DS.orbitReps2(ds, i, j).map(D => DS.m(ds, i, j, D));
-
-const withAllTrivalentVertices = ds => DS.withBranchings(
-  ds, 1,
-  DS.orbitReps2(ds, 1, 2).toArray().map(D => [D, 3 / DS.r(ds, 1, 2, D)])
-);
 
 
 if (require.main == module) {
   const n = parseInt(process.argv[2])
 
-  covers.covers(DS.parse('<1.1:1:1,1,1:0,0>'), n * 6)
+  covers.covers(DS.parse('<1.1:1:1,1,1:0,3>'), n * 6)
     .filter(ds => DS.orbitReps2(ds, 1, 2).size == n)
-    .filter(ds => allRs(ds, 1, 2).every(r => r == 1 || r == 3))
-    .map(withAllTrivalentVertices)
     .filter(DS2D.isProtoEuclidean)
     .flatMap(ds => generators.results(branch.branchings(ds)))
     .filter(ds => allMs(ds, 1, 2).every(r => r == 3))
