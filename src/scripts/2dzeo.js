9a30550d4b9462bd97a83baad5e448990b549e8f
odf <olaf.delgado-friedrichs@anu.edu.au>
2017-08-15 16:18:50 +1000

    shorten the test code for edge-sharing quadrangles

--- src/scripts/2dzeo.js
+++ src/scripts/2dzeo.js
@@ -7,12 +7,8 @@ import * as DS2D from '../dsymbols/delaney2d';
 import * as props from '../dsymbols/properties';
 
 
-const good = ds => DS.orbitReps2(ds, 0, 2).every(D => {
-  const a = DS.m(ds, 0, 1, D);
-  const b = DS.m(ds, 0, 1, ds.s(2, D));
-
-  return a >= 4 && b >= 4 && (a > 4 || b > 4);
-});
+const noEdgeSharingQuads = ds => DS.orbitReps2(ds, 0, 2)
+  .every(D => DS.m(ds, 0, 1, D) > 4 || DS.m(ds, 0, 1, ds.s(2, D)) > 4);
 
 
 if (require.main == module) {
@@ -22,7 +18,7 @@ if (require.main == module) {
     .filter(ds => DS.orbitReps2(ds, 1, 2).size == n)
     .filter(DS2D.isProtoEuclidean)
     .flatMap(ds => generators.results(branch.branchings(ds, 4)))
-    .filter(good)
+    .filter(noEdgeSharingQuads)
     .filter(props.isMinimal)
     .filter(DS2D.isEuclidean)
     .forEach(ds => console.log(`${ds}`));
