eefc09b12edd6640f511fd3f9620f8cae9a736b3
odf <olaf.delgado@gmail.com>
2017-08-25 20:29:02 +1000

    fix basis extension code

--- src/arithmetic/linearAlgebraExact.js
+++ src/arithmetic/linearAlgebraExact.js
@@ -1,8 +1,8 @@
 export const extendBasis = (v, bs, ops, overField=true) => {
-  // properly implement the case overField=false
-
-  if (bs.length == 0)
-    bs.push(v);
+  if (bs.length == 0) {
+    if (v.findIndex(x => ops.ne(x, 0)) >= 0)
+      bs.push(v);
+  }
   else {
     const [nrows, ncols] = ops.shape(bs);
     if (v.length != ncols)
@@ -22,11 +22,22 @@ export const extendBasis = (v, bs, ops, overField=true) => {
 
       if (colV < colBs || colV >= ncols || colBs >= ncols)
         break;
-      else if (colV == colBs)
-        v = ops.minus(v, ops.times(b, ops.div(v[colV], b[colV])));
+      else if (colV == colBs) {
+        if (overField || ops.eq(0, ops.mod(v[colV], b[colV]))) {
+          v = ops.minus(v, ops.times(b, ops.div(v[colV], b[colV])));
+        }
+        else {
+          const [x, r, s, t, u] = ops.gcdex(v[colV], b[colV]);
+          bs[rowBs] = ops.plus(ops.times(v, r), ops.times(b, s));
+          v = ops.plus(ops.times(v, t), ops.times(b, u));
+        }
+      }
 
       ++rowBs;
     }
+
+    while (colV < ncols && ops.eq(v[colV], 0))
+      ++colV;
 
     if (colV < ncols)
       bs.splice(rowBs, 0, v);
