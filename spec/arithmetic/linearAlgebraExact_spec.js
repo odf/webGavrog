8170b09fdd62fac6d3d3b514ed62dfcf258784b1
odf <olaf.delgado@gmail.com>
2017-09-06 12:30:09 +1000

    add another nullSpace spec

--- spec/arithmetic/linearAlgebraExact_spec.js
+++ spec/arithmetic/linearAlgebraExact_spec.js
@@ -12,13 +12,22 @@ const test = function(ops) {
       ([A, v]) => {
         const b = ops.times(A, v);
         return ops.eq(ops.times(A, ops.solve(A, b)), b);
-      }));
+      }
+    ));
   });
 
   this.describe('the nullSpace operator', function() {
     this.it('determines the correct null space rank', spec.property(
       [spec.generators.linearEquations(jsc.nat)],
       ([A, _]) => ops.rank(ops.nullSpace(A)) == ops.shape(A)[0] - ops.rank(A)
+    ));
+    this.it('returns a correct solution', spec.property(
+      [spec.generators.linearEquations(jsc.nat)],
+      ([A, _]) => {
+        const N = ops.nullSpace(A);
+        return N == null ||
+          ops.times(A, N).every(v => v.every(x => ops.eq(x, 0)));
+      }
     ));
   });
 };
