3f1dfd09278f75e852e2182548c386e877bb0f70
odf <olaf.delgado@gmail.com>
2018-05-28 20:08:04 +1000

    remove plexus-form code

--- src/plexus-form/fields/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-module.exports = {
-    CheckBox: require('./checkbox'),
-    InputField: require('./input'),
-    UserDefinedField: require('./user_defined'),
-    Selection: require('./selection'),
-    make: require('./make'),
-};
