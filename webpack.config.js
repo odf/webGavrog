b11308975e21d022e62046ad6ead6871fac9e8a8
odf <olaf.delgado@gmail.com>
2019-02-05 19:44:36 +1100

    update to Babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -2,25 +2,22 @@ var fs = require('fs');
 var path = require('path');
 var execSync = require('child_process').execSync;
 
-var basedir = __dirname;
-
 var execOpts = { encoding: 'utf8' };
 var gitRev = execSync('git rev-parse HEAD', execOpts);
 var gitDate = execSync('git show -s --format=%ci ${GIT_REV}', execOpts);
 
 
-fs.writeFileSync(path.join(basedir, 'src', 'version.js'),
+fs.writeFileSync(path.resolve(__dirname, 'src', 'version.js'),
                  'export const gitRev = "' + gitRev.trim() + '";\n' +
                  'export const gitDate = "' + gitDate.trim() + '";\n');
 
 
 module.exports = [ "main", "sceneWorker" ].map(function(name) {
   return {
-    context: path.join(basedir, "src"),
-    entry: ["babel-polyfill", "./ui/" + name],
+    entry: ['babel-polyfill', path.resolve(__dirname, 'src', 'ui', name)],
     output: {
-      path: path.join(basedir, "public", "js"),
-      filename: name+".js"
+      path: path.resolve(__dirname, 'public', 'js'),
+      filename: name+'.js'
     },
     module: {
       rules: [
@@ -30,11 +27,10 @@ module.exports = [ "main", "sceneWorker" ].map(function(name) {
         { test: /\.elm$/,
           exclude: [/node_modules/, /elm-stuff/],
           use: "elm-webpack-loader" },
-        { test: /\.json$/, use: "json-loader" }
       ]
     },
     resolve: {
-      extensions: [ ".js", ".elm", ".json" ]
+      extensions: [ ".js", ".elm" ]
     }
   };
 });
