b3e56e1a3ae39d74175c0930281f01b11ff85d89
odf <olaf.delgado@gmail.com>
2019-02-19 20:50:30 +1100

    GUI: prototype the appearance of a color selection tool

--- src/elm/GuiMain.elm
+++ src/elm/GuiMain.elm
@@ -767,6 +767,34 @@ viewCurrentDialog model =
                     , color = Element.rgba 0.0 0.0 0.0 0.2
                     }
                 ]
+
+        slider =
+            \pos ->
+                Element.row
+                    [ Element.width Element.fill
+                    , Element.height Element.fill
+                    ]
+                    [ Element.el
+                        [ Element.width <| Element.fillPortion (pos - 3) ]
+                        Element.none
+                    , Element.el
+                        [ Border.shadow
+                            { offset = ( 1.0, 3.0 )
+                            , size = 2.0
+                            , blur = 4.0
+                            , color = Element.rgba 0.0 0.0 0.0 0.3
+                            }
+                        , Border.color <| Element.rgb 1.0 1.0 1.0
+                        , Border.solid
+                        , Border.widthXY 1 0
+                        , Element.width <| Element.fillPortion 6
+                        , Element.height Element.fill
+                        ]
+                        Element.none
+                    , Element.el
+                        [ Element.width <| Element.fillPortion (252 - pos) ]
+                        Element.none
+                    ]
     in
     case model.visibleDialog of
         Nothing ->
@@ -786,7 +814,54 @@ viewCurrentDialog model =
 
         Just Options ->
             wrap <|
-                Options.view OptionsMsg model.optionSpecsTmp
+                Element.column [ Element.spacing 16 ]
+                    [ Options.view OptionsMsg model.optionSpecsTmp
+                    , Element.el
+                        [ Element.width Element.fill
+                        , Element.height <| Element.px 24
+                        , Element.inFront <| slider 64
+                        , Background.gradient
+                            { angle = pi / 2
+                            , steps =
+                                [ Element.rgb 1.0 0.0 0.0
+                                , Element.rgb 1.0 1.0 0.0
+                                , Element.rgb 0.0 1.0 0.0
+                                , Element.rgb 0.0 1.0 1.0
+                                , Element.rgb 0.0 0.0 1.0
+                                , Element.rgb 1.0 0.0 1.0
+                                , Element.rgb 1.0 0.0 0.0
+                                ]
+                            }
+                        ]
+                        Element.none
+                    , Element.el
+                        [ Element.width Element.fill
+                        , Element.height <| Element.px 24
+                        , Element.inFront <| slider 192
+                        , Background.gradient
+                            { angle = pi / 2
+                            , steps =
+                                [ Element.rgb 0.5 0.5 0.5
+                                , Element.rgb 0.0 1.0 1.0
+                                ]
+                            }
+                        ]
+                        Element.none
+                    , Element.el
+                        [ Element.width Element.fill
+                        , Element.height <| Element.px 24
+                        , Element.inFront <| slider 0
+                        , Background.gradient
+                            { angle = pi / 2
+                            , steps =
+                                [ Element.rgb 0.0 0.0 0.0
+                                , Element.rgb 0.0 1.0 1.0
+                                , Element.rgb 1.0 1.0 1.0
+                                ]
+                            }
+                        ]
+                        Element.none
+                    ]
 
 
 viewAbout : Model -> Element.Element Msg
