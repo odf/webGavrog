a7216b572b4abd071fa52d2aa68dca9481a1002d
odf <olaf.delgado@gmail.com>
2019-10-18 17:42:46 +0200

    scripts/canonical: update legacy script

--- src/scripts/canonical.js
+++ src/scripts/canonical.js
@@ -8,7 +8,7 @@ import * as props   from '../dsymbols/properties';
 const text = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
 
 DS.parseSymbols(text)
-  .map(ds => [derived.canonical(ds), I.List(props.invariant(ds))])
+  .map(ds => [derived.canonical(ds), props.invariant(ds)])
   .sort(function(a, b) {
     if (DS.dim(a[0]) != DS.dim(b[0]))
       return DS.dim(a[0]) - DS.dim(b[0]);
@@ -16,9 +16,9 @@ DS.parseSymbols(text)
       return DS.size(a[0]) - DS.size(b[0]);
     const ia = a[1];
     const ib = b[1];
-    for (let i = 0; i < ia.size && i < ib.size; ++i)
-      if (ia.get(i) != ib.get(i))
-        return ia.get(i) - ib.get(i);
-    return ia.size() - ib.size();
+    for (let i = 0; i < ia.length && i < ib.length; ++i)
+      if (ia[i] != ib[i])
+        return ia[i] - ib[i];
+    return ia.length - ib.length;
   })
   .forEach(e => console.log(`${e[0]}`));
