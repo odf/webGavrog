a0a471498a304a9765f0cfa3c9e14f9bf20174cb
odf <olaf.delgado@gmail.com>
2019-10-21 12:05:54 +0200

    scripts/compareTilingList: also check for duplicates in each list

--- src/scripts/compareTilingLists.js
+++ src/scripts/compareTilingLists.js
@@ -16,12 +16,34 @@ const invarsA = listA.map(ds => props.invariant(ds).join(' '));
 const invarsB = listB.map(ds => props.invariant(ds).join(' '));
 
 
+console.log("Duplicates in first list:");
+for (let i = 0; i < listA.length; ++i) {
+  const j = invarsA.indexOf(invarsA[i]);
+  if (j < i)
+    console.log(`#${j+1},#${i+1} ${listA[i]}`);
+}
+console.log();
+
+
+console.log("In first list, but not in second:");
 for (let i = 0; i < listA.length; ++i) {
   if (invarsB.indexOf(invarsA[i]) < 0)
-    console.log(`- ${i} ${listA[i]}`);
+    console.log(`#${i+1} ${listA[i]}`);
 }
+console.log();
 
+
+console.log("Duplicates in second list:");
+for (let i = 0; i < listB.length; ++i) {
+  const j = invarsB.indexOf(invarsB[i]);
+  if (j < i)
+    console.log(`#${j+1},#${i+1} ${listB[i]}`);
+}
+console.log();
+
+
+console.log("In second list, but not in first:");
 for (let i = 0; i < listB.length; ++i) {
   if (invarsA.indexOf(invarsB[i]) < 0)
-    console.log(`+ ${i} ${listB[i]}`);
+    console.log(`#${i+1} ${listB[i]}`);
 }
