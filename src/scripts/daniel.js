7beb155d06e7cbf78fae98619a05f638051e568b
odf <olaf.delgado@gmail.com>
2019-10-31 20:15:34 +1100

    extract and debug 2d D-set generator

--- src/scripts/daniel.js
+++ src/scripts/daniel.js
@@ -241,123 +241,6 @@ const branchings = ds => {
 }
 
 
-const delaneySets = maxSize => {
-  const _firstUndefined = data => {
-    const i = data.indexOf(null);
-    if (i >= 0)
-      return [Math.floor(i / 3) + 1, i % 3];
-  };
-
-  const _size = data => data.length / 3;
-
-  const _get = (data, i, D) => data[(D - 1) * 3 + i];
-
-  const _set = (data, i, D, E) => {
-    data[(D - 1) * 3 + i] = E;
-    data[(E - 1) * 3 + i] = D;
-  };
-
-  const _makeDelaneySet = data => {
-    const dim = 2;
-    const size = _size(data);
-    const ops = new Array(dim + 1).fill(0).map(_ => []);
-    const brs = new Array(dim).fill(0).map(_ => []);
-
-    for (let D = 1; D <= size; ++D) {
-      for (let i = 0; i <= 2; ++i)
-        ops[i].push([D, _get(data, i, D)]);
-    }
-    return DS.build(dim, size, (_, i) => ops[i], (_, i) => brs[i]);
-  };
-
-  const _scan = (data, w, D, limit) => {
-    let [E, k] = [D, 0];
-
-    while (k < limit && _get(w[k], E) != null)
-      [E, k] = [_get(w[k], E), k + 1];
-
-    return [E, k];
-  };
-
-  const _scan02Orbit = (data, D) => {
-    const [head, i] = _scan(data, [0, 2, 0, 2], D, 4);
-    const [tail, j] = _scan(data, [2, 0, 2, 0], head, 4 - i);
-    const gap = 4 - i - j;
-    return [head, tail, gap, 2 * (i % 2)];
-  };
-
-  const _potentialChildren = (data, maxSize) => {
-    const size = _size(data);
-    const limit = Math.min(size + 1, maxSize);
-    const [D, i] = _firstUndefined(data);
-    const result = [];
-
-    for (let E = D; E < limit; ++E) {
-      if (E > size)
-        data = data.concat([null, null, null]);
-
-      if (_get(data, E, i) == null) {
-        const out = data.slice();
-        _set(out, i, D, E);
-
-        const [head, tail, gap, k] = _scan02Orbit(out, D);
-        if (gap == 1)
-          _set(out, k, head, tail);
-        else if (gap > 0 || head == tail)
-          result.push(out);
-      }
-    }
-
-    return result;
-  };
-
-  const _compareRenumberedFrom = (data, D0) => {
-    const n2o = [ null, D0];
-    const o2n = { [D0]: 1 };
-
-    for (let D = 1; D <= _size(data); ++D) {
-      if (D >= n2o.length)
-        throw new Error("symbol is not transitive");
-
-      for (const i of [0, 1, 2]) {
-        const E = _get(data, i, n2o[D]);
-        if (E != null && o2n[E] == null) {
-          o2n[E] = n2o.length;
-          n2o.push(E);
-        }
-
-        const nval = o2n[E];
-        const oval = _get(data, i, D);
-        if (oval != nval)
-          return oval == null ? -1 : nval == null ? 1 : nval - oval;
-      }
-    }
-
-    return 0;
-  };
-
-  const _isCanonical = data => {
-    for (let D = 1; D <= _size(data); ++D) {
-      if (_compareRenumberedFrom(data, D) < 0)
-        return false;
-    }
-    return true;
-  };
-
-  return generators.backtracker({
-    root: [null, null, null],
-
-    extract(data) {
-      return _firstUndefined(data) ? null : _makeDelaneySet(data);
-    },
-
-    children(data) {
-      return _potentialChildren(data, maxSize).filter(_isCanonical);
-    }
-  });
-}
-
-
 function* coversGenerator(ds, maxDeg) {
   timers && timers.start('covers');
   const fun = fundamental.fundamentalGroup(ds);
@@ -395,8 +278,8 @@ if (require.main == module) {
     const maxSize = parseInt(arg);
     const ds0 = DS.parse('<1.1:1:1,1,1:0,0>');
 
-    //for (const dset of coversGenerator(ds0, maxSize)) {
-    for (const dset of generators.results(delaneySets(maxSize))) {
+    for (const dset of coversGenerator(ds0, maxSize)) {
+    //for (const dset of generators.results(delaneySets(maxSize))) {
       timers && timers.start('branchings');
       for (const ds of generators.results(branchings(dset)))
         console.log(`${ds}`);
