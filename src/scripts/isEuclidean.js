a3a84668d2404b5db68eb2bd1b7e3abb10bd6154
odf <olaf.delgado@gmail.com>
2019-12-14 10:40:13 +1100

    isEuclidean: switch to using the readline interface

--- src/scripts/isEuclidean.js
+++ src/scripts/isEuclidean.js
@@ -1,11 +1,20 @@
 import * as fs from 'fs';
+import * as readline from 'readline';
 
-import { parseSymbols } from '../dsymbols/delaney';
+import { parse } from '../dsymbols/delaney';
 import { isEuclidean } from '../dsymbols/delaney2d';
 
-const text = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
 
-for (const ds of parseSymbols(text)) {
-  if (isEuclidean(ds))
-    console.log(`${ds}`);
-}
+const reader = readline.createInterface({
+  input: fs.createReadStream(process.argv[2], { encoding: 'utf8' }),
+  crlfDelay: Infinity
+});
+
+
+reader.on('line', line => {
+  if (line.match(/^\s*</)) {
+    const ds = parse(line);
+    if (isEuclidean(ds))
+      console.log(`${ds}`);
+  }
+});
