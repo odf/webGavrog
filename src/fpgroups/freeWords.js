19f61975af2e412f78ed2097b7d066897564c792
odf <olaf.delgado@gmail.com>
2020-01-16 11:58:01 +0100

    freeWords: fix a typo in previous change

--- src/fpgroups/freeWords.js
+++ src/fpgroups/freeWords.js
@@ -10,7 +10,7 @@ const _normalize = ws => {
     for (const x of w) {
       if (!Number.isSafeInteger(x))
         throw new Error(`illegal word ${w} - all letters must be integers`);
-      else else if (x == 0)
+      else if (x == 0)
         throw new Error(`illegal word ${w} - all letters must be non-zero`);
       else if (head > 0 && x == - tmp[head-1])
         --head;
