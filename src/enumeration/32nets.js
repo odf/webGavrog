66d0f217b9ffe41ff0fcb1bfe9a2ad47d94191dc
odf <olaf.delgado@gmail.com>
2020-02-27 12:25:08 +0100

    32nets: tweak imports

--- src/enumeration/32nets.js
+++ src/enumeration/32nets.js
@@ -1,21 +1,21 @@
-import * as branch from './branchings2d';
-import * as covers from '../dsymbols/covers';
-import * as DS from '../dsymbols/delaney';
-import * as DS2D from '../dsymbols/delaney2d';
+import { covers } from '../dsymbols/covers';
+import * as delaney from '../dsymbols/delaney';
+import * as delaney2d from '../dsymbols/delaney2d';
 import * as props from '../dsymbols/properties';
+import { branchings } from './branchings2d';
 
 
 export function* netsOfTransitivity32() {
-  const base = DS.parse('<1.1:1:1,1,1:0,0>');
+  const base = delaney.parse('<1.1:1:1,1,1:0,0>');
 
-  for (const dset of covers.covers(base, 8)) {
+  for (const dset of covers(base, 8)) {
     if (dset.orbitReps2(1, 2).length != 3 || dset.orbitReps2(0, 2).length != 2)
       continue;
 
-    if (!DS2D.isProtoEuclidean(dset))
+    if (!delaney2d.isProtoEuclidean(dset))
       continue;
 
-    for (const ds of branch.branchings(dset, 3, 3, 0, [1, 2, 3, 4, 5, 6])) {
+    for (const ds of branchings(dset, 3, 3, 0, [1, 2, 3, 4, 5, 6])) {
       if (props.isMinimal(ds))
         yield ds;
     }
