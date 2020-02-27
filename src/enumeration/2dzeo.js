7f2bc22a942e9e6ef467967cc09fc37aad3fdfee
odf <olaf.delgado@gmail.com>
2020-02-27 12:04:02 +0100

    2dzeo: rewrite as plain generator and export it

--- src/enumeration/2dzeo.js
+++ src/enumeration/2dzeo.js
@@ -1,26 +1,37 @@
 import { seq } from '../common/lazyseq';
-
 import * as covers from '../dsymbols/covers';
 import * as DS from '../dsymbols/delaney';
 import * as DS2D from '../dsymbols/delaney2d';
 import * as props from '../dsymbols/properties';
-
 import * as branch from './branchings2d';
 
 
-const noEdgeSharingQuads = ds => DS.orbitReps2(ds, 0, 2)
-  .every(D => DS.m(ds, 0, 1, D) > 4 || DS.m(ds, 0, 1, ds.s(2, D)) > 4);
+export function* twoDimensionalZeolites(nrVertices) {
+  const base = DS.parse('<1.1:1:1,1,1:0,3>');
+
+  for (const dset of covers.covers(base, nrVertices * 6)) {
+    if (dset.orbitReps2(1, 2).length != nrVertices)
+      continue;
+
+    if (!DS2D.isProtoEuclidean(dset))
+      continue;
+
+    for (const ds of branch.branchings(dset, 4)) {
+      const reps = ds.orbitReps2(0, 2);
+
+      if (reps.some(D => ds.m(0, 1, D) == 4 && ds.m(0, 1, ds.s(2, D)) == 4))
+        continue;
+
+      if (props.isMinimal(ds) && DS2D.isEuclidean(ds))
+        yield ds;
+    }
+  }
+}
 
 
 if (require.main == module) {
   const n = parseInt(process.argv[2])
 
-  seq(covers.covers(DS.parse('<1.1:1:1,1,1:0,3>'), n * 6))
-    .filter(ds => DS.orbitReps2(ds, 1, 2).length == n)
-    .filter(DS2D.isProtoEuclidean)
-    .flatMap(ds => branch.branchings(ds, 4))
-    .filter(noEdgeSharingQuads)
-    .filter(props.isMinimal)
-    .filter(DS2D.isEuclidean)
-    .forEach(ds => console.log(`${ds}`));
+  for (const ds of twoDimensionalZeolites(n))
+    console.log(`${ds}`);
 }
