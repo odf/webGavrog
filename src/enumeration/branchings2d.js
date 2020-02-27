61dfb2c69726497567223d0484b4612d70193b5d
odf <olaf.delgado@gmail.com>
2020-02-27 14:31:20 +0100

    branchings2d: remove test script

--- src/enumeration/branchings2d.js
+++ src/enumeration/branchings2d.js
@@ -79,22 +79,3 @@ export const branchings = (
     }
   });
 }
-
-
-if (require.main == module) {
-  const covers = require('../dsymbols/covers');
-
-  const noEdgeSharingQuads = ds => DS.orbitReps2(ds, 0, 2)
-    .every(D => DS.m(ds, 0, 1, D) > 4 || DS.m(ds, 0, 1, ds.s(2, D)) > 4);
-
-  const n = parseInt(process.argv[2]) || 2
-
-  covers.covers(DS.parse('<1.1:1:1,1,1:0,3>'), n * 6)
-    .filter(ds => DS.orbitReps2(ds, 1, 2).length == n)
-    .filter(DS2D.isProtoEuclidean)
-    .flatMap(ds => branchings(ds, 4))
-    .filter(noEdgeSharingQuads)
-    .filter(props.isMinimal)
-    .filter(DS2D.isEuclidean)
-    .forEach(ds => console.log(`${ds}`));
-}
