ecf1f2b9632648e241a557552941ac48b344d03e
odf <olaf.delgado@gmail.com>
2020-02-27 12:09:45 +0100

    32nets: update so that it executes again

--- src/enumeration/32nets.js
+++ src/enumeration/32nets.js
@@ -1,14 +1,15 @@
-import * as branch from '../dsymbols/branchings2d';
+import * as branch from './branchings2d';
 import * as covers from '../dsymbols/covers';
 import * as DS from '../dsymbols/delaney';
 import * as DS2D from '../dsymbols/delaney2d';
 import * as props from '../dsymbols/properties';
 
+import { seq } from '../common/lazyseq';
 
 if (require.main == module) {
   const branchings = ds => branch.branchings(ds, 3, 3, 0, [1, 2, 3, 4, 5, 6]);
 
-  covers.covers(DS.parse('<1.1:1:1,1,1:0,0>'), 8)
+  seq(covers.covers(DS.parse('<1.1:1:1,1,1:0,0>'), 8))
     .filter(ds => DS.orbitReps2(ds, 1, 2).length == 3
             && DS.orbitReps2(ds, 0, 2).length == 2)
     .filter(DS2D.isProtoEuclidean)
