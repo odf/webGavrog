06424a37b35b23c4edd1951e01137e87042385a2
odf <olaf.delgado@gmail.com>
2020-02-27 12:23:24 +0100

    32nets: rewrite as plain generator and export

--- src/enumeration/32nets.js
+++ src/enumeration/32nets.js
@@ -4,16 +4,26 @@ import * as DS from '../dsymbols/delaney';
 import * as DS2D from '../dsymbols/delaney2d';
 import * as props from '../dsymbols/properties';
 
-import { seq } from '../common/lazyseq';
+
+export function* netsOfTransitivity32() {
+  const base = DS.parse('<1.1:1:1,1,1:0,0>');
+
+  for (const dset of covers.covers(base, 8)) {
+    if (dset.orbitReps2(1, 2).length != 3 || dset.orbitReps2(0, 2).length != 2)
+      continue;
+
+    if (!DS2D.isProtoEuclidean(dset))
+      continue;
+
+    for (const ds of branch.branchings(dset, 3, 3, 0, [1, 2, 3, 4, 5, 6])) {
+      if (props.isMinimal(ds))
+        yield ds;
+    }
+  }
+}
+
 
 if (require.main == module) {
-  const branchings = ds => branch.branchings(ds, 3, 3, 0, [1, 2, 3, 4, 5, 6]);
-
-  seq(covers.covers(DS.parse('<1.1:1:1,1,1:0,0>'), 8))
-    .filter(ds => DS.orbitReps2(ds, 1, 2).length == 3
-            && DS.orbitReps2(ds, 0, 2).length == 2)
-    .filter(DS2D.isProtoEuclidean)
-    .flatMap(ds => branchings(ds))
-    .filter(props.isMinimal)
-    .forEach(ds => console.log(`${ds}`));
+  for (const ds of netsOfTransitivity32())
+    console.log(`${ds}`);
 }
