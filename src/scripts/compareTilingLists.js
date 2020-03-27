54ecf3059b4f188be360ac04e12945c2dff30245
odf <olaf.delgado@gmail.com>
2020-03-28 10:40:28 +1100

    scripts/compareTilingLists: update to new D-symbol file parsing method

--- src/scripts/compareTilingLists.js
+++ src/scripts/compareTilingLists.js
@@ -1,26 +1,26 @@
 import * as fs from 'fs';
 
-import * as DS from '../dsymbols/delaney';
 import * as props from '../dsymbols/properties';
+import symbols from '../io/ds';
 
 
-const listA = DS.parseSymbols(
-  fs.readFileSync(process.argv[2], { encoding: 'utf8' })
-);
+const listA = [
+  ...symbols(fs.readFileSync(process.argv[2], { encoding: 'utf8' }))
+];
 
-const listB = DS.parseSymbols(
-  fs.readFileSync(process.argv[3], { encoding: 'utf8' })
-);
+const listB = [
+  ...symbols(fs.readFileSync(process.argv[3], { encoding: 'utf8' }))
+];
 
-const invarsA = listA.map(ds => props.invariant(ds).join(' '));
-const invarsB = listB.map(ds => props.invariant(ds).join(' '));
+const invarsA = listA.map(e => props.invariant(e.symbol).join(' '));
+const invarsB = listB.map(e => props.invariant(e.symbol).join(' '));
 
 
 console.log("Duplicates in first list:");
 for (let i = 0; i < listA.length; ++i) {
   const j = invarsA.indexOf(invarsA[i]);
   if (j < i)
-    console.log(`#${j+1},#${i+1} ${listA[i]}`);
+    console.log(`#${j+1},#${i+1} ${listA[i].symbol}`);
 }
 console.log();
 
@@ -28,7 +28,7 @@ console.log();
 console.log("In first list, but not in second:");
 for (let i = 0; i < listA.length; ++i) {
   if (invarsB.indexOf(invarsA[i]) < 0)
-    console.log(`#${i+1} ${listA[i]}`);
+    console.log(`#${i+1} ${listA[i].symbol}`);
 }
 console.log();
 
@@ -37,7 +37,7 @@ console.log("Duplicates in second list:");
 for (let i = 0; i < listB.length; ++i) {
   const j = invarsB.indexOf(invarsB[i]);
   if (j < i)
-    console.log(`#${j+1},#${i+1} ${listB[i]}`);
+    console.log(`#${j+1},#${i+1} ${listB[i].symbol}`);
 }
 console.log();
 
@@ -45,5 +45,5 @@ console.log();
 console.log("In second list, but not in first:");
 for (let i = 0; i < listB.length; ++i) {
   if (invarsA.indexOf(invarsB[i]) < 0)
-    console.log(`#${i+1} ${listB[i]}`);
+    console.log(`#${i+1} ${listB[i].symbol}`);
 }
