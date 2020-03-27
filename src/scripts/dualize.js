24032e0110f1f676152269824483abafbc33f331
odf <olaf.delgado@gmail.com>
2020-03-27 19:40:11 +1100

    remove inessential scripts

--- src/scripts/dualize.js
+++
@@ -1,8 +0,0 @@
-import * as fs from 'fs';
-
-import * as DS      from '../dsymbols/delaney';
-import * as derived from '../dsymbols/derived';
-
-const text = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
-
-DS.parseSymbols(text).forEach(ds => console.log(`${derived.dual(ds)}`));
--- src/scripts/minimal.js
+++
@@ -1,9 +0,0 @@
-import * as fs from 'fs';
-
-import * as delaney from '../dsymbols/delaney';
-import * as derived from '../dsymbols/derived';
-
-const text = fs.readFileSync(process.argv[2], { encoding: 'utf8' });
-
-for (const ds of delaney.parseSymbols(text))
-  console.log(`${derived.minimal(ds)}`);
