bf343e051ab94e0835580e673ddc31f5ddf1fc0a
odf <olaf.delgado@gmail.com>
2020-03-28 10:19:54 +1100

    scripts/checkSystreFile: clean up and refactor

--- src/scripts/checkSystreFile.js
+++ src/scripts/checkSystreFile.js
@@ -5,26 +5,23 @@ import * as periodic from '../pgraphs/periodic';
 import * as symmetries from '../pgraphs/symmetries';
 
 
-const checkGraph = (graph, writeInfo) => {
-  if (!periodic.isLocallyStable(graph)) {
-    writeInfo('Error - next-nearest neighbor collisions');
-    return false;
-  }
+const cmp = (x, y) => (x > y) - (x < y);
+const cmpLex = (xs, ys) => xs.map((x, i) => cmp(x, ys[i])).find(x => x) || 0;
 
-  if (symmetries.isLadder(graph)) {
-    writeInfo('Error - non-crystallographic (ladder)');
-    return false;
-  }
 
-  if (periodic.hasSecondOrderCollisions(graph)) {
-    writeInfo('Error - second-order collisions');
-    return false;
-  }
-
-  return true;
+const checkGraph = graph => {
+  if (!periodic.isLocallyStable(graph))
+    return 'next-nearest neighbor collisions';
+  else if (symmetries.isLadder(graph))
+    return 'non-crystallographic (ladder)';
+  else if (periodic.hasSecondOrderCollisions(graph))
+    return 'second-order collisions';
 };
 
-const makeCsLookup = data => {
+
+const makeCsLookup = path => {
+  const data = JSON.parse(fs.readFileSync(path, { encoding: 'utf8' }));
+
   const result = {};
 
   for (const { symbol, vertices } of data) {
@@ -44,60 +41,60 @@ const makeCsLookup = data => {
 };
 
 
-const cmp = (x, y) => (x > y) - (x < y);
-const cmpLex = (xs, ys) => xs.map((x, i) => cmp(x, ys[i])).find(x => x) || 0;
-
-
-const [inputPath, data2dPath, data3dPath] = process.argv.slice(2, 5);
-
-const data2d = JSON.parse(fs.readFileSync(data2dPath, { encoding: 'utf8' }));
-const data3d = JSON.parse(fs.readFileSync(data3dPath, { encoding: 'utf8' }));
-
-const input = fs.readFileSync(inputPath, { encoding: 'utf8' });
-
-const lookup2d = makeCsLookup(data2d);
-const lookup3d = makeCsLookup(data3d);
-
-
-for (const { graph, name } of cgd.structures(input)) {
-  const ok = checkGraph(graph, msg => console.log(`${name}: ${msg}`));
-  if (!ok)
-    continue;
-
+const coordinationSequence = graph => {
   const G = symmetries.minimalImageWithOrbits(graph).graph;
   const syms = symmetries.symmetries(G).symmetries;
-  const nodeOrbits = symmetries.nodeOrbits(G, syms);
 
-  const cs = nodeOrbits.map(
+  const cs = symmetries.nodeOrbits(G, syms).map(
     ([v]) => periodic.coordinationSeq(G, v, 10).slice(1)
   );
   cs.sort(cmpLex);
 
-  const lookup = G.dim == 2 ? lookup2d : lookup3d;
-  if (lookup[cs] == null)
-    lookup[cs] = { rcsr: [], cgd: [] };
+  return cs;
+};
 
-  const entry = lookup[cs];
 
-  if (entry.cgd.indexOf(name) >= 0)
-    console.log(`${name}: duplicate`);
-  else if (entry.rcsr.indexOf(name) >= 0)
-    console.log(`${name}: ok!`);
+const [inputPath, data2dPath, data3dPath] = process.argv.slice(2, 5);
+
+const input = fs.readFileSync(inputPath, { encoding: 'utf8' });
+const lookup2d = makeCsLookup(data2dPath);
+const lookup3d = makeCsLookup(data3dPath);
+
+
+for (const { graph, name } of cgd.structures(input)) {
+  const msg = checkGraph(graph);
+
+  if (msg)
+    console.log(`${name}: Error - ${msg}`);
   else {
-    if (entry.rcsr.length == 0)
-      console.log(`${name}: does not match anything`);
-    else
-      console.log(`${name}: mismatch (found ${entry.rcsr})`);
+    const lookup = graph.dim == 2 ? lookup2d : lookup3d;
+    const cs = coordinationSequence(graph);
 
-    for (const seq of cs)
-      console.log('    ' + seq);
+    if (lookup[cs] == null)
+      lookup[cs] = { rcsr: [], cgd: [] };
 
-    if (lookup[name]) {
-      console.log(`  RCSR entry for '${name}' has:`);
-      for (const seq of lookup[name])
+    const entry = lookup[cs];
+
+    if (entry.cgd.indexOf(name) >= 0)
+      console.log(`${name}: duplicate`);
+    else if (entry.rcsr.indexOf(name) >= 0)
+      console.log(`${name}: ok!`);
+    else {
+      if (entry.rcsr.length == 0)
+        console.log(`${name}: does not match anything`);
+      else
+        console.log(`${name}: mismatch (found ${entry.rcsr})`);
+
+      for (const seq of cs)
         console.log('    ' + seq);
-    }
-  }
 
-  entry.cgd.push(name);
+      if (lookup[name]) {
+        console.log(`  RCSR entry for '${name}' has:`);
+        for (const seq of lookup[name])
+          console.log('    ' + seq);
+      }
+    }
+
+    entry.cgd.push(name);
+  }
 }
