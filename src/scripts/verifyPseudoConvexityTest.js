c51cdeaf4c8f6d16d4fd41e380d64ca257c645f2
odf <olaf.delgado@gmail.com>
2020-03-28 18:06:15 +1100

    scripts/verifyPseudoConvexityTest: refactor and clean up

--- src/scripts/verifyPseudoConvexityTest.js
+++ src/scripts/verifyPseudoConvexityTest.js
@@ -1,6 +1,6 @@
 import * as fs from 'fs';
 
-import { rationalMatrices } from '../arithmetic/types';
+import { rationalMatrices as ops } from '../arithmetic/types';
 import * as timing from '../common/timing';
 import { finiteUniversalCover } from '../dsymbols/covers';
 import { orbitReps2, orbit2 } from '../dsymbols/delaney';
@@ -10,10 +10,8 @@ import symbols from '../io/ds';
 
 
 const hasNonDegenerateBarycentricPlacement = ds => {
-  const ops = rationalMatrices;
   const cov = makeCover(ds);
-  const skel = skeleton(cov);
-  const pos = chamberPositions(cov, skel);
+  const pos = chamberPositions(cov, skeleton(cov));
 
   for (const D of cov.elements()) {
     const t = pos[D];
@@ -33,53 +31,49 @@ const hasNonDegenerateBarycentricPlacement = ds => {
 
 const isThreeConnectedSpherical = ds => {
   const cov = finiteUniversalCover(ds);
-  const vertexReps = orbitReps2(cov, 1, 2);
-  const vertexNumber = Array(cov.size + 1);
+  const vertexRep = [];
+  const neighbors = [];
 
-  for (let i = 0; i < vertexReps.length; ++i) {
-    for (const D of orbit2(cov, 1, 2, vertexReps[i])) {
-      vertexNumber[D] = i;
-    }
+  for (const D of orbitReps2(cov, 1, 2)) {
+    neighbors[D] = [];
+    for (const E of orbit2(cov, 1, 2, D))
+      vertexRep[E] = D;
   }
 
-  const neigbors = Array(vertexReps.length).fill(0).map(() => []);
-
   for (const D of orbitReps2(cov, 0, 2)) {
-    const E = cov.s(2, cov.s(0, D));
-    const v = vertexNumber[D];
-    const w = vertexNumber[E];
+    const v = vertexRep[D];
+    const w = vertexRep[cov.s(2, cov.s(0, D))];
 
-    if (w == v || neigbors[v].includes(w))
+    if (w == v || neighbors[v].includes(w))
       return false;
 
-    neigbors[v].push(w);
-    neigbors[w].push(v);
+    neighbors[v].push(w);
+    neighbors[w].push(v);
   }
 
   const faces = [];
   for (const D of orbitReps2(cov, 0, 1)) {
     const f = [];
-
     let E = D;
     do {
       E = cov.s(1, cov.s(0, E));
-      f.push(vertexNumber[E]);
+      f.push(vertexRep[E]);
     }
     while (E != D);
 
     faces.push(f);
   }
 
-  const badFaceIntersection = (i, j) => {
-    const matches = faces[i].map(v => faces[j].includes(v));
-    matches.push(matches[0]);
-    return matches.filter((x, k) => !x && matches[k + 1]).length > 1;
-  };
-
-  for (let i = 0; i < faces.length; ++i) {
-    for (let j = i + 1; j < faces.length; ++j) {
-      if (badFaceIntersection(i, j) || badFaceIntersection(j, i))
-        return false;
+  for (const fa of faces) {
+    for (const fb of faces) {
+      let count = 0;
+      for (let k = 0; k < fa.length; ++k) {
+        if (!fb.includes(fa[k]) && fb.includes(fa[(k + 1) % fa.length])) {
+          ++count;
+          if (count > 1)
+            return false;
+        }
+      }
     }
   }
 
