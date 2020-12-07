5b0c4a0838d0d7320e7dda81f54e80a04c45e4fe
odf <olaf.delgado@gmail.com>
2020-12-07 13:38:51 +1100

    ui: move some functions from makeScene.js into new file geometries.js

---
+++ src/ui/geometries.js
@@ -0,0 +1,115 @@
+import { coordinateChangesF as opsF } from '../geometry/types';
+import { subD } from './surface';
+
+
+const range = n => [...Array(n).keys()];
+const normalized = v => opsF.div(v, opsF.norm(v));
+
+
+export const geometry = (vertsIn, faces) => {
+  const normals = vertsIn.map(v => opsF.times(v, 0));
+
+  for (const f of faces) {
+    const n = f.length;
+    for (let i = 0; i < n; ++i) {
+      const u = f[i];
+      const v = f[(i + 1) % n];
+      const w = f[(i + 2) % n];
+
+      const a = opsF.minus(vertsIn[u], vertsIn[v]);
+      const b = opsF.minus(vertsIn[w], vertsIn[v]);
+
+      normals[v] = opsF.plus(normals[v], opsF.crossProduct(b, a));
+    }
+  }
+
+  const vertices = vertsIn.map((v, i) => ({
+    pos: v,
+    normal: normalized(normals[i])
+  }));
+
+  return { vertices, faces }
+};
+
+
+export const splitGeometry = ({ vertices, faces }, faceLabels) => {
+  const facesByLabel = {};
+
+  for (let f = 0; f < faces.length; ++f) {
+    const label = faceLabels[f];
+    if (facesByLabel[label] == null)
+      facesByLabel[label] = [];
+    facesByLabel[label].push(faces[f]);
+  }
+
+  const subMeshes = {};
+  for (const label of Object.keys(facesByLabel)) {
+    const vertexMap = {};
+    const subVerts = [];
+    for (const vs of facesByLabel[label]) {
+      for (const v of vs) {
+        if (vertexMap[v] == null) {
+          vertexMap[v] = subVerts.length;
+          subVerts.push(vertices[v]);
+        }
+      }
+    };
+
+    const subFaces = facesByLabel[label].map(vs => vs.map(v => vertexMap[v]));
+    subMeshes[label] = { vertices: subVerts, faces: subFaces };
+  }
+
+  return subMeshes;
+};
+
+
+export const makeBall = radius => {
+  const t0 = {
+    pos: [[1,0,0], [0,1,0], [0,0,1], [-1,0,0], [0,-1,0], [0,0,-1]],
+    faces : [[0,1,2], [1,0,5], [2,1,3], [0,2,4],
+             [3,5,4], [5,3,1], [4,5,0], [3,4,2]],
+    isFixed: [false, false, false, false, false, false]
+  };
+  const t = subD(subD(subD(t0)));
+
+  return geometry(t.pos.map(v => opsF.times(normalized(v), radius)), t.faces);
+};
+
+
+export const makeStick = (radius, segments) => {
+  const n = segments;
+  const a = Math.PI * 2 / n;
+
+  const bottom = range(n).map(i => [
+    Math.cos(a * i) * radius, Math.sin(a * i) * radius, 0
+  ]);
+  const top = range(n).map(i => [
+    Math.cos(a * i) * radius, Math.sin(a * i) * radius, 1
+  ]);
+  const vertices = [].concat(bottom, top);
+
+  const faces = range(n).map(i => {
+    const j = (i + 1) % n;
+    return [i, j, j+n, i+n];
+  });
+
+  return geometry(vertices, faces);
+};
+
+
+export const stickTransform = (p, q, ballRadius, stickRadius) => {
+  const w = opsF.minus(q, p);
+  const d = normalized(w);
+  const ex = [1,0,0];
+  const ey = [0,1,0];
+  const t = Math.abs(opsF.times(d, ex)) > 0.9 ? ey : ex;
+  const u = normalized(opsF.crossProduct(d, t));
+  const v = normalized(opsF.crossProduct(d, u));
+
+  const r = Math.min(ballRadius, stickRadius);
+  const s = Math.sqrt(ballRadius * ballRadius - r * r);
+  const p1 = opsF.plus(p, opsF.times(s, d));
+  const w1 = opsF.minus(w, opsF.times(2 * s, d));
+
+  return { basis: [ u, v, w1 ], shift: p1 };
+};
--- src/ui/makeScene.js
+++ src/ui/makeScene.js
@@ -5,13 +5,13 @@ import * as derived from '../dsymbols/derived';
 import * as tilings from '../dsymbols/tilings';
 import * as periodic from '../pgraphs/periodic';
 import * as netSyms from '../pgraphs/symmetries';
+import * as geometries from './geometries';
 
 import { serialize as encode } from '../common/pickler';
 import { timer } from '../common/timing';
 import { invariantBasis } from '../spacegroups/unitCells';
 import { centeringLatticePoints } from '../spacegroups/spacegroups';
 import { identifySpacegroup } from '../spacegroups/spacegroupFinder';
-import { subD } from './surface';
 
 import {
   coordinateChangesQ as opsQ,
@@ -23,6 +23,10 @@ const range = n => [...Array(n).keys()];
 const centeredRange = n => range(n).map(i => i - Math.ceil(n/2) + 1);
 const withDefault = (value, fallback) => value != null ? value : fallback;
 
+const asVec3 = v => [v[0], v[1], v[2] || 0];
+const applyToPoint = (cc, vec) => opsQ.vector(opsQ.times(cc, opsQ.point(vec)));
+
+
 const cartesian = (...vs) => (
   vs.length == 0 ?
     [[]] :
@@ -30,120 +34,6 @@ const cartesian = (...vs) => (
       ...vs[0].map(v => cartesian(...vs.slice(1)).map(xs => [v].concat(xs)))
     )
 );
-
-
-const normalized = v => opsF.div(v, opsF.norm(v));
-const asVec3 = v => [v[0], v[1], v[2] || 0];
-const applyToPoint = (cc, vec) => opsQ.vector(opsQ.times(cc, opsQ.point(vec)));
-
-
-const geometry = (vertsIn, faces) => {
-  const normals = vertsIn.map(v => opsF.times(v, 0));
-
-  for (const f of faces) {
-    const n = f.length;
-    for (let i = 0; i < n; ++i) {
-      const u = f[i];
-      const v = f[(i + 1) % n];
-      const w = f[(i + 2) % n];
-
-      const a = opsF.minus(vertsIn[u], vertsIn[v]);
-      const b = opsF.minus(vertsIn[w], vertsIn[v]);
-
-      normals[v] = opsF.plus(normals[v], opsF.crossProduct(b, a));
-    }
-  }
-
-  const vertices = vertsIn.map((v, i) => ({
-    pos: v,
-    normal: normalized(normals[i])
-  }));
-
-  return { vertices, faces }
-};
-
-
-const splitGeometry = ({ vertices, faces }, faceLabels) => {
-  const facesByLabel = {};
-
-  for (let f = 0; f < faces.length; ++f) {
-    const label = faceLabels[f];
-    if (facesByLabel[label] == null)
-      facesByLabel[label] = [];
-    facesByLabel[label].push(faces[f]);
-  }
-
-  const subMeshes = {};
-  for (const label of Object.keys(facesByLabel)) {
-    const vertexMap = {};
-    const subVerts = [];
-    for (const vs of facesByLabel[label]) {
-      for (const v of vs) {
-        if (vertexMap[v] == null) {
-          vertexMap[v] = subVerts.length;
-          subVerts.push(vertices[v]);
-        }
-      }
-    };
-
-    const subFaces = facesByLabel[label].map(vs => vs.map(v => vertexMap[v]));
-    subMeshes[label] = { vertices: subVerts, faces: subFaces };
-  }
-
-  return subMeshes;
-};
-
-
-const makeBall = radius => {
-  const t0 = {
-    pos: [[1,0,0], [0,1,0], [0,0,1], [-1,0,0], [0,-1,0], [0,0,-1]],
-    faces : [[0,1,2], [1,0,5], [2,1,3], [0,2,4],
-             [3,5,4], [5,3,1], [4,5,0], [3,4,2]],
-    isFixed: [false, false, false, false, false, false]
-  };
-  const t = subD(subD(subD(t0)));
-
-  return geometry(t.pos.map(v => opsF.times(normalized(v), radius)), t.faces);
-};
-
-
-const makeStick = (radius, segments) => {
-  const n = segments;
-  const a = Math.PI * 2 / n;
-
-  const bottom = range(n).map(i => [
-    Math.cos(a * i) * radius, Math.sin(a * i) * radius, 0
-  ]);
-  const top = range(n).map(i => [
-    Math.cos(a * i) * radius, Math.sin(a * i) * radius, 1
-  ]);
-  const vertices = [].concat(bottom, top);
-
-  const faces = range(n).map(i => {
-    const j = (i + 1) % n;
-    return [i, j, j+n, i+n];
-  });
-
-  return geometry(vertices, faces);
-};
-
-
-const stickTransform = (p, q, ballRadius, stickRadius) => {
-  const w = opsF.minus(q, p);
-  const d = normalized(w);
-  const ex = [1,0,0];
-  const ey = [0,1,0];
-  const t = Math.abs(opsF.times(d, ex)) > 0.9 ? ey : ex;
-  const u = normalized(opsF.crossProduct(d, t));
-  const v = normalized(opsF.crossProduct(d, u));
-
-  const r = Math.min(ballRadius, stickRadius);
-  const s = Math.sqrt(ballRadius * ballRadius - r * r);
-  const p1 = opsF.plus(p, opsF.times(s, d));
-  const w1 = opsF.minus(w, opsF.times(2 * s, d));
-
-  return { basis: [ u, v, w1 ], shift: p1 };
-};
 
 
 const baseShifts = (dim, options) => dim == 3 ?
@@ -168,8 +58,8 @@ const addUnitCell = (model, basis, origin, ballRadius, stickRadius) => {
   const instances = model.instances.slice();
 
   const n = meshes.length;
-  meshes.push(makeBall(ballRadius));
-  meshes.push(makeStick(stickRadius, 48));
+  meshes.push(geometries.makeBall(ballRadius));
+  meshes.push(geometries.makeStick(stickRadius, 48));
 
   const dim = basis.length;
   const corners = dim == 3 ?
@@ -197,7 +87,7 @@ const addUnitCell = (model, basis, origin, ballRadius, stickRadius) => {
 
     for (const p0 of startPoints) {
       const p = opsF.plus(origin, p0);
-      const transform = stickTransform(
+      const transform = geometries.stickTransform(
         asVec3(p), asVec3(opsF.plus(p, u)), ballRadius, stickRadius
       );
 
@@ -486,7 +376,9 @@ const makeNetModel = (data, options, runJob, log) => csp.go(
     const t = timer();
 
     yield log('Making the net geometry...');
-    const meshes = [ makeBall(ballRadius), makeStick(stickRadius, 48) ];
+    const meshes = [
+      geometries.makeBall(ballRadius), geometries.makeStick(stickRadius, 48)
+    ];
     const instances = [];
 
     for (let i = 0; i < displayList.length; ++i) {
@@ -508,7 +400,7 @@ const makeNetModel = (data, options, runJob, log) => csp.go(
       else {
         const p = opsF.times(pos[item.head], basis);
         const q = opsF.times(opsF.plus(pos[item.tail], item.shift), basis);
-        const transform = stickTransform(
+        const transform = geometries.stickTransform(
           asVec3(p), asVec3(q), ballRadius, stickRadius
         );
 
@@ -548,7 +440,7 @@ const splitMeshes = (meshes, faceLabelLists) => {
   const partLists = [];
 
   for (let i = 0; i < meshes.length; ++i) {
-    const parts = splitGeometry(meshes[i], faceLabelLists[i]);
+    const parts = geometries.splitGeometry(meshes[i], faceLabelLists[i]);
     const keys = Object.keys(parts);
     partLists[i] = [];
 
@@ -822,7 +714,9 @@ const makeTilingModel = (data, options, runJob, log) => csp.go(function*() {
       cov, skel, embedding.positions, orbitReps, basis,
       subDLevel, tighten, edgeWidth, runJob, log
     );
-    const meshes = rawMeshes.map(({ pos, faces }) => geometry(pos, faces));
+    const meshes = rawMeshes.map(
+      ({ pos, faces }) => geometries.geometry(pos, faces)
+    );
     const faceLabelLists = rawMeshes.map(({ faceLabels }) => faceLabels);
 
     embedding[key] = dim == 2 ?
